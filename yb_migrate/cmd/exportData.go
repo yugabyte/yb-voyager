3a4c140f905891f70aa2c9e9d883ae7e0205ae5a
sanyamsinghal <ssinghal@yugabyte.com>
2021-11-25 08:46:01 +0000

    Implementation of export data command started

--- yb_migrate/cmd/exportData.go
+++ yb_migrate/cmd/exportData.go
@@ -17,6 +17,8 @@ package cmd
 
 import (
 	"fmt"
+	"yb_migrate/migration"
+	"yb_migrate/migrationutil"
 
 	"github.com/spf13/cobra"
 )
@@ -35,12 +37,59 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("export data called")
+		exportData()
 	},
 }
 
 func init() {
 	exportCmd.AddCommand(exportDataCmd)
 
-	//exportDataCmd.PersistentFlags().StringVar(&sourceDBType, "source-db-type", "",
-	//	"source database type (Oracle/PostgreSQL/MySQL)")
+	exportDataCmd.PersistentFlags().StringVar(&exportMode, "export mode", "offline",
+		"offline | online")
 }
+
+func exportData() {
+	projectDirName := migrationutil.GetProjectDirName(source.DBType, source.Schema, source.DBName)
+
+	if source.DBType == "oracle" {
+		migrationutil.CreateMigrationProject(exportDir, projectDirName, source.Schema)
+	} else {
+		migrationutil.CreateMigrationProject(exportDir, projectDirName, source.DBName)
+	}
+
+	if exportMode == "offline" {
+		exportDataOffline()
+	} else {
+		exportDataOnline()
+	}
+}
+
+func exportDataOffline() {
+	/*
+		TODO: check and clean the data directory before export everytime this command
+	*/
+
+	switch source.DBType {
+	case "oracle":
+		fmt.Printf("Prepare Ora2Pg for data export from Oracle\n")
+		if source.Port == "" {
+			source.Port = "1521"
+		}
+		migration.OracleExportDataOffline(&source, exportDir)
+	case "postgres":
+		fmt.Printf("Prepare ysql_dump for data export from PG\n")
+		if source.Port == "" {
+			source.Port = "5432"
+		}
+		migration.PostgresExportDataOffline(&source, exportDir)
+	case "mysql":
+		fmt.Printf("Prepare Ora2Pg for data export from MySQL\n")
+		if source.Port == "" {
+			source.Port = "3306"
+		}
+		// migration.mysqlDataExportOffline()
+	}
+
+}
+
+func exportDataOnline() {}
