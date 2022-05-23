yugabyte/yb-voyager

commit 9302b1981dc10e60183f64db62bad842b45089c1

Added support for PG13 (#128)
* Added support for PG13

yb_migrate/src/srcdb/pg_dump_extract_schema.go
@@ -70,7 +70,6 @@ func parseSchemaFile(source *Source, exportDir string) {
 		createSchemaSqls, createExtensionSqls, createProcedureSqls, setSessionVariables strings.Builder
 
 	var isPossibleFlag bool = true
-
 	for i := 0; i < numLines; i++ {
 		if sqlTypeInfoCommentPattern.MatchString(schemaFileLines[i]) {
 			sqlType := extractSqlTypeFromSqlInfoComment(schemaFileLines[i])
@@ -173,10 +172,11 @@ func extractSqlTypeFromSqlInfoComment(sqlInfoComment string) string {
 
 func extractSqlStatements(schemaFileLines []string, index *int) string {
 	var sqlStatement strings.Builder
-
 	for (*index) < len(schemaFileLines) {
 		if isSqlComment(schemaFileLines[(*index)]) {
 			break
+		} else if shouldSkipLine(schemaFileLines[(*index)]) {
+			continue
 		} else {
 			sqlStatement.WriteString(schemaFileLines[(*index)] + "\n")
 		}
@@ -188,4 +188,8 @@ func extractSqlStatements(schemaFileLines []string, index *int) string {
 
 func isSqlComment(line string) bool {
 	return len(line) >= 2 && line[:2] == "--"
+}
+
+func shouldSkipLine(line string) bool {
+	return strings.HasPrefix(line, "SET default_table_access_method")
 }

commit 34b857db42675b4a87b5f6b7009d038d039492c7

Fix an issue where `export schema` hangs because of a regression.
A recent commit 9302b1981 introduced a regression because of which
`export schema` command went into an infinite loop. The change
missed incrementing loop index in one of the branches in the loop body.

yb_migrate/src/srcdb/pg_dump_extract_schema.go
@@ -176,12 +176,12 @@ func extractSqlStatements(schemaFileLines []string, index *int) string {
 		if isSqlComment(schemaFileLines[(*index)]) {
 			break
 		} else if shouldSkipLine(schemaFileLines[(*index)]) {
+			(*index)++
 			continue
 		} else {
 			sqlStatement.WriteString(schemaFileLines[(*index)] + "\n")
+			(*index)++
 		}
-
-		(*index)++
 	}
 	return sqlStatement.String()
 }