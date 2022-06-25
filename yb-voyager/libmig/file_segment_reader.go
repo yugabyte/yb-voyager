f03089a6825bf24ea03d30ac23d8f81d01a76504
Amit Jambure <ajambure@yugabyte.com>
2022-06-25 09:48:30 +0530

    Import File.

---
+++ yb-voyager/libmig/file_segment_reader.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+type FileSegmentReader struct {
+	fileName    string
+	fh          *os.File
+	StartOffset int64
+	EndOffset   int64
+	CurOffset   int64
+}
+
+func NewFileSegmentReader(fileName string, startOffset, endOffset int64) (*FileSegmentReader, error) {
+	fh, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	if endOffset == -1 {
+		fi, err := fh.Stat()
+		if err != nil {
+			return nil, err
+		}
+		endOffset = fi.Size()
+	}
+	r := &FileSegmentReader{
+		fileName:    fileName,
+		fh:          fh,
+		StartOffset: startOffset,
+		EndOffset:   endOffset,
+		CurOffset:   startOffset,
+	}
+	return r, nil
+}
+
+func (r *FileSegmentReader) Read(p []byte) (n int, err error) {
+	if r.CurOffset >= r.EndOffset {
+		return 0, io.EOF
+	}
+	n, err = r.fh.ReadAt(p, r.CurOffset)
+	if r.CurOffset+int64(n) >= r.EndOffset {
+		n = int(r.EndOffset) - int(r.CurOffset)
+		err = io.EOF
+	}
+	r.CurOffset = r.CurOffset + int64(n)
+	return n, err
+}
+
+func (r *FileSegmentReader) Close() error {
+	return r.fh.Close()
+}
