b57d8fcfe7d13ebabeb954e409a846ac96da0fb8
Amit Jambure <ajambure@yugabyte.com>
2022-06-26 19:09:54 +0530

    DataFileDescriptor.

--- yb-voyager/libmig/main.go
+++ yb-voyager/libmig/main.go
@@ -11,11 +11,17 @@ func main() {
 	ctx := context.Background()
 
 	migstate := NewMigrationState("/Users/amit.jambure/export-dir")
-	//op := NewImportFileOp(migstate, "test.txt", NewTableID("testdb", "public", "foo"))
-	op := NewImportFileOp(migstate, "/tmp/category_data.sql", NewTableID("testdb", "public", "category"))
-	op.BatchSize = 5
 
-	err := op.Run(ctx)
+	desc1 := &DataFileDescriptor{FileType: FILE_TYPE_CSV}
+	op1 := NewImportFileOp(migstate, "test.txt", NewTableID("testdb", "public", "foo"), desc1)
+	op1.BatchSize = 4
+	err := op1.Run(ctx)
+	panicOnErr(err)
+
+	desc2 := &DataFileDescriptor{FileType: FILE_TYPE_ORA2PG}
+	op2 := NewImportFileOp(migstate, "/tmp/category_data.sql", NewTableID("testdb", "public", "category"), desc2)
+	op2.BatchSize = 5
+	err = op2.Run(ctx)
 	panicOnErr(err)
 }
 
