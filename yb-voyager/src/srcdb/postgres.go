db9ad9604d98465064617a1a2b92e15a7ff419f2
sanyamsinghal <ssinghal@yugabyte.com>
2022-07-07 12:24:00 +0000

    Support for MySQL Table Partitions in ora2pg was introduced after v23.1 release hence using master github branch

--- yb-voyager/src/srcdb/postgres.go
+++ yb-voyager/src/srcdb/postgres.go
@@ -58,7 +58,11 @@ func (pg *PostgreSQL) GetTableApproxRowCount(tableProgressMetadata *utils.TableP
 	// table partition names are under schema as a namespace
 
 	log.Infof("Querying '%s' approx row count of table %q", query, tableProgressMetadata.TableName)
+	err := pg.db.QueryRow(context.Background(), query).Scan(&approxRowCount)
 	if err != nil {
+		utils.ErrExit("Failed to query %q for approx row count of %q: %s", query, tableProgressMetadata.FullTableName, err)
+	}
+
 	log.Infof("Table %q has approx %v rows.", tableProgressMetadata.FullTableName, approxRowCount)
 	return approxRowCount.Int64
 }
