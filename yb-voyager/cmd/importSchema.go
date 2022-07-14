yugabyte/yb-voyager

commit afd16c96a0d0e03b2c4b0fc61ce2380c0f6e0459

Specific Schema List support for export from PG (#179)
* Refactored the code for flags validation and setting default values
* Added COMMENT as a separate category of SQL statements for postgres
* Added support for providing schema list to export schema and data from postgres
* Updated pg-dvdrental testcase with mandatory flag --source-db-schema

yb-voyager/cmd/importSchema.go
@@ -18,7 +18,6 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/jackc/pgx/v4"
@@ -36,7 +35,7 @@ var importSchemaCmd = &cobra.Command{
 	Long:  ``,
 
 	PreRun: func(cmd *cobra.Command, args []string) {
-		validateImportFlags(cmd)
+		validateImportFlags()
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {
@@ -67,13 +66,13 @@ func importSchema() {
 	payload := callhome.GetPayload(exportDir)
 	payload.TargetDBVersion = targetDBVersion
 
-	// in case of postgreSQL as source, there can be multiple schemas present in a database
-	targetSchemas := []string{target.Schema}
-	if sourceDBType == "postgresql" {
+	var targetSchemas []string
+	if sourceDBType == "postgresql" { // in case of postgreSQL as source, there can be multiple schemas present in a database
 		source = srcdb.Source{DBType: sourceDBType}
-		targetSchemas = append(targetSchemas, utils.GetObjectNameListFromReport(analyzeSchemaInternal(), "SCHEMA")...)
+		targetSchemas = utils.GetObjectNameListFromReport(analyzeSchemaInternal(), "SCHEMA")
 	} else if sourceDBType == "oracle" { // ORACLE PACKAGEs are exported as SCHEMAs
 		source = srcdb.Source{DBType: sourceDBType}
+		targetSchemas = append(targetSchemas, YUGABYTEDB_DEFAULT_SCHEMA)
 		targetSchemas = append(targetSchemas, utils.GetObjectNameListFromReport(analyzeSchemaInternal(), "PACKAGE")...)
 	}
 
@@ -102,22 +101,23 @@ func importSchema() {
 		}
 	}
 
-	schemaExists := checkIfTargetSchemaExists(conn, target.Schema)
-	createSchemaQuery := fmt.Sprintf("CREATE SCHEMA %s", target.Schema)
-	/* --target-db-schema(or target.Schema) flag valid for Oracle & MySQL
-	only create target.Schema, other required schemas are created via .sql files */
-	if !schemaExists {
-		utils.PrintAndLog("creating schema '%s' in target database...", target.Schema)
-		_, err := conn.Exec(bgCtx, createSchemaQuery)
-		if err != nil {
-			utils.ErrExit("Failed to create %q schema in the target DB: %s", target.Schema, err)
+	if sourceDBType != POSTGRESQL {
+		schemaExists := checkIfTargetSchemaExists(conn, target.Schema)
+		createSchemaQuery := fmt.Sprintf("CREATE SCHEMA %s", target.Schema)
+		/* --target-db-schema(or target.Schema) flag valid for Oracle & MySQL
+		only create target.Schema, other required schemas are created via .sql files */
+		if !schemaExists {
+			utils.PrintAndLog("creating schema '%s' in target database...", target.Schema)
+			_, err := conn.Exec(bgCtx, createSchemaQuery)
+			if err != nil {
+				utils.ErrExit("Failed to create %q schema in the target DB: %s", target.Schema, err)
+			}
 		}
-	}
 
-	if sourceDBType != POSTGRESQL && target.Schema == "public" &&
-		!utils.AskPrompt("do you really want to import into 'public' schema") {
-		log.Infof("User selected not to import in the `public` schema. Exiting.")
-		os.Exit(1)
+		if target.Schema == YUGABYTEDB_DEFAULT_SCHEMA &&
+			!utils.AskPrompt("do you really want to import into 'public' schema") {
+			utils.ErrExit("User selected not to import in the `public` schema. Exiting.")
+		}
 	}
 
 	YugabyteDBImportSchema(&target, exportDir)

migtests/tests/pg-dvdrental/env.sh
@@ -1,2 +1,3 @@
 export SOURCE_DB_TYPE="postgresql"
 export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"dvdrental"}
+export SOURCE_DB_SCHEMA="public"

yb-voyager/cmd/analyzeSchema.go
@@ -809,28 +809,10 @@ var analyzeSchemaCmd = &cobra.Command{
 	Long:  ``,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		cmd.Parent().PersistentPreRun(cmd.Parent(), args)
-		checkExportDirFlag()
-		checkSourceDBType()
-		setSourceDefaultPort() //will set only if required
-		validatePortRange()
-		checkOrSetDefaultSSLMode()
-
-		//marking flags as required based on conditions
-		cmd.MarkPersistentFlagRequired("source-db-type")
-		cmd.MarkPersistentFlagRequired("source-db-user")
-		cmd.MarkPersistentFlagRequired("source-db-password")
-		if source.DBType != ORACLE {
-			cmd.MarkPersistentFlagRequired("source-db-name")
-		} else if source.DBType == ORACLE {
-			cmd.MarkPersistentFlagRequired("source-db-schema")
-			validateOracleParams()
-		}
-
-		if source.TableList != "" {
-			checkTableListFlag(source.TableList)
-		}
-
-		checkReportOutputFormat()
+		setExportFlagsDefaults()
+		validateReportOutputFormat()
+		validateExportFlags()
+		markFlagsRequired(cmd)
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {
@@ -847,7 +829,7 @@ func init() {
 		"allowed report formats: html | txt | json | xml")
 }
 
-func checkReportOutputFormat() {
+func validateReportOutputFormat() {
 	allowedOutputFormats := []string{"html", "json", "txt", "xml"}
 	outputFormat = strings.ToLower(outputFormat)
 
@@ -856,5 +838,5 @@ func checkReportOutputFormat() {
 			return
 		}
 	}
-	utils.ErrExit("Invalid output format: %s. Supported formats are [%v]", outputFormat, allowedOutputFormats)
+	utils.ErrExit("Error: Invalid output format: %s. Supported formats are %v", outputFormat, allowedOutputFormats)
 }

yb-voyager/cmd/constants.go
@@ -45,3 +45,8 @@ var IMPORT_SESSION_SETTERS = []string{
 }
 
 var supportedSourceDBTypes = []string{ORACLE, MYSQL, POSTGRESQL}
+
+var validSSLModes = map[string][]string{
+	"mysql":      {"disable", "prefer", "require", "verify-ca", "verify-full"},
+	"postgresql": {"disable", "allow", "prefer", "require", "verify-ca", "verify-full"},
+}

yb-voyager/cmd/export.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/yugabyte/yb-voyager/yb-voyager/src/srcdb"
 	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils"
+	"golang.org/x/exp/slices"
 
 	"github.com/spf13/cobra"
 )
@@ -39,27 +40,9 @@ var exportCmd = &cobra.Command{
 
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		cmd.Parent().PersistentPreRun(cmd.Parent(), args)
-		checkExportDirFlag()
-		checkSourceDBType()
+		setExportFlagsDefaults()
 		validateExportFlags()
-		setSourceDefaultPort() //will set only if required
-		validatePortRange()
-		checkOrSetDefaultSSLMode()
-
-		//marking flags as required based on conditions
-		cmd.MarkPersistentFlagRequired("source-db-type")
-		cmd.MarkPersistentFlagRequired("source-db-user")
-		cmd.MarkPersistentFlagRequired("source-db-password")
-		if source.DBType != ORACLE {
-			cmd.MarkPersistentFlagRequired("source-db-name")
-		} else if source.DBType == ORACLE {
-			cmd.MarkPersistentFlagRequired("source-db-schema")
-			validateOracleParams()
-		}
-
-		if source.TableList != "" {
-			checkTableListFlag(source.TableList)
-		}
+		markFlagsRequired(cmd)
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {
@@ -117,7 +100,8 @@ func registerCommonExportFlags(cmd *cobra.Command) {
 	//out of schema and db-name one should be mandatory(oracle vs others)
 
 	cmd.PersistentFlags().StringVar(&source.Schema, "source-db-schema", "",
-		"[For Oracle Only] source schema name which needs to be migrated to YugabyteDB")
+		"source schema name which needs to be migrated to YugabyteDB (valid for Oracle, PostgreSQL)\n"+
+			"Note: in case of PostgreSQL, it can be a single or comma separated list of schemas")
 
 	// TODO SSL related more args will come. Explore them later.
 	cmd.PersistentFlags().StringVar(&source.SSLCertPath, "source-ssl-cert", "",
@@ -139,85 +123,113 @@ func registerCommonExportFlags(cmd *cobra.Command) {
 		"clean the project's data directory for already existing files before start(Note: works only for export data command)")
 }
 
-func checkSourceDBType() {
-	if source.DBType == "" {
-		utils.ErrExit("Requried flag: source-db-type. Supported source db types are: %s", supportedSourceDBTypes)
-	}
-
-	source.DBType = strings.ToLower(source.DBType)
-	for _, sourceDBType := range supportedSourceDBTypes {
-		if sourceDBType == source.DBType {
-			return //if matches any allowed type
-		}
-	}
-	utils.ErrExit("Invalid source db type: %s. Supported source db types are: %s", source.DBType, supportedSourceDBTypes)
-}
-
-func validateExportFlags() {
-	if source.DBType != ORACLE {
-		if source.DBSid != "" {
-			utils.ErrExit("ERROR: --oracle-db-sid flag is only valid for 'oracle' db type")
-		}
-		if source.OracleHome != "" {
-			utils.ErrExit("ERROR: --oracle-home flag is only valid for 'oracle' db type")
-		}
-		if source.TNSAlias != "" {
-			utils.ErrExit("ERROR: --oracle-tns-alias flag is only valid for 'oracle' db type")
-		}
-		if source.Schema != "" {
-			utils.ErrExit("ERROR: --source-db-schema flag is only valid for 'oracle' db type")
-		}
-	}
+func setExportFlagsDefaults() {
+	setSourceDefaultPort() //will set only if required
+	setDefaultSSLMode()
 }
 
 func setSourceDefaultPort() {
-	if source.Port == -1 {
-		switch source.DBType {
-		case ORACLE:
-			source.Port = ORACLE_DEFAULT_PORT
-		case POSTGRESQL:
-			source.Port = POSTGRES_DEFAULT_PORT
-		case MYSQL:
-			source.Port = MYSQL_DEFAULT_PORT
+	if source.Port != -1 {
+		return
+	}
+	switch source.DBType {
+	case ORACLE:
+		source.Port = ORACLE_DEFAULT_PORT
+	case POSTGRESQL:
+		source.Port = POSTGRES_DEFAULT_PORT
+	case MYSQL:
+		source.Port = MYSQL_DEFAULT_PORT
+	}
+}
+
+func setDefaultSSLMode() {
+	if source.SSLMode != "" {
+		return
+	}
+	switch source.DBType {
+	case MYSQL, POSTGRESQL:
+		source.SSLMode = "prefer"
+	}
+}
+
+func validateExportFlags() {
+	validateExportDirFlag()
+	validateSourceDBType()
+	validateSourceSchema()
+	validatePortRange()
+	validateSSLMode()
+	validateOracleParams()
+	validateTableListFlag(source.TableList)
+
+	// checking if wrong flag is given used for a db type
+	if source.DBType != ORACLE {
+		if source.DBSid != "" {
+			utils.ErrExit("Error: --oracle-db-sid flag is only valid for 'oracle' db type")
 		}
+		if source.OracleHome != "" {
+			utils.ErrExit("Error: --oracle-home flag is only valid for 'oracle' db type")
+		}
+		if source.TNSAlias != "" {
+			utils.ErrExit("Error: --oracle-tns-alias flag is only valid for 'oracle' db type")
+		}
+	}
+}
+
+func validateSourceDBType() {
+	if source.DBType == "" {
+		utils.ErrExit("Error: required flag \"source-db-type\" not set")
+	}
+
+	source.DBType = strings.ToLower(source.DBType)
+	if !slices.Contains(supportedSourceDBTypes, source.DBType) {
+		utils.ErrExit("Error: Invalid source-db-type: %q. Supported source db types are: %s", source.DBType, supportedSourceDBTypes)
+	}
+}
+
+func validateSourceSchema() {
+	if source.Schema == "" {
+		return
+	}
+
+	schemaList := utils.CsvStringToSlice(source.Schema)
+	switch source.DBType {
+	case MYSQL:
+		utils.ErrExit("Error: --source-db-schema flag is not valid for 'MySQL' db type")
+	case ORACLE:
+		if len(schemaList) > 1 {
+			utils.ErrExit("Error: single schema at a time is allowed to export from oracle. List of schemas provided: %s", schemaList)
+		}
+	case POSTGRESQL:
+		// In PG, its supported to export more than one schema
+		source.Schema = strings.Join(schemaList, "|") // clean and correct formatted for pg
 	}
 }
 
 func validatePortRange() {
 	if source.Port < 0 || source.Port > 65535 {
-		utils.ErrExit("Invalid port number %d. Valid range is 0-65535", source.Port)
+		utils.ErrExit("Error: Invalid port number %d. Valid range is 0-65535", source.Port)
 	}
 }
 
-func checkOrSetDefaultSSLMode() {
-
-	switch source.DBType {
-	case MYSQL:
-		if source.SSLMode == "disable" || source.SSLMode == "prefer" || source.SSLMode == "require" || source.SSLMode == "verify-ca" || source.SSLMode == "verify-full" {
-			return
-		} else if source.SSLMode == "" {
-			source.SSLMode = "prefer"
-		} else {
-			utils.ErrExit("Invalid sslmode %q. Required one of [disable, prefer, require, verify-ca, verify-full]", source.SSLMode)
-		}
-	case POSTGRESQL:
-		if source.SSLMode == "disable" || source.SSLMode == "allow" || source.SSLMode == "prefer" || source.SSLMode == "require" || source.SSLMode == "verify-ca" || source.SSLMode == "verify-full" {
-			return
-		} else if source.SSLMode == "" {
-			source.SSLMode = "prefer"
-		} else {
-			utils.ErrExit("Invalid sslmode %q. Required one of [disable, allow, prefer, require, verify-ca, verify-full]", source.SSLMode)
-		}
+func validateSSLMode() {
+	if source.DBType == ORACLE || slices.Contains(validSSLModes[source.DBType], source.SSLMode) {
+		return
+	} else {
+		utils.ErrExit("Error: Invalid sslmode: %q. Valid SSL modes are %v", validSSLModes[source.DBType])
 	}
 }
 
 func validateOracleParams() {
+	if source.DBType != ORACLE {
+		return
+	}
+
 	// in oracle, object names are stored in UPPER CASE by default(case insensitive)
 	if !utils.IsQuotedString(source.Schema) {
 		source.Schema = strings.ToUpper(source.Schema)
 	}
 	if source.DBName == "" && source.DBSid == "" && source.TNSAlias == "" {
-		utils.ErrExit(`ERROR: one flag required out of "oracle-tns-alias", "source-db-name", "oracle-db-sid" required.`)
+		utils.ErrExit(`Error: one flag required out of "oracle-tns-alias", "source-db-name", "oracle-db-sid" required.`)
 	} else if source.TNSAlias != "" {
 		//Priority order for Oracle: oracle-tns-alias > source-db-name > oracle-db-sid
 		utils.PrintAndLog("Using TNS Alias for export.")
@@ -230,4 +242,19 @@ func validateOracleParams() {
 		utils.PrintAndLog("Using SID for export.")
 	}
 
+}
+
+func markFlagsRequired(cmd *cobra.Command) {
+	// mandatory for all
+	cmd.MarkPersistentFlagRequired("source-db-type")
+	cmd.MarkPersistentFlagRequired("source-db-user")
+	cmd.MarkPersistentFlagRequired("source-db-password")
+
+	switch source.DBType {
+	case POSTGRESQL, ORACLE: // schema and database names are mandatory
+		cmd.MarkPersistentFlagRequired("source-db-name")
+		cmd.MarkPersistentFlagRequired("source-db-schema")
+	case MYSQL:
+		cmd.MarkPersistentFlagRequired("source-db-name")
+	}
 }

yb-voyager/cmd/exportData.go
@@ -89,7 +89,7 @@ func exportDataOffline() bool {
 
 	var tableList []string
 	if source.TableList != "" {
-		userTableList := strings.Split(source.TableList, ",")
+		userTableList := utils.CsvStringToSlice(source.TableList)
 
 		if source.DBType == POSTGRESQL {
 			// in postgres format should be schema.table, public is default and other parts of code assume schema.table format
@@ -177,14 +177,17 @@ func exportDataOnline() bool {
 	return false
 }
 
-func checkTableListFlag(tableListString string) {
-	tableList := strings.Split(tableListString, ",")
-	//TODO: update regexp once table name with double quotes are allowed/supported
-	tableNameRegex := regexp.MustCompile("[a-zA-Z0-9_.]+")
+func validateTableListFlag(tableListString string) {
+	if tableListString == "" {
+		return
+	}
 
+	tableList := utils.CsvStringToSlice(tableListString)
+	// TODO: update regexp once table name with double quotes are allowed/supported
+	tableNameRegex := regexp.MustCompile("[a-zA-Z0-9_.]+")
 	for _, table := range tableList {
 		if !tableNameRegex.MatchString(table) {
-			utils.ErrExit("invalid table name '%v' with --table-list flag", table)
+			utils.ErrExit("Error: Invalid table name '%v' provided wtih --table-list flag", table)
 		}
 	}
 }

yb-voyager/cmd/import.go
@@ -34,12 +34,13 @@ var importCmd = &cobra.Command{
 	Long:  ``,
 
 	PreRun: func(cmd *cobra.Command, args []string) {
-		validateImportFlags(cmd)
+		validateImportFlags()
+		sourceDBType = ExtractMetaInfo(exportDir).SourceDBType
+		markImportFlagsRequired(cmd)
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {
 		target.ImportMode = true
-		sourceDBType = ExtractMetaInfo(exportDir).SourceDBType
 		importSchema()
 		importData()
 	},
@@ -50,17 +51,13 @@ func init() {
 	registerCommonImportFlags(importCmd)
 }
 
-func validateImportFlags(cmd *cobra.Command) {
-	checkExportDirFlag()
+func validateImportFlags() {
+	validateExportDirFlag()
 	checkOrSetDefaultTargetSSLMode()
 	validateTargetPortRange()
 
-	if target.TableList != "" {
-		checkTableListFlag(target.TableList)
-	}
-	if target.Schema != YUGABYTEDB_DEFAULT_SCHEMA && ExtractMetaInfo(exportDir).SourceDBType == "postgresql" {
-		utils.ErrExit("ERROR: --target-db-schema flag is only valid for export from 'oracle' and 'mysql' db type")
-	}
+	validateTableListFlag(target.TableList)
+	validateTargetSchemaFlag()
 }
 
 func registerCommonImportFlags(cmd *cobra.Command) {
@@ -160,10 +157,26 @@ func validateTargetPortRange() {
 	}
 }
 
+func validateTargetSchemaFlag() {
+	if target.Schema == "" {
+		return
+	}
+	if target.Schema != YUGABYTEDB_DEFAULT_SCHEMA && sourceDBType == "postgresql" {
+		utils.ErrExit("Error: --target-db-schema flag is not valid for export from 'postgresql' db type")
+	}
+}
+
 func checkOrSetDefaultTargetSSLMode() {
 	if target.SSLMode == "" {
 		target.SSLMode = "prefer"
 	} else if target.SSLMode != "disable" && target.SSLMode != "prefer" && target.SSLMode != "require" && target.SSLMode != "verify-ca" && target.SSLMode != "verify-full" {
 		utils.ErrExit("Invalid sslmode %q. Required one of [disable, allow, prefer, require, verify-ca, verify-full]", target.SSLMode)
+	}
+}
+
+func markImportFlagsRequired(cmd *cobra.Command) {
+	switch sourceDBType {
+	case ORACLE, MYSQL:
+		cmd.MarkPersistentFlagRequired("target-db-schema")
 	}
 }

yb-voyager/cmd/importData.go
@@ -95,7 +95,7 @@ var importDataCmd = &cobra.Command{
 	Long:  `This command will import the data exported from the source database into YugabyteDB database.`,
 
 	PreRun: func(cmd *cobra.Command, args []string) {
-		validateImportFlags(cmd)
+		validateImportFlags()
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {

commit 3d479cf365d120146db9636834f67e2521dc08c5

Bug fix: start-clean behaviour for MySQL was not as expected, regression caused by afd16c96a0d0e03b2c4b0fc61ce2380c0f6e0459

yb-voyager/cmd/importSchema.go
@@ -67,13 +67,18 @@ func importSchema() {
 	payload.TargetDBVersion = targetDBVersion
 
 	var targetSchemas []string
-	if sourceDBType == "postgresql" { // in case of postgreSQL as source, there can be multiple schemas present in a database
+	switch sourceDBType {
+	case "postgresql": // in case of postgreSQL as source, there can be multiple schemas present in a database
 		source = srcdb.Source{DBType: sourceDBType}
 		targetSchemas = utils.GetObjectNameListFromReport(analyzeSchemaInternal(), "SCHEMA")
-	} else if sourceDBType == "oracle" { // ORACLE PACKAGEs are exported as SCHEMAs
+	case "oracle": // ORACLE PACKAGEs are exported as SCHEMAs
 		source = srcdb.Source{DBType: sourceDBType}
 		targetSchemas = append(targetSchemas, target.Schema)
 		targetSchemas = append(targetSchemas, utils.GetObjectNameListFromReport(analyzeSchemaInternal(), "PACKAGE")...)
+	case "mysql":
+		source = srcdb.Source{DBType: sourceDBType}
+		targetSchemas = append(targetSchemas, target.Schema)
+
 	}
 
 	utils.PrintAndLog("schemas to be present in target database: %v\n", targetSchemas)