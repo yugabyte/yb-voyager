4fa4236e1276165cf275053e2a6c03667a1f8589
Amit Jambure <ajambure@yugabyte.com>
2022-08-23 15:06:39 +0530

    Embed GIT_COMMIT_HASH and output it from `version` command.

--- yb-voyager/src/utils/version.go
+++ yb-voyager/src/utils/version.go
@@ -3,4 +3,15 @@ package utils
 const (
 	// This constant must be updated on every release.
 	YB_VOYAGER_VERSION = "v1.0.0-beta.1"
+
+	// @Refer: https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
+	GIT_COMMIT_HASH = "$Format:%H$"
 )
+
+func GitCommitHash() string {
+	if len(GIT_COMMIT_HASH) == 40 {
+		// Substitution has happened.
+		return GIT_COMMIT_HASH
+	}
+	return ""
+}
