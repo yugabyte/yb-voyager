4165f4801724ee12e3ed0e09c416fbe39a816554
Priyanshi Gupta <pgupta@yugabyte.com>
2022-09-27 09:32:06 +0530

    Enabled INDEXES_RENAMING param for MySQL - fixes (GH Issue #136) (#333)
    * Added INDEXES_RENAMING option for MySQL
    
    * Added INDEXES_RENAMING option for MySQL(#136)

--- yb-voyager/src/srcdb/source.go
+++ yb-voyager/src/srcdb/source.go
@@ -188,7 +188,9 @@ func (source *Source) PopulateOra2pgConfigFile(configFilePath string) {
 		} else if strings.HasPrefix(line, "PARALLEL_TABLES") {
 			lines[i] = "PARALLEL_TABLES " + strconv.Itoa(source.NumConnections)
 		} else if strings.HasPrefix(line, "PG_VERSION") {
-			lines[i] = "PG_VERSION " + strconv.Itoa(11) //TODO YugabyteDB compatible with postgres version ?
+			lines[i] = "PG_VERSION " + strconv.Itoa(11) 
+		} else if strings.HasPrefix(line, "INDEXES_RENAMING") && source.DBType == "mysql"{
+			lines[i] = "INDEXES_RENAMING 1"
 		}
 	}
 
