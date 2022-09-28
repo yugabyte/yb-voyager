6fa7ef4b4ff7173a69a27db27938064af3c7f6f3
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2022-09-28 11:49:15 +0000

    Added logging for source and target db version

--- yb-voyager/cmd/exportSchema.go
+++ yb-voyager/cmd/exportSchema.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -77,7 +77,7 @@ func exportSchema() {
 	source.DB().CheckRequiredToolsAreInstalled()
 	sourceDBVersion := source.DB().GetVersion()
 
-	fmt.Printf("%s version: %s\n", source.DBType, sourceDBVersion)
+	utils.PrintAndLog("%s version: %s\n", source.DBType, sourceDBVersion)
 
 	CreateMigrationProjectIfNotExists(source.DBType, exportDir)
 	source.DB().ExportSchema(exportDir)
