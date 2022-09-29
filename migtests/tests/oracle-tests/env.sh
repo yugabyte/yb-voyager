a413a7ffeb6ab56a048feb278e2b2c13921dcddd
Rahul Barigidad <rahulbarigidad9@gmail.com>
2022-09-29 08:28:56 -0400

    Initial tests commit

---
+++ migtests/tests/oracle-tests/env.sh
@@ -0,0 +1,6 @@
+export SOURCE_DB_TYPE="oracle"
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"ORCL"}
+export SOURCE_DB_PORT=${SOURCE_DB_PORT:-1521}
+export SOURCE_DB_USER="admin"
+export SOURCE_DB_HOST="rbarigidad-dms-oracle.cbtcvpszcgdq.us-west-2.rds.amazonaws.com"
+export SOURCE_DB_SCHEMA="admin"
---
+++ migtests/tests/oracle-tests/orascript.sql
@@ -0,0 +1,9 @@
+drop table RAW_TYPE;
+drop table NUMERIC_SIZE;
+drop table NUMERIC_TYPES_NUMBER;
+drop table TEST;
+drop table SUPPLIERS;
+drop table COMMENT_TEST;
+drop table FREEZE;
+create table test(id int);
+insert into test values(1);
