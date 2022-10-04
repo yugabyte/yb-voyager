34bd1204a78a2a8350f74ed1c9f0837e96242486
Amit Jambure <98899964+amit-yb@users.noreply.github.com>
2022-10-04 11:00:55 +0530

    Update version to v0.80 . (#397)

--- yb-voyager/src/utils/version.go
+++ yb-voyager/src/utils/version.go
@@ -2,7 +2,7 @@ package utils
 
 const (
 	// This constant must be updated on every release.
-	YB_VOYAGER_VERSION = "v1.0.0-beta.2"
+	YB_VOYAGER_VERSION = "v0.80"
 
 	// @Refer: https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
 	GIT_COMMIT_HASH = "$Format:%H$"
