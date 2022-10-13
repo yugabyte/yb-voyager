5ca3ed6150c5aec7a3a8ba51eccfd40c3313d7d3
Chetan <chetan.naidu@ip-192-168-1-5.us-west-2.compute.internal>
2022-10-13 22:55:32 +0530

    Committing changes as per main

--- migtests/scripts/yugabytedb/env.sh
+++ migtests/scripts/yugabytedb/env.sh
@@ -1,10 +1,9 @@
-export TARGET_DB_HOST=${TARGET_DB_HOST:-"10.9.200.131"}
+export TARGET_DB_HOST=${TARGET_DB_HOST:-"127.0.0.1"}
 export TARGET_DB_PORT=${TARGET_DB_PORT:-5433}
-export TARGET_DB_USER=${TARGET_DB_USER:-"ybvoyager"}
-export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-"password"}
-export TARGET_DB_NAME="partition_tests"
-# # The PG driver, used to connect to YB, is case-sensitive about database name.
-# if [ "${TARGET_DB_NAME}" == "" ]
-# then
-# 	export TARGET_DB_NAME=`echo ${SOURCE_DB_NAME} | tr [A-Z] [a-z]`
-# fi
+export TARGET_DB_USER=${TARGET_DB_USER:-"yugabyte"}
+export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-''}
+# The PG driver, used to connect to YB, is case-sensitive about database name.
+if [ "${TARGET_DB_NAME}" == "" ]
+then
+	export TARGET_DB_NAME=`echo ${SOURCE_DB_NAME} | tr [A-Z] [a-z]`
+fi
