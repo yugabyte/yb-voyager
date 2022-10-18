29320d805054dc4d2f488ed8d7057e4499ab36d4
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2022-10-18 00:02:51 +0000

    Provide datatype mapping list to ora2pg.conf file

--- yb-voyager/src/srcdb/source.go
+++ yb-voyager/src/srcdb/source.go
@@ -196,6 +196,8 @@ func (source *Source) PopulateOra2pgConfigFile(configFilePath string) {
 			if source.UseOrafce && strings.EqualFold(source.DBType, "oracle") {
 				lines[i] = "USE_ORAFCE 1"
 			}
+		} else if strings.HasPrefix(line, "#DATA_TYPE") || strings.HasPrefix(line, "DATA_TYPE") {
+			lines[i] = "DATA_TYPE      VARCHAR2:varchar,NVARCHAR2:varchar,DATE:timestamp,LONG:text,LONG RAW:bytea,CLOB:text,NCLOB:text,BLOB:bytea,BFILE:bytea,RAW(16):uuid,RAW(32):uuid,RAW:bytea,UROWID:oid,ROWID:oid,FLOAT:double precision,DEC:decimal,DECIMAL:decimal,DOUBLE PRECISION:double precision,INT:integer,INTEGER:integer,REAL:real,SMALLINT:smallint,BINARY_FLOAT:double precision,BINARY_DOUBLE:double precision,TIMESTAMP:timestamp,XMLTYPE:xml,BINARY_INTEGER:integer,PLS_INTEGER:integer,TIMESTAMP WITH TIME ZONE:timestamp with time zone,TIMESTAMP WITH LOCAL TIME ZONE:timestamp with time zone"
 		}
 	}
 
