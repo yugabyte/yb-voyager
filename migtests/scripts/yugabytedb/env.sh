9cb4492698fde83839c7242ddf3199f895af2d05
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-21 21:30:09 +0530

    Changes to env files

--- migtests/scripts/yugabytedb/env.sh
+++ migtests/scripts/yugabytedb/env.sh
@@ -1,8 +1,7 @@
-export TARGET_DB_HOST=${TARGET_DB_HOST:-"10.9.200.131"}
+export TARGET_DB_HOST=${TARGET_DB_HOST:-"127.0.0.1"}
 export TARGET_DB_PORT=${TARGET_DB_PORT:-5433}
-export TARGET_DB_USER=${TARGET_DB_USER:-"ybvoyager"}
-export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-"password"}
-export TARGET_DB_NAME=${TARGET_DB_NAME:-"postgres"}
+export TARGET_DB_USER=${TARGET_DB_USER:-"yugabyte"}
+export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-''}
 # The PG driver, used to connect to YB, is case-sensitive about database name.
 if [ "${TARGET_DB_NAME}" == "" ]
 then
