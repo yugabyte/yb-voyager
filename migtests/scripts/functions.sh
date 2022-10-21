9cb9b1882ea6d77b2d9d3dcf3a08d4b6c181aa00
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-21 11:56:44 +0530

    Committing changes for ora data types and partition tests

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -54,6 +54,13 @@ run_mysql() {
 	mysql -u ${SOURCE_DB_USER} -p${SOURCE_DB_PASSWORD} -h ${SOURCE_DB_HOST} -P ${SOURCE_DB_PORT} -D ${db_name} -e "${sql}"
 }
 
+run_sqlplus() {
+	db_name=$1
+	sql=$2
+	conn_string="${SOURCE_DB_USER}/${SOURCE_DB_PASSWORD}@(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = ${SOURCE_DB_HOST}) (PORT = ${SOURCE_DB_PORT}))(CONNECT_DATA = (SID = ${db_name})))"
+	echo exit | sqlplus ${conn_string} @${sql}
+}
+
 export_schema() {
 	args="--export-dir ${EXPORT_DIR}
 		--source-db-type ${SOURCE_DB_TYPE}
