3b2c7cb90fbd44f8784ec8200214e7359817368d
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-27 16:11:08 +0530

    Committing changes

---
+++ migtests/tests/oracle-tests/ora-views-test/row_count_source_db.sql
@@ -0,0 +1,7 @@
+SELECT count(*) FROM customers;
+SELECT count(*) FROM stores;
+SELECT count(*) FROM products;
+SELECT count(*) FROM orders;
+SELECT count(*) FROM shipments;
+SELECT count(*) FROM order_items;
+SELECT count(*) FROM inventory;
