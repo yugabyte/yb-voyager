acf67d09e1b9515d9d8e66bdbb8938559355693b
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-27 17:12:32 +0530

    Committing changes

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -57,7 +57,7 @@ run_mysql() {
 run_sqlplus() {
 	db_name=$1
 	sql=$2
-	conn_string="${SOURCE_DB_USER}/${SOURCE_DB_PASSWORD}@(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = ${SOURCE_DB_HOST}) (PORT = ${SOURCE_DB_PORT}))(CONNECT_DATA = (SID = ${db_name})))"
+	conn_string=${SOURCE_DB_USER}/${SOURCE_DB_PASSWORD}@(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = ${SOURCE_DB_HOST}) (PORT = ${SOURCE_DB_PORT}))(CONNECT_DATA = (SID = ${db_name})))
 	echo exit | sqlplus ${conn_string} @${sql}
 }
 
