b5cea0212b6a397fa0f8f3d3034f5d5381b93bb9
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-28 12:54:05 +0530

    Committing changes for validations:

--- migtests/tests/oracle-tests/ora-datatypes-test/validate.py
+++ migtests/tests/oracle-tests/ora-datatypes-test/validate.py
@@ -9,26 +9,17 @@ def main():
 
 
 def file_import_done_checks(tgt, tag):
-	row_count_numeric = tgt.get_row_count("numeric_types_number")
-    row_count_float = tgt.get_row_count("numeric_types_float")
-    row_count_binary_float = tgt.get_row_count("numeric_types_binary_float")
-    row_count_numeric_binary_double = tgt.get_row_count("numeric_types_binary_double")
-    row_count_numeric_types = tgt.get_row_count("numeric_types")
-    row_count_date_time = tgt.get_row_count("date_time_types")
-    row_count_interval_types = tgt.get_row_count("interval_types")
-    row_count_char_types = tgt.get_row_count("CHAR_TYPES")
-    row_count_long_type = tgt.get_row_count("LONG_TYPE")
-    row_count_raw_type = tgt.get_row_count("RAW_TYPE")
-    assert row_count_numeric == 5
-    assert row_count_float == 5
-    assert row_count_binary_float == 5
-    assert row_count_numeric_binary_double == 5
-    assert row_count_numeric_types == 8
-    assert row_count_date_time == 10
-    assert row_count_interval_types == 4
-    assert row_count_char_types == 5
-    assert row_count_long_type == 5
-    assert row_count_raw_type == 6
+    tables = tgt.row_count_of_all_tables()
+    assert tables.get("numeric_types_number") == 4
+    assert tables.get("numeric_types_float") == 5
+    assert tables.get("numeric_types_binary_float") == 5
+    assert tables.get("numeric_types_binary_double") == 5
+    assert tables.get("numeric_types") == 8
+    assert tables.get("date_time_types") == 10
+    assert tables.get("interval_types") == 4
+    assert tables.get("CHAR_TYPES") == 6
+    assert tables.get("LONG_TYPE") == 4
+    assert tables.get("RAW_TYPE") == 6
 
 if __name__ == "__main__":
 	main()
