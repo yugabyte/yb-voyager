6054424935d0093299db19aa74f5f9040a65b584
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-28 19:19:29 +0530

    Fixing the logic to pick the right file

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -140,5 +140,5 @@ import_data_file() {
 
 ora_delete_schema_contents() {
 	db_name=$1
-	run_sqlplus ${db_name} migtests/scripts/oracle/schema_deletion
+	run_sqlplus ${db_name} oracle/schema_deletion
 }
