41523866e518f7cd0af07e63f2833299a3e4a1c5
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-10-28 13:32:48 +0530

    Local commit

---
+++ migtests/tests/oracle-tests/ora-views-test/dml/index.sql
@@ -0,0 +1,43 @@
+SET FEEDBACK 1
+SET NUMWIDTH 10
+SET LINESIZE 80
+SET TRIMSPOOL ON
+SET TAB OFF
+SET PAGESIZE 100
+SET ECHO OFF 
+
+CREATE INDEX emp_department_ix
+       ON employees (department_id);
+
+CREATE INDEX emp_job_ix
+       ON employees (job_id);
+
+CREATE INDEX emp_manager_ix
+       ON employees (manager_id);
+
+CREATE INDEX emp_name_ix
+       ON employees (last_name, first_name);
+
+CREATE INDEX dept_location_ix
+       ON departments (location_id);
+
+CREATE INDEX jhist_job_ix
+       ON job_history (job_id);
+
+CREATE INDEX jhist_employee_ix
+       ON job_history (employee_id);
+
+CREATE INDEX jhist_department_ix
+       ON job_history (department_id);
+
+CREATE INDEX loc_city_ix
+       ON locations (city);
+
+CREATE INDEX loc_state_province_ix	
+       ON locations (state_province);
+
+CREATE INDEX loc_country_ix
+       ON locations (country_id);
+
+COMMIT;
+
--- migtests/tests/oracle-tests/ora-views-test/dml/remove_blob.sql
+++
@@ -1,4 +0,0 @@
-drop table products cascade constraints;
-drop view customer_order_products;
-drop view product_reviews;
-drop view product_orders;
