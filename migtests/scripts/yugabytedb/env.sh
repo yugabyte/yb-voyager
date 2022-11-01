30101d3a1c8495cca01c471b5b8c93ab6e29b49d
Cloud User <centos@ip-10-9-127-115.us-west-2.compute.internal>
2022-11-01 06:49:08 -0400

    Added fixes

--- migtests/scripts/yugabytedb/env.sh
+++ migtests/scripts/yugabytedb/env.sh
@@ -1,7 +1,7 @@
 export TARGET_DB_HOST=${TARGET_DB_HOST:-"127.0.0.1"}
 export TARGET_DB_PORT=${TARGET_DB_PORT:-5433}
-export TARGET_DB_USER=${TARGET_DB_USER:-"yugabyte"}
-export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-''}
+export TARGET_DB_USER=${TARGET_DB_USER:-"ybvoyager"}
+export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-'password'}
 # The PG driver, used to connect to YB, is case-sensitive about database name.
 if [ "${TARGET_DB_NAME}" == "" ]
 then
