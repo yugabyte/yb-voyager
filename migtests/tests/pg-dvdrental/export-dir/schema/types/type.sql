4456d7e5d55a625e906f1eea2d408b04bf68a445
Cloud User <centos@ip-10-9-127-115.us-west-2.compute.internal>
2022-11-01 03:27:37 -0400

    Added fixes

---
+++ migtests/tests/pg-dvdrental/export-dir/schema/types/type.sql
@@ -0,0 +1,22 @@
+-- setting variables for current session
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+CREATE TYPE public.mpaa_rating AS ENUM (
+    'G',
+    'PG',
+    'PG-13',
+    'R',
+    'NC-17'
+);
+
+
