d9fd2d60b00075c739d58594d49cc3c7f99686ab
Chetan <chetan.naidu@ip-192-168-1-6.us-west-2.compute.internal>
2022-11-03 15:15:34 +0530

    Commititng changes locally

--- migtests/tests/oracle-tests/ora-sanity-test/row_count_source_db.sql
+++
@@ -1,7 +0,0 @@
-SELECT count(*) FROM customers;
-SELECT count(*) FROM stores;
-SELECT count(*) FROM products;
-SELECT count(*) FROM orders;
-SELECT count(*) FROM shipments;
-SELECT count(*) FROM order_items;
-SELECT count(*) FROM inventory;
