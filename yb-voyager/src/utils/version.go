54c7cd5378b3b21ceb4132944bf28053bd6cd75d
Amit Jambure <ajambure@yugabyte.com>
2022-11-04 09:08:27 +0000

    Update version to 0.81 .

--- yb-voyager/src/utils/version.go
+++ yb-voyager/src/utils/version.go
@@ -2,7 +2,7 @@ package utils
 
 const (
 	// This constant must be updated on every release.
-	YB_VOYAGER_VERSION = "main"
+	YB_VOYAGER_VERSION = "0.81"
 
 	// @Refer: https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
 	GIT_COMMIT_HASH = "$Format:%H$"
