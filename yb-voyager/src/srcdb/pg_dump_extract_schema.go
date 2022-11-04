yugabyte/yb-voyager

commit 938d5f3d0d91e1763b12694cb3763242f3363492

[#531] Bug fix for PostgreSQL Export Schema having SQL statements with comments in it (#532)
- Also, simplified the pg_dump's schema.sql file parsing logic

yb-voyager/src/srcdb/pg_dump_extract_schema.go
@@ -1,8 +1,10 @@
 package srcdb
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
@@ -14,7 +16,7 @@ import (
 
 func pgdumpExtractSchema(source *Source, connectionUri string, exportDir string) {
 	fmt.Printf("exporting the schema %10s", "")
-	go utils.Wait("done\n", "error\n")
+	go utils.Wait("done\n", "")
 
 	pgDumpPath, err := GetAbsPathOfPGCommand("pg_dump")
 	if err != nil {
@@ -45,35 +47,54 @@ func pgdumpExtractSchema(source *Source, connectionUri string, exportDir string)
 	}
 
 	//Parsing the single file to generate multiple database object files
-	parseSchemaFile(exportDir)
+	returnCode := parseSchemaFile(exportDir)
 
 	log.Info("Export of schema completed.")
-	utils.WaitChannel <- 0
+	utils.WaitChannel <- returnCode
 	<-utils.WaitChannel
 }
 
-// NOTE: This is for case when --schema-only option is provided with pg_dump[Data shouldn't be there]
-func parseSchemaFile(exportDir string) {
-	log.Info("Begun parsing the schema file.")
-	schemaFilePath := filepath.Join(exportDir, "temp", "schema.sql")
-	schemaDirPath := filepath.Join(exportDir, "schema")
-	schemaFileData, err := ioutil.ReadFile(schemaFilePath)
+func readSchemaFile(path string) []string {
+	file, err := os.Open(path)
 	if err != nil {
-		utils.ErrExit("Failed to read file %q: %v", schemaFilePath, err)
+		utils.ErrExit("error in opening schema file %s: %v", path, err)
+	}
+	defer file.Close()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !shouldSkipLine(line) {
+			lines = append(lines, line)
+		}
 	}
 
-	schemaFileLines := strings.Split(string(schemaFileData), "\n")
-	numLines := len(schemaFileLines)
+	if scanner.Err() != nil {
+		utils.ErrExit("error in reading schema file %s: %v", path, scanner.Err())
+	}
 
-	sessionVariableStartPattern := regexp.MustCompile("-- Dumped by pg_dump.*")
+	return lines
+}
 
-	// For example: -- Name: address address_city_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
-	sqlTypeInfoCommentPattern := regexp.MustCompile("--.*Type:.*")
+// For example: -- Name: address address_city_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+var sqlInfoCommentRegex = regexp.MustCompile("-- Name:.*; Type:.*; Schema: .*")
+
+// NOTE: This is for case when --schema-only option is provided with pg_dump[Data shouldn't be there]
+func parseSchemaFile(exportDir string) int {
+	log.Info("Begun parsing the schema file.")
+	schemaFilePath := filepath.Join(exportDir, "temp", "schema.sql")
+
+	lines := readSchemaFile(schemaFilePath)
+	var delimiterIndexes []int
+	for i, line := range lines {
+		if isDelimiterLine(line) {
+			delimiterIndexes = append(delimiterIndexes, i)
+		}
+	}
 
 	// map to store the sql statements for each db object type
 	// map's key are based on the elements of 'utils.postgresSchemaObjectList' array
 	objSqlStmts := make(map[string]*strings.Builder)
-
 	// initialize the map
 	pgObjList := utils.GetSchemaObjectList("postgresql")
 	for _, objType := range pgObjList {
@@ -81,40 +102,32 @@ func parseSchemaFile(exportDir string) {
 	}
 
 	var uncategorizedSqls, setSessionVariables strings.Builder
-
-	var isPossibleFlag bool = true
-	for i := 0; i < numLines; i++ {
-		if sqlTypeInfoCommentPattern.MatchString(schemaFileLines[i]) {
-			sqlType := extractSqlTypeFromSqlInfoComment(schemaFileLines[i])
-
-			i += 2 // jumping to start of sql statement
-			sqlStatement := extractSqlStatements(schemaFileLines, &i)
-
-			// TODO: TABLESPACE
+	for i := 0; i < len(delimiterIndexes)-1; i++ {
+		stmts := strings.Join(lines[delimiterIndexes[i]+1:delimiterIndexes[i+1]], "\n") + "\n\n\n"
+		if i == 0 {
+			// Deal with SET statments
+			setSessionVariables.WriteString("-- setting variables for current session\n")
+			setSessionVariables.WriteString(stmts)
+		} else {
+			delimiterLine := lines[delimiterIndexes[i]]
+			sqlType := extractSqlTypeFromComment(delimiterLine)
 			switch sqlType {
 			case "SCHEMA", "TYPE", "DOMAIN", "SEQUENCE", "INDEX", "RULE", "FUNCTION",
 				"AGGREGATE", "PROCEDURE", "VIEW", "TRIGGER", "EXTENSION", "COMMENT":
-				objSqlStmts[sqlType].WriteString(sqlStatement)
+				objSqlStmts[sqlType].WriteString(stmts)
 			case "TABLE", "DEFAULT", "CONSTRAINT", "FK CONSTRAINT", "TABLE ATTACH":
-				objSqlStmts["TABLE"].WriteString(sqlStatement)
+				objSqlStmts["TABLE"].WriteString(stmts)
 			case "MATERIALIZED VIEW":
-				objSqlStmts["MVIEW"].WriteString(sqlStatement)
+				objSqlStmts["MVIEW"].WriteString(stmts)
 			case "COLLATION":
-				objSqlStmts["COLLATION"].WriteString(sqlStatement)
+				objSqlStmts["COLLATION"].WriteString(stmts)
 			default:
-				uncategorizedSqls.WriteString(sqlStatement)
+				uncategorizedSqls.WriteString(stmts)
 			}
-		} else if isPossibleFlag && sessionVariableStartPattern.MatchString(schemaFileLines[i]) {
-			i++
-
-			setSessionVariables.WriteString("-- setting variables for current session")
-			sqlStatement := extractSqlStatements(schemaFileLines, &i)
-			setSessionVariables.WriteString(sqlStatement)
-
-			isPossibleFlag = false
 		}
 	}
 
+	schemaDirPath := filepath.Join(exportDir, "schema")
 	for objType, sqlStmts := range objSqlStmts {
 		if sqlStmts.Len() == 0 { // create .sql file only if there are DDLs
 			continue
@@ -124,55 +137,42 @@ func parseSchemaFile(exportDir string) {
 
 		err := ioutil.WriteFile(filePath, dataBytes, 0644)
 		if err != nil {
-			utils.ErrExit("Failed to create sql file for for %q: %v", objType, err)
+			utils.ErrExit("Failed to create sql file for %q: %v", objType, err)
 		}
 	}
 
 	if uncategorizedSqls.Len() > 0 {
 		filePath := filepath.Join(schemaDirPath, "uncategorized.sql")
 		// TODO: add it to the analyze-schema report in case of postgresql
-		utils.PrintAndLog("Some uncategorized sql statements are present in %q, Needs to review and import them manually!!", filePath)
+		utils.PrintAndLog("\nSome uncategorized sql statements are present in %q, Needs to review and import them manually!!", filePath)
 		ioutil.WriteFile(filePath, []byte(setSessionVariables.String()+uncategorizedSqls.String()), 0644)
+		return 1
 	}
+	return 0
 }
 
-func extractSqlTypeFromSqlInfoComment(sqlInfoComment string) string {
-	sqlInfoCommentSlice := strings.Split(sqlInfoComment, "; ")
-	var sqlType strings.Builder
+// Example sqlInfoComment: -- Name: address address_city_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+func extractSqlTypeFromComment(comment string) string {
+	sqlInfoCommentSlice := strings.Split(comment, ";")
 	for _, info := range sqlInfoCommentSlice {
+		info = strings.Trim(info, " ")
 		if info[:4] == "Type" {
-			typeInfo := strings.Split(info, ": ")
-			typeInfoValue := typeInfo[1]
-
-			for i := 0; i < len(typeInfoValue) && typeInfoValue[i] != ';'; i++ {
-				sqlType.WriteByte(typeInfoValue[i])
-			}
+			return strings.Trim(strings.Split(info, ":")[1], " ")
 		}
 	}
 
-	return sqlType.String()
-}
-
-func extractSqlStatements(schemaFileLines []string, index *int) string {
-	var sqlStatement strings.Builder
-	for (*index) < len(schemaFileLines) {
-		if isSqlComment(schemaFileLines[(*index)]) {
-			break
-		} else if shouldSkipLine(schemaFileLines[(*index)]) {
-			(*index)++
-			continue
-		} else {
-			sqlStatement.WriteString(schemaFileLines[(*index)] + "\n")
-			(*index)++
-		}
-	}
-	return sqlStatement.String()
-}
-
-func isSqlComment(line string) bool {
-	return len(line) >= 2 && line[:2] == "--"
+	utils.ErrExit("Unable to extract sqlType from comment: %s", comment)
+	return "" // unreachable
 }
 
 func shouldSkipLine(line string) bool {
-	return strings.HasPrefix(line, "SET default_table_access_method")
+	return strings.HasPrefix(line, "SET default_table_access_method") ||
+		strings.Compare(line, "--") == 0 || len(line) == 0 ||
+		strings.EqualFold(line, "-- PostgreSQL database dump complete") ||
+		strings.EqualFold(line, "-- PostgreSQL database dump") ||
+		strings.HasPrefix(line, "-- Dumped from database version")
+}
+
+func isDelimiterLine(line string) bool {
+	return strings.HasPrefix(line, "-- Dumped by pg_dump version") || sqlInfoCommentRegex.MatchString(line)
 }

commit 801ad0216c0b4eaf00df994290085eeb4602d431

Regression in 938d5f3d0d91e1763b12694cb3763242f3363492: Not exporting the last SQL statement from temp/schema.sql file

yb-voyager/src/srcdb/pg_dump_extract_schema.go
@@ -102,8 +102,14 @@ func parseSchemaFile(exportDir string) int {
 	}
 
 	var uncategorizedSqls, setSessionVariables strings.Builder
-	for i := 0; i < len(delimiterIndexes)-1; i++ {
-		stmts := strings.Join(lines[delimiterIndexes[i]+1:delimiterIndexes[i+1]], "\n") + "\n\n\n"
+	for i := 0; i < len(delimiterIndexes); i++ {
+		var stmts string
+		if i == len(delimiterIndexes)-1 {
+			stmts = strings.Join(lines[delimiterIndexes[i]+1:], "\n") + "\n\n\n"
+		} else {
+			stmts = strings.Join(lines[delimiterIndexes[i]+1:delimiterIndexes[i+1]], "\n") + "\n\n\n"
+		}
+
 		if i == 0 {
 			// Deal with SET statments
 			setSessionVariables.WriteString("-- setting variables for current session\n")