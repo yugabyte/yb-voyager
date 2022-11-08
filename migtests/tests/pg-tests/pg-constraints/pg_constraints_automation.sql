0422916337b0be23c35986f359415ddf0c48cf43
Priyanshi Gupta <pgupta@yugabyte.com>
2022-11-08 14:16:21 +0000

    added automation test with validations for postgresql - datatypes, sequences, and constraints

---
+++ migtests/tests/pg-tests/pg-constraints/pg_constraints_automation.sql
@@ -0,0 +1,99 @@
+drop table if exists not_null_check;
+
+CREATE TABLE not_null_check (
+id serial primary key,
+	first_name VARCHAR(50) not null,
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into not_null_check (first_name, last_name, email, gender, ip_address) values ('Modestine', 'MacMeeking', 'mmacmeeking0@zimbio.com', 'Female', '208.44.58.185');
+insert into not_null_check (first_name, last_name, email, gender, ip_address) values ('Genna', 'Kaysor', 'gkaysor1@hibu.com', 'Female', '202.48.51.58');
+insert into not_null_check (first_name, last_name, email, gender, ip_address) values ('Tess', 'Wesker', 'twesker2@scientificamerican.com', 'Female', '177.153.32.186');
+insert into not_null_check (first_name, last_name, email, gender, ip_address) values ('Magnum', 'Danzelman', 'mdanzelman3@storify.com', 'Bigender', '192.200.33.56');
+insert into not_null_check (first_name, last_name, email, gender, ip_address) values ('Mitzi', 'Pidwell', 'mpidwell4@shutterfly.com', 'Female', '216.4.250.71');
+insert into not_null_check (first_name, last_name, email, gender, ip_address) values ('Milzie', 'Rohlfing', 'mrohlfing5@java.com', 'Female', '230.101.87.42');
+
+
+drop table if exists unique_test;
+
+CREATE TABLE unique_test (
+id serial primary key,
+	first_name VARCHAR(50) unique,
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into unique_test (first_name, last_name, email, gender, ip_address) values ('Genna', 'Kaysor', 'gkaysor1@hibu.com', 'Female', '202.48.51.58');
+insert into unique_test (first_name, last_name, email, gender, ip_address) values ('Tess', 'Wesker', 'twesker2@scientificamerican.com', 'Female', '177.153.32.186');
+insert into unique_test (first_name, last_name, email, gender, ip_address) values ('Magnum', 'Danzelman', 'mdanzelman3@storify.com', 'Bigender', '192.200.33.56');
+insert into unique_test (first_name, last_name, email, gender, ip_address) values ('Mitzi', 'Pidwell', 'mpidwell4@shutterfly.com', 'Female', '216.4.250.71');
+insert into unique_test (first_name, last_name, email, gender, ip_address) values ('Milzie', 'Rohlfing', 'mrohlfing5@java.com', 'Female', '230.101.87.42');
+
+
+
+drop table if exists check_test;
+
+CREATE TABLE check_test (
+    ID serial primary key,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255),
+    Age int,
+    CHECK (Age>=18)
+);
+insert into check_test (first_name, last_name, age) values ('Modestine', 'MacMeeking', 20);
+insert into check_test (first_name, last_name, age) values ('Genna', 'Kaysor', 50);
+insert into check_test (first_name, last_name, age) values ('Tess', 'Wesker', 56);
+insert into check_test (first_name, last_name, age) values ('Magnum', 'Danzelman', 89);
+insert into check_test (first_name, last_name, age) values ('Mitzi', 'Pidwell', 34);
+insert into check_test (first_name, last_name, age) values ('Milzie', 'Rohlfing', 70);
+
+
+
+drop table if exists default_test;
+
+CREATE TABLE default_test (
+    ID serial primary key,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255),
+    Age int default 18
+);
+insert into default_test (first_name, last_name, age) values ('Modestine', 'MacMeeking', 20);
+insert into default_test (first_name, last_name, age) values ('Genna', 'Kaysor', 50);
+insert into default_test (first_name, last_name, age) values ('Tess', 'Wesker', 56);
+insert into default_test (first_name, last_name, age) values ('Magnum', 'Danzelman', 89);
+insert into default_test (first_name, last_name, age) values ('Mitzi', 'Pidwell', 34);
+insert into default_test (first_name, last_name, age) values ('Milzie', 'Rohlfing', 70);
+insert into default_test (first_name, last_name) values ('Milzie', 'Rohlfing');
+
+
+drop table if exists foreign_test;
+drop table if exists primary_test;
+
+CREATE TABLE primary_test (
+    ID serial primary key,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255),
+    Age int
+);
+insert into primary_test (first_name, last_name, age) values ('Modestine', 'MacMeeking', 20);
+insert into primary_test (first_name, last_name, age) values ('Genna', 'Kaysor', 50);
+insert into primary_test (first_name, last_name, age) values ('Tess', 'Wesker', 56);
+insert into primary_test (first_name, last_name, age) values ('Magnum', 'Danzelman', 89);
+insert into primary_test (first_name, last_name, age) values ('Mitzi', 'Pidwell', 34);
+insert into primary_test (first_name, last_name, age) values ('Milzie', 'Rohlfing', 70);
+
+CREATE TABLE foreign_test (
+    ID int NOT NULL,
+    ONumber int NOT NULL,
+    PID int,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (PID) REFERENCES primary_test(ID)
+);
+
+insert into foreign_test values (1,3,4);
+insert into foreign_test values (2,2,3);
+insert into foreign_test values (3,2,1);
+insert into foreign_test values (4,1,2);
---
+++ migtests/tests/pg-tests/pg-datatypes/pg_datatypes_automation.sql
@@ -0,0 +1,54 @@
+-- datatypes 
+
+drop table if exists num_types;
+
+create table num_types(v1 smallint, v2 integer,v3 bigint,v4 decimal(6,3),v5 numeric, v6 money);
+
+insert into num_types values(182,34453,654385451,453.23,22334.542,120.50);
+insert into num_types values(32762,-3415123,654312385451,999.999,-22334.542,10.4);
+insert into num_types values(-323,53,-90654385451,-459.230,9992334.54290,-12000500.50);
+
+\d num_types
+
+select * from num_types;
+
+drop type if exists week cascade;
+
+CREATE TYPE week AS ENUM ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
+
+drop table if exists datatypes1;
+
+create table datatypes1(bool_type boolean,char_type1 CHAR (1),varchar_type VARCHAR(100),byte_type bytea, enum_type week);
+
+insert into datatypes1 values(true,'z','this is a string','01010','Mon');
+insert into datatypes1 values(false,'5','Lorem ipsum dolor sit amet, consectetuer adipiscing elit.','-abcd','Fri');
+insert into datatypes1 values(true,'z','this is a string','4458','Sun');
+
+\d datatypes1
+
+select * from datatypes1;
+
+drop table if exists datetime_type;
+
+create table datetime_type(v1 date, v2 time, v3 timestamp,v4 TIMESTAMP without TIME ZONE default CURRENT_TIMESTAMP(0));
+
+insert into datetime_type values('1996-12-02', '09:00:00',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP(0));
+insert into datetime_type values('2006-12-02', '12:00:50','2022-11-01 15:55:58.091241',CURRENT_TIMESTAMP(0));
+insert into datetime_type values('1992-01-23', null,current_timestamp,'2022-11-01 15:58:02');
+
+\d datetime_type
+
+select * from datetime_type;
+
+drop table if exists datatypes2;
+
+create table datatypes2(v1 json, v2 BIT(10), v3 int ARRAY[4], v4 text[][]);
+
+insert into datatypes2 values ('{"key1": "value1", "key2": "value2"}',B'10'::bit(10),'{20000, 14600, 23500, 13250}', '{{“FD”, “MF”}, {“FD”, “Property”}}');
+insert into datatypes2 values ('["a","b","c",1,2,3]',B'100011'::bit(10),'{20000, 14600, 23500, 13250}', '{{“FD”, “MF”}, {"act","two"}}');
+insert into datatypes2 values (null,B'1'::bit(10),null, '{{“FD”}, {"act"}}');
+
+\d datatypes2
+
+select * from datatypes2;
+
