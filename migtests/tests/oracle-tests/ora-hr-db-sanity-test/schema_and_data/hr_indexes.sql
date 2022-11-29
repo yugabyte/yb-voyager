540a8a665ac7f344265d43291b7e66fec907e28b
Ubuntu <ubuntu@ip-10-9-12-234.us-west-2.compute.internal>
2022-11-29 14:01:40 +0000

    Committing hr schema as a sanity test

---
+++ migtests/tests/oracle-tests/ora-hr-db-sanity-test/schema_and_data/hr_indexes.sql
@@ -0,0 +1,35 @@
+CREATE INDEX emp_department_ix
+       ON employees (department_id);
+
+CREATE INDEX emp_job_ix
+       ON employees (job_id);
+
+CREATE INDEX emp_manager_ix
+       ON employees (manager_id);
+
+CREATE INDEX emp_name_ix
+       ON employees (last_name, first_name);
+
+CREATE INDEX dept_location_ix
+       ON departments (location_id);
+
+CREATE INDEX jhist_job_ix
+       ON job_history (job_id);
+
+CREATE INDEX jhist_employee_ix
+       ON job_history (employee_id);
+
+CREATE INDEX jhist_department_ix
+       ON job_history (department_id);
+
+CREATE INDEX loc_city_ix
+       ON locations (city);
+
+CREATE INDEX loc_state_province_ix	
+       ON locations (state_province);
+
+CREATE INDEX loc_country_ix
+       ON locations (country_id);
+
+COMMIT;
+
