867ca5ff160223c8318431a1f99e792d783a4357
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2022-12-08 08:37:59 +0000

    [PostgreSQL] Moved import of RULE after TABLE and VIEW - RULE can be defined on tables and views only - Added automation tests for rules in 'pg-tests/pg-views-and-rules' test case

--- migtests/tests/pg-tests/pg-views-and-rules/env.sh
+++ migtests/tests/pg-tests/pg-views-and-rules/env.sh
@@ -1,4 +1,4 @@
 export SOURCE_DB_TYPE="postgresql"
-export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_views"}
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_views_and_rules"}
 export SOURCE_DB_SCHEMA="test_views"
 export TARGET_DB_SCHEMA="TEST_Views"
