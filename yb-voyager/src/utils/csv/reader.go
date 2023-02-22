714b1087d7b92b226fce9e8ea5bff7cf4e9d4d74
Amit Jambure <ajambure@yugabyte.com>
2023-02-22 13:10:07 +0000

    Use io.Reader instead of mmap in the CSV parser.

--- yb-voyager/src/utils/csv/reader.go
+++ yb-voyager/src/utils/csv/reader.go
@@ -1,21 +1,22 @@
 package csv
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
-
-	"github.com/edsrzf/mmap-go"
 )
 
 type Reader struct {
 	QuoteChar  byte
 	EscapeChar byte
 
-	fileName string
-	file     *os.File
-	mmap     mmap.MMap
-	buf      []byte
+	fileName     string
+	file         *os.File
+	buf          []byte
+	remainingBuf []byte
+	pendingBytes []byte
+	eof          bool
 }
 
 func Open(fileName string) (*Reader, error) {
@@ -24,60 +25,95 @@ func Open(fileName string) (*Reader, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error opening file %s: %v", fileName, err)
 	}
-	mmap_, err := mmap.Map(f, mmap.RDONLY, 0)
-	if err != nil {
-		return nil, fmt.Errorf("error creating mmap for file %s: %v", fileName, err)
-	}
-	r := &Reader{QuoteChar: '"', EscapeChar: '"', fileName: fileName, file: f, mmap: mmap_, buf: []byte(mmap_)}
+	buf := make([]byte, 1024*1024)
+	r := &Reader{QuoteChar: '"', EscapeChar: '"', fileName: fileName, file: f, buf: buf}
 	return r, nil
 }
 
 func (r *Reader) Close() error {
-
-	err := r.mmap.Unmap()
-	if err != nil {
-		return fmt.Errorf("error unmapping file %s: %v", r.fileName, err)
-	}
 	return r.file.Close()
 }
 
 func (r *Reader) Read() (string, error) {
+retry:
+
+	if len(r.remainingBuf) == 0 {
+		n1 := len(r.pendingBytes)
+		if n1 > 0 {
+			// We have some pending bytes from the previous read.
+			// Copy them to the beginning of the buffer.
+			copy(r.buf, r.pendingBytes)
+			r.pendingBytes = r.pendingBytes[:0]
+		}
+		// Read the next chunk of the file.
+		n2, err := r.file.Read(r.buf[n1:])
+		n := n1 + n2
+		if err != nil {
+			if err == io.EOF {
+				r.eof = true
+			} else {
+				return "", fmt.Errorf("error reading file %s: %v", r.fileName, err)
+			}
+		}
+		r.remainingBuf = r.buf[:n]
+	}
+	if len(r.remainingBuf) == 0 && r.eof {
+		return "", io.EOF
+	}
+	line, remainingBuf, err := r.read(r.remainingBuf)
+	if len(remainingBuf) == len(r.remainingBuf) { // No progress.
+		return "", fmt.Errorf("unterminated quoted field in file %s", r.fileName)
+	}
+	if err == errEndOfBuffer {
+		// We have a partial line in the buffer.
+		// Copy it to the pending bytes and read the next chunk.
+		r.pendingBytes = append(r.pendingBytes, remainingBuf...)
+		r.remainingBuf = r.remainingBuf[:0]
+		goto retry
+	}
+	r.remainingBuf = remainingBuf
+	if len(r.remainingBuf) == 0 && line == "" {
+		goto retry
+	}
+	return line, nil
+}
+
+var errEndOfBuffer = errors.New("end of buffer")
+
+func (r *Reader) read(buf []byte) (string, []byte, error) {
 	i := 0
 	for {
-		if len(r.buf) == 0 { // Empty buffer.
-			return "", io.EOF
+		if len(buf) == 0 { // Empty buffer.
+			return "", nil, errEndOfBuffer
 		}
-		if i == len(r.buf) {
+		if i == len(buf) {
 			// No newline found in the buffer.
-			// Return the entire remaining buffer as the last record.
-			line := string(r.buf)
-			r.buf = r.buf[:0]
-			return line, nil
+			return "", buf, errEndOfBuffer
 		}
-		if r.buf[i] == '\n' {
+		if buf[i] == '\n' {
 			// Found a newline that is outside of a quoted field.
-			line := string(r.buf[:i+1]) // including the newline.
-			r.buf = r.buf[i+1:]         // reading after the newline.
-			if len(line) == 0 {         // Skip empty lines.
+			line := string(buf[:i+1]) // including the newline.
+			buf = buf[i+1:]           // reading after the newline.
+			if len(line) == 0 {       // Skip empty lines.
 				i = 0 // Reset the index to the beginning of the buffer.
 				continue
 			}
-			return line, nil
+			return line, buf, nil
 		}
-		if r.buf[i] != r.QuoteChar {
+		if buf[i] != r.QuoteChar {
 			i++
 			continue
 		}
 		// Found a quote.
 		i++ // Enter the quoted field.
 		// Find the next unescaped quote.
-		for ; i < len(r.buf); i++ {
-			if r.buf[i] != r.QuoteChar {
+		for ; i < len(buf); i++ {
+			if buf[i] != r.QuoteChar {
 				continue
 			}
 			// Found a quote.
 			if r.QuoteChar == r.EscapeChar {
-				if i+1 < len(r.buf) && r.buf[i+1] == r.QuoteChar {
+				if i+1 < len(buf) && buf[i+1] == r.QuoteChar {
 					// The i'th quote is escaping the i+1'th quote.
 					i++ // Skip the next quote as well.
 				} else {
@@ -85,14 +121,14 @@ func (r *Reader) Read() (string, error) {
 				}
 			} else {
 				// Check for an escaped quote.
-				escaped := r.buf[i-1] == r.EscapeChar && r.buf[i-2] != r.EscapeChar
+				escaped := buf[i-1] == r.EscapeChar && buf[i-2] != r.EscapeChar
 				if !escaped {
 					break
 				}
 			}
 		}
-		if i == len(r.buf) {
-			return "", fmt.Errorf("unterminated quoted field")
+		if i == len(buf) {
+			return "", buf, errEndOfBuffer
 		}
 		i++
 	}
