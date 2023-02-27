46cbfcb53ea4d60193d431198ef94a33fb6a0a2b
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2023-02-27 08:49:58 +0000

    Added a reserved keywords test case for MySQL

---
+++ migtests/tests/mysql-tests/mysql-reserved-words/ReservedWords.sql
@@ -0,0 +1,33 @@
+﻿Create Table `order` (
+    id int PRIMARY KEY,
+    name varchar(10)
+);
+
+Create Table `user` (
+    id int PRIMARY KEY,
+    name varchar(10)
+);
+
+
+Create Table `group` (
+    id int PRIMARY KEY,
+    name varchar(10)
+);
+
+INSERT into `order` values(1, 'abc');
+INSERT into `order` values(2, 'abc');
+INSERT into `order` values(3, 'abc');
+INSERT into `order` values(4, 'abc');
+INSERT into `order` values(5, 'abc');
+
+INSERT into `user` values(1, 'abc');
+INSERT into `user` values(2, 'abc');
+INSERT into `user` values(3, 'abc');
+INSERT into `user` values(4, 'abc');
+INSERT into `user` values(5, 'abc');
+
+INSERT into `group` values(1, 'abc');
+INSERT into `group` values(2, 'abc');
+INSERT into `group` values(3, 'abc');
+INSERT into `group` values(4, 'abc');
+INSERT into `group` values(5, 'abc');
---
+++ migtests/tests/mysql-tests/mysql-reserved-words/env.sh
@@ -0,0 +1,3 @@
+export SOURCE_DB_TYPE="mysql"
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"reserved_words"}
+export TARGET_DB_SCHEMA="reserved_words"
