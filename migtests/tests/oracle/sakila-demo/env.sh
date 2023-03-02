ccb8dfa89e3172c1a2a406ca66fce880ebe2f119
Priyanshi Gupta <pgupta@yugabyte.com>
2023-03-02 18:15:07 +0530

    updated the env.sh in sakila-demo test case in oracle (#781)

---
+++ migtests/tests/oracle/sakila-demo/env.sh
@@ -0,0 +1,7 @@
+export SOURCE_DB_TYPE="oracle"
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"DMS"}
+export SOURCE_DB_HOST=${SOURCE_DB_HOST:-"ssinghal-dms-oracle.cbtcvpszcgdq.us-west-2.rds.amazonaws.com"}
+export SOURCE_DB_PORT=${SOURCE_DB_PORT:-1521}
+export SOURCE_DB_USER=${SOURCE_DB_USER:-"ybvoyager"}
+export SOURCE_DB_PASSWORD=${SOURCE_DB_PASSWORD:-"password"}
+export SOURCE_DB_SCHEMA=${SOURCE_DB_SCHEMA:-"sakila_demo"}
