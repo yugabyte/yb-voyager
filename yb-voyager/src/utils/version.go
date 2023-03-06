0b91595f2d08b930509a0f3cb966bb5883b77056
ShivanshGahlot <shivanshgahlot@gmail.com>
2023-03-06 14:12:24 +0530

    Updated to version 1.1.0

--- yb-voyager/src/utils/version.go
+++ yb-voyager/src/utils/version.go
@@ -2,7 +2,7 @@ package utils
 
 const (
 	// This constant must be updated on every release.
-	YB_VOYAGER_VERSION = "main"
+	YB_VOYAGER_VERSION = "1.1.0"
 
 	// @Refer: https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
 	GIT_COMMIT_HASH = "$Format:%H$"
