3239c304c87d70cc5722b0cb90769a1f6d100e50
ShivanshGahlot <shivanshgahlot@gmail.com>
2023-03-13 17:38:09 +0530

    Added user creation steps for oracle

--- migtests/scripts/oracle/env.sh
+++ migtests/scripts/oracle/env.sh
@@ -1,10 +1,10 @@
-# We use an already deployed RDS Oracle instance for testing.
-# ssinghal-dms-oracle: https://us-west-2.console.aws.amazon.com/rds/home?region=us-west-2#database:id=ssinghal-dms-oracle;is-cluster=false
-# export SOURCE_DB_HOST=${SOURCE_DB_HOST:-"35.165.118.74"}
+# We use an already deployed RDS Oracle instance on docker for testing.
+
 export SOURCE_DB_PORT=${SOURCE_DB_PORT:-1521}
-# export SOURCE_DB_USER=${SOURCE_DB_USER:-"sakila_demo"}
-# export SOURCE_DB_PASSWORD=${SOURCE_DB_PASSWORD:-"password"}
-# export SOURCE_DB_SCHEMA=${SOURCE_DB_SCHEMA:-"sakila_demo"}
+export SOURCE_DB_USER=${SOURCE_DB_USER:-"ybvoyager"}
+export SOURCE_DB_PASSWORD=${SOURCE_DB_PASSWORD:-"password"}
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"ORCLPDB1"}
+export SOURCE_DB_HOST=${SOURCE_DB_HOST:-"localhost"}
 export SOURCE_DB_USER_SCHEMA_OWNER=${SOURCE_DB_USER_SCHEMA_OWNER:-"TEST_SCHEMA"}
 export SOURCE_DB_USER_SCHEMA_OWNER_PASSWORD=${SOURCE_DB_USER_SCHEMA_OWNER_PASSWORD:-"password"}
 export SOURCE_DB_USER_SYS=${SOURCE_DB_USER_SYS:-"SYS"}
