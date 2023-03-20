e525a1bad9c2460b6a6b93f10c7c360ef670fcc3
Shubham <centos@ip-10-9-6-84.us-west-2.compute.internal>
2023-03-20 11:54:33 +0000

    Dummy pr to check env variables

--- migtests/scripts/yugabytedb/env.sh
+++ migtests/scripts/yugabytedb/env.sh
@@ -1,9 +1,9 @@
-export TARGET_DB_HOST=${TARGET_DB_HOST:-"127.0.0.1"}
+export TARGET_DB_HOST=${TARGET_DB_HOST:-"172.151.25.154"}
 export TARGET_DB_PORT=${TARGET_DB_PORT:-5433}
 export TARGET_DB_USER=${TARGET_DB_USER:-"ybvoyager"}
-export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-'Test@123#$%^&*()!'}
+export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD:-'password'}
 export TARGET_DB_ADMIN_USER=${TARGET_DB_ADMIN_USER:-"yugabyte"}
-export TARGET_DB_ADMIN_PASSWORD=${TARGET_DB_ADMIN_PASSWORD:-''}
+export TARGET_DB_ADMIN_PASSWORD=${TARGET_DB_ADMIN_PASSWORD:-'password'}
 export TARGET_DB_SCHEMA=${TARGET_DB_SCHEMA:-'public'}
 
 # The PG driver, used to connect to YB, is case-sensitive about database name.
