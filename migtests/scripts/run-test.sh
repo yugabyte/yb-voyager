e03fcead9223401ec10aa4037f5232c9c41449d4
ShivanshGahlot <shivanshgahlot@gmail.com>
2023-03-20 14:07:29 +0530

    Added user creation steps for oracle

--- migtests/scripts/run-test.sh
+++ migtests/scripts/run-test.sh
@@ -21,7 +21,7 @@ export EXPORT_DIR=${EXPORT_DIR:-"${TEST_DIR}/export-dir"}
 export PYTHONPATH="${REPO_ROOT}/migtests/lib"
 echo $LD_WOW
 echo $WOW
-echo $TARGET_DB_HOST
+echo $TARGET_DB_PORT
 export LIBRARY_PATH=${LD_LIBRARY_PATH}
 export TARGET_DB_HOST=${TARGET_DB_HOST}
 export TARGET_DB_PORT=${TARGET_DB_PORT}
