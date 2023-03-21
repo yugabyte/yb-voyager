a8e6988bf20f8ebb8f34d548364955c83de0752b
ShivanshGahlot <shivanshgahlot@gmail.com>
2023-03-21 16:34:45 +0530

    Few more changes

--- migtests/lib/yb.py
+++ migtests/lib/yb.py
@@ -153,7 +153,7 @@ class PostgresDB:
 	def fetch_all_triggers(self, schema_name="public") -> List[str]:
 		cur = self.conn.cursor()
 		cur.execute(f"SELECT trigger_name FROM information_schema.triggers WHERE trigger_schema = '{schema_name}'")
-		return set(cur.fetchall())
+		return [trigger[0] for trigger in cur.fetchall()]
 
 	def fetch_all_procedures(self, schema_name="public") -> List[str]:
 		cur = self.conn.cursor()
