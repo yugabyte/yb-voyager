6ce36984d4ffe613d32850b9079dc72705937394
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2023-04-05 17:40:18 +0000

    Kill debezium process if parent process dies/exits

--- yb-voyager/src/dbzm/dbzm.go
+++ yb-voyager/src/dbzm/dbzm.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -46,6 +47,10 @@ func (d *Debezium) Start() error {
 	cmdStr := fmt.Sprintf("cd %q; %s > %s 2>&1", DEBEZIUM_DIST_DIR, filepath.Join(DEBEZIUM_DIST_DIR, "run.sh"), logFile)
 	log.Infof("running command: %s\n", cmdStr)
 	d.cmd = exec.Command("/bin/bash", "-c", cmdStr)
+	d.cmd.SysProcAttr = &syscall.SysProcAttr{
+		Pdeathsig: syscall.SIGKILL, // kill the debezium process if the parent process dies
+	}
+
 	err = d.cmd.Start()
 	if err != nil {
 		return fmt.Errorf("Error starting debezium: %v", err)
