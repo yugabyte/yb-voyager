0afd9c70e5d28cb5757b47fa7ca174daabe88c3c
Aneesh Makala <amakala@yugabyte.com>
2023-04-28 13:04:23 +0530

    initial

--- yb-voyager/src/dbzm/dbzm.go
+++ yb-voyager/src/dbzm/dbzm.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"syscall"
+
+	"github.com/tebeka/atexit"
+	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils"
+
+	// "syscall"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -42,18 +48,37 @@ func (d *Debezium) Start() error {
 	logFile, _ := filepath.Abs(filepath.Join(d.ExportDir, "logs/debezium.log"))
 	log.Infof("debezium logfile path: %s\n", logFile)
 
-	cmdStr := fmt.Sprintf("%s %s > %s 2>&1", filepath.Join(DEBEZIUM_DIST_DIR, "run.sh"), DEBEZIUM_CONF_FILEPATH, logFile)
-	log.Infof("running command: %s\n", cmdStr)
-	d.cmd = exec.Command("/bin/bash", "-c", cmdStr)
-	d.cmd.SysProcAttr = &syscall.SysProcAttr{
-		Pdeathsig: syscall.SIGKILL, // kill the debezium process if the parent process dies
+	// cmdStr := fmt.Sprintf("%s %s > %s 2>&1", filepath.Join(DEBEZIUM_DIST_DIR, "run.sh"), DEBEZIUM_CONF_FILEPATH, logFile)
+	// log.Infof("running command: %s\n", cmdStr)
+	// d.cmd = exec.Command("/bin/bash", "-c", cmdStr)
+	d.cmd = exec.Command(filepath.Join(DEBEZIUM_DIST_DIR, "run.sh"), DEBEZIUM_CONF_FILEPATH)
+	// open the out file for writing
+	outfile, err := os.Create(logFile)
+	if err != nil {
+		panic(err)
 	}
+	defer outfile.Close()
+	d.cmd.Stdout = outfile
+	d.cmd.Stderr = outfile
+	// d.cmd.SysProcAttr = &syscall.SysProcAttr{
+	// 	Pdeathsig: syscall.SIGKILL, // kill the debezium process if the parent process dies
+	// }
+
+	atexit.Register(func() {
+		d.Stop()
+	})
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigs
+		utils.PrintAndLog("Received signal %s. Exiting", sig)
+		atexit.Exit(0)
+	}()
 
 	err = d.cmd.Start()
 	if err != nil {
 		return fmt.Errorf("Error starting debezium: %v", err)
 	}
-
 	log.Infof("Debezium started successfully")
 	go func() {
 		d.err = d.cmd.Wait()
@@ -82,10 +107,12 @@ func (d *Debezium) GetExportStatus() (*ExportStatus, error) {
 func (d *Debezium) Stop() error {
 	if d.IsRunning() {
 		log.Infof("Stopping debezium...")
-		err := d.cmd.Process.Kill()
+		err := d.cmd.Process.Signal(syscall.SIGTERM)
 		if err != nil {
 			return fmt.Errorf("Error stopping debezium: %v", err)
 		}
+		d.cmd.Wait()
+		log.Info("Stopped debezium.")
 	}
 	return nil
 }
