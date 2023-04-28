1f68e8f3d426431f4c2131432a759f0b7620ed36
Shubham <centos@ip-10-9-6-84.us-west-2.compute.internal>
2023-04-28 06:02:40 +0000

    Added user creation steps required for debezium snapshot export

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -90,6 +90,9 @@ grant_user_permission_mysql() {
 		"GRANT SELECT ON ${SOURCE_DB_NAME}.* TO '${SOURCE_DB_USER}'@'${SOURCE_DB_HOST}';"
 		"GRANT SHOW VIEW ON ${SOURCE_DB_NAME}.* TO '${SOURCE_DB_USER}'@'${SOURCE_DB_HOST}';"
 		"GRANT TRIGGER ON ${SOURCE_DB_NAME}.* TO '${SOURCE_DB_USER}'@'${SOURCE_DB_HOST}';"
+		# Extra steps required to enable Debezium export 
+		"GRANT FLUSH_TABLES ON *.* TO '${SOURCE_DB_USER}'@'${SOURCE_DB_HOST}';"
+		"GRANT REPLICATION CLIENT ON *.* TO '${SOURCE_DB_USER}'@'${SOURCE_DB_HOST}';"
 	)
 
 	for command in "${commands[@]}"; do
@@ -109,22 +112,26 @@ grant_user_permission_oracle(){
 	db_schema=$2
 
 	cat > oracle-inputs.sql << EOF
-	grant connect to ybvoyager;
-	grant select_catalog_role to ybvoyager;
-	grant select any dictionary to ybvoyager;
-	grant select on sys.argument$ to ybvoyager;
+	GRANT CONNECT TO ybvoyager;
+	GRANT SELECT_CATALOG_ROLE TO ybvoyager;
+	GRANT SELECT ANY DICTIONARY TO ybvoyager;
+	GRANT SELECT ON SYS.ARGUMENT$ TO ybvoyager;
 	BEGIN
-    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner='${db_schema}' and object_type = 'TYPE') LOOP
+    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type = 'TYPE') LOOP
        		EXECUTE IMMEDIATE 'grant execute on '||R.owner||'."'||R.object_name||'" to ybvoyager';
    		END LOOP;
 	END;
 	/
 	BEGIN
-    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner='${db_schema}' and object_type in ('VIEW','SEQUENCE','TABLE PARTITION','TABLE','SYNONYM','MATERIALIZED VIEW')) LOOP
+    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type in ('VIEW','SEQUENCE','TABLE PARTITION','TABLE','SYNONYM','MATERIALIZED VIEW')) LOOP
         	EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to ybvoyager';
   		END LOOP;
 	END;
 	/
+	/*
+	Extra steps required to enable Debezium export
+	*/
+	GRANT FLASHBACK ANY TABLE TO ybvoyager;
 EOF
 	
 	run_sqlplus_as_sys ${db_name} "oracle-inputs.sql"
