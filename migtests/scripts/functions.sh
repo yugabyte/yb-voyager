903ebac8d3d34ed7c9f517aa6b046b4286f0f97e
Priyanshi Gupta <pgupta@yugabyte.com>
2023-06-01 08:34:13 +0530

    [DB-6716]changed the oracle permissions to remove the nested tables (#901)

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -123,9 +123,15 @@ grant_user_permission_oracle(){
 	END;
 	/
 	BEGIN
-    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type in ('VIEW','SEQUENCE','TABLE PARTITION','TABLE','SYNONYM','MATERIALIZED VIEW')) LOOP
+    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type in ('VIEW','SEQUENCE','TABLE PARTITION','SYNONYM','MATERIALIZED VIEW')) LOOP
         	EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to ybvoyager';
   		END LOOP;
+	END;
+	/
+	BEGIN
+		FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type ='TABLE' MINUS SELECT owner, table_name from all_nested_tables where owner = UPPER('${db_schema}')) LOOP
+			EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to  ybvoyager';
+		END LOOP;
 	END;
 	/
 	/*
