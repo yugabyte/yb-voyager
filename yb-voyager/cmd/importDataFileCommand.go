d58c54874e053f7059665b822e4c3259152434c2
Priyanshi Gupta <pgupta@yugabyte.com>
2023-06-02 11:09:49 +0530

    [DB-6768]Changed the default delimiter according to file format csv (',') and text ('\t') (#902)
    * Change the default delimiter according to file format csv (',') and text ('\t')
    * added automation tests for default delimiter

--- yb-voyager/cmd/importDataFileCommand.go
+++ yb-voyager/cmd/importDataFileCommand.go
@@ -208,6 +208,7 @@ func checkImportDataFileFlags(cmd *cobra.Command) {
 	fileFormat = strings.ToLower(fileFormat)
 	checkFileFormat()
 	checkDataDirFlag()
+	setDefaultForDelimiter()
 	checkDelimiterFlag()
 	checkHasHeader()
 	checkAndParseFileOpts()
@@ -326,6 +327,20 @@ func setDefaultForNullString() {
 	}
 }
 
+func setDefaultForDelimiter() {
+	if delimiter != "" {
+		return
+	}
+	switch fileFormat {
+	case datafile.CSV:
+		delimiter = `,`
+	case datafile.TEXT:
+		delimiter = `\t`
+	default:
+		panic("unsupported file format")
+	}
+}
+
 // resolves and check the given string is a single byte character
 func resolveAndCheckSingleByteChar(value string) (string, bool) {
 	if len(value) == 1 {
@@ -367,8 +382,8 @@ func init() {
 	importDataFileCmd.Flags().StringVar(&fileFormat, "format", "csv",
 		fmt.Sprintf("supported data file types: %v", supportedFileFormats))
 
-	importDataFileCmd.Flags().StringVar(&delimiter, "delimiter", ",",
-		"character used as delimiter in rows of the table(s)")
+	importDataFileCmd.Flags().StringVar(&delimiter, "delimiter", "",
+		`character used as delimiter in rows of the table(s)(default is comma for CSV and tab for TEXT format)`)
 
 	importDataFileCmd.Flags().StringVar(&dataDir, "data-dir", "",
 		"path to the directory contains data files to import into table(s)")
