12e5997f140d7e301b3e4252ee7c0d3629d4eea0
Amit Jambure <ajambure@yugabyte.com>
2023-06-19 09:23:52 +0000

    Add test case to validate glob patterns in --file-table-map.

--- migtests/tests/import-file/schema.sql
+++ migtests/tests/import-file/schema.sql
@@ -117,3 +117,6 @@ create table t1_null_string_text(i int, j timestamp, k bigint, l varchar(30));
 create table t1_null_string_csv2(i int, j timestamp, k bigint, l varchar(30));
 
 create table t1_null_string_text2(i int, j timestamp, k bigint, l varchar(30));
+
+-- Table to test multiple-files-to-single-table import.
+create table foo (k int primary key, v text);
