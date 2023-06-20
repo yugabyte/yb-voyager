2ec4930f058bd67b87fefd4eb189027b3bc8e710
Amit Jambure <ajambure@yugabyte.com>
2023-06-20 09:36:26 +0000

    Add couple of more test cases.

--- migtests/tests/import-file/schema.sql
+++ migtests/tests/import-file/schema.sql
@@ -120,3 +120,5 @@ create table t1_null_string_text2(i int, j timestamp, k bigint, l varchar(30));
 
 -- Table to test multiple-files-to-single-table import.
 create table foo (k int primary key, v text);
+create table foo2 (k int primary key, v text);
+create table foo3 (k int primary key, v text);
