d340985d5edc3258204f969e201426f801ab5b96
Aneesh Makala <amakala@yugabyte.com>
2023-06-23 16:14:51 +0530

    fix ssl mode

--- migtests/tests/pg/ssl/env.sh
+++ migtests/tests/pg/ssl/env.sh
@@ -1,7 +1,7 @@
 export SOURCE_DB_TYPE="postgresql"
 export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_datatypes"}
 export SOURCE_DB_SCHEMA="public"
-export SOURCE_DB_SSL_MODE="verify_ca"
+export SOURCE_DB_SSL_MODE="verify-ca"
 export SOURCE_DB_SSL_CERT="/home/ubuntu/.postgresql/postgresql.crt"
 export SOURCE_DB_SSL_KEY="/home/ubuntu/.postgresql/postgresql.key"
 export SOURCE_DB_SSL_ROOT_CERT="/home/ubuntu/.postgresql/root.crt"
