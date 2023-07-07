4a1880f5bc5ea7f99fad318d2c24fcffb17d98b8
Shubham Dabriwala <65844208+shubham-yb@users.noreply.github.com>
2023-07-07 15:47:36 +0530

    Enhanced Oracle automation (#912)
    * Enhanced Oracle automation
    
    * Updated YB-DB docker versions
    
    * Added Case Sensitivity Cases

--- migtests/tests/pg/misc-objects-1/pg_misc_objects1.sql
+++ migtests/tests/pg/misc-objects-1/pg_misc_objects1.sql
@@ -52,9 +52,9 @@ comment on table session_log is 'Our session logs';
 \dt+ session_log
 
 
-drop table if exists "Mixed_Case_Test";
+drop table if exists "Mixed_Case_Table_Name_Test";
 
-create table "Mixed_Case_Test" (
+create table "Mixed_Case_Table_Name_Test" (
 	id serial primary key,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
@@ -66,15 +66,15 @@ create table "Mixed_Case_Test" (
 
 -- aut int NOT NULL GENERATED ALWAYS AS ((id*9)+1) stored
 
-insert into "Mixed_Case_Test" (first_name, last_name, email, gender, ip_address) values ('Modestine', 'MacMeeking', 'mmacmeeking0@zimbio.com', 'Female', '208.44.58.185');
-insert into "Mixed_Case_Test" (first_name, last_name, email, gender, ip_address) values ('Genna', 'Kaysor', 'gkaysor1@hibu.com', 'Female', '202.48.51.58');
-insert into "Mixed_Case_Test" (first_name, last_name, email, gender, ip_address) values ('Tess', 'Wesker', 'twesker2@scientificamerican.com', 'Female', '177.153.32.186');
-insert into "Mixed_Case_Test" (first_name, last_name, email, gender, ip_address) values ('Magnum', 'Danzelman', 'mdanzelman3@storify.com', 'Bigender', '192.200.33.56');
-insert into "Mixed_Case_Test" (first_name, last_name, email, gender, ip_address) values ('Mitzi', 'Pidwell', 'mpidwell4@shutterfly.com', 'Female', '216.4.250.71');
+insert into "Mixed_Case_Table_Name_Test" (first_name, last_name, email, gender, ip_address) values ('Modestine', 'MacMeeking', 'mmacmeeking0@zimbio.com', 'Female', '208.44.58.185');
+insert into "Mixed_Case_Table_Name_Test" (first_name, last_name, email, gender, ip_address) values ('Genna', 'Kaysor', 'gkaysor1@hibu.com', 'Female', '202.48.51.58');
+insert into "Mixed_Case_Table_Name_Test" (first_name, last_name, email, gender, ip_address) values ('Tess', 'Wesker', 'twesker2@scientificamerican.com', 'Female', '177.153.32.186');
+insert into "Mixed_Case_Table_Name_Test" (first_name, last_name, email, gender, ip_address) values ('Magnum', 'Danzelman', 'mdanzelman3@storify.com', 'Bigender', '192.200.33.56');
+insert into "Mixed_Case_Table_Name_Test" (first_name, last_name, email, gender, ip_address) values ('Mitzi', 'Pidwell', 'mpidwell4@shutterfly.com', 'Female', '216.4.250.71');
 
-\d "Mixed_Case_Test"
+\d "Mixed_Case_Table_Name_Test"
 
-select * from "Mixed_Case_Test";
+select * from "Mixed_Case_Table_Name_Test";
 
 drop table if exists "Case_Sensitive_Columns";
 
