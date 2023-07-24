24b723eb963f773b75befe106db2a90d17aba49f
Shubham <centos@ip-10-9-6-84.us-west-2.compute.internal>
2023-07-24 09:40:20 +0000

    Cleanup and Added Oracle step

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -113,33 +113,33 @@ grant_user_permission_oracle(){
 	db_name=$1
 	db_schema=$2
 
-		cat > oracle-inputs.sql << EOF
-		GRANT CONNECT TO ybvoyager;
-		GRANT SELECT_CATALOG_ROLE TO ybvoyager;
-		GRANT SELECT ANY DICTIONARY TO ybvoyager;
-		GRANT SELECT ON SYS.ARGUMENT$ TO ybvoyager;
-		BEGIN
-	    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type = 'TYPE') LOOP
-	       		EXECUTE IMMEDIATE 'grant execute on '||R.owner||'."'||R.object_name||'" to ybvoyager';
-	   		END LOOP;
-		END;
-		/
-		BEGIN
-	    	FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type in ('VIEW','SEQUENCE','TABLE PARTITION','SYNONYM','MATERIALIZED VIEW')) LOOP
-	        	EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to ybvoyager';
-	  		END LOOP;
-		END;
-		/
-		BEGIN
-			FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type ='TABLE' MINUS SELECT owner, table_name from all_nested_tables where owner = UPPER('${db_schema}')) LOOP
-				EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to  ybvoyager';
-			END LOOP;
-		END;
-		/
-		/*
-		Extra steps required to enable Debezium export
-		*/
-		GRANT FLASHBACK ANY TABLE TO ybvoyager;
+	cat > oracle-inputs.sql << EOF
+	GRANT CONNECT TO ybvoyager;
+	GRANT SELECT_CATALOG_ROLE TO ybvoyager;
+	GRANT SELECT ANY DICTIONARY TO ybvoyager;
+	GRANT SELECT ON SYS.ARGUMENT$ TO ybvoyager;
+	BEGIN
+		FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type = 'TYPE') LOOP
+	   		EXECUTE IMMEDIATE 'grant execute on '||R.owner||'."'||R.object_name||'" to ybvoyager';
+		END LOOP;
+	END;
+	/
+	BEGIN
+		FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type in ('VIEW','SEQUENCE','TABLE PARTITION','SYNONYM','MATERIALIZED VIEW')) LOOP
+	    	EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to ybvoyager';
+		END LOOP;
+	END;
+	/
+	BEGIN
+		FOR R IN (SELECT owner, object_name FROM all_objects WHERE owner=UPPER('${db_schema}') and object_type ='TABLE' MINUS SELECT owner, table_name from all_nested_tables where owner = UPPER('${db_schema}')) LOOP
+			EXECUTE IMMEDIATE 'grant select on '||R.owner||'."'||R.object_name||'" to  ybvoyager';
+		END LOOP;
+	END;
+	/
+	/*
+	Extra steps required to enable Debezium export
+	*/
+	GRANT FLASHBACK ANY TABLE TO ybvoyager;
 EOF
 	run_sqlplus_as_sys ${db_name} "oracle-inputs.sql"	
 
