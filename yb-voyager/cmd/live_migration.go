6f5195753aad12ab23db643436dc8a2e18461f9a
Aneesh Makala <amakala@yugabyte.com>
2023-08-03 19:45:58 +0530

    Enabling parallel streaming import (#972)
    Parallel import of events on target DB. 
    - we create N channels that hold events. hash(event.primaryKey values) % NUM_CHANNELS determines the channel to add to.
    - we create N goroutines that consume from the N channels (1:1)
    - the goroutines consume events, batch them (size & time based), and then call tgtDb.ExecuteBatch. 
    
    Note: the actual parallelism is controlled by --parallel-jobs param which determines the connection pool size. THis is just about how many event channels are created that can independently call tgtDB.ExecuteBatch.

--- yb-voyager/cmd/live_migration.go
+++ yb-voyager/cmd/live_migration.go
@@ -18,17 +18,41 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"hash/fnv"
 	"os"
 	"path/filepath"
+	"sort"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/yugabyte/yb-voyager/yb-voyager/src/tgtdb"
+	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils"
 )
+
+var NUM_EVENT_CHANNELS int
+var EVENT_CHANNEL_SIZE int // has to be > MAX_EVENTS_PER_BATCH
+var MAX_EVENTS_PER_BATCH int
+var MAX_INTERVAL_BETWEEN_BATCHES int //ms
+var END_OF_QUEUE_SEGMENT_EVENT = &tgtdb.Event{Op: "end_of_source_queue_segment"}
+
+func init() {
+	NUM_EVENT_CHANNELS = utils.GetEnvAsInt("NUM_EVENT_CHANNELS", 512)
+	EVENT_CHANNEL_SIZE = utils.GetEnvAsInt("EVENT_CHANNEL_SIZE", 2000)
+	MAX_EVENTS_PER_BATCH = utils.GetEnvAsInt("MAX_EVENTS_PER_BATCH", 2000)
+	MAX_INTERVAL_BETWEEN_BATCHES = utils.GetEnvAsInt("MAX_INTERVAL_BETWEEN_BATCHES", 2000)
+}
 
 func streamChanges() error {
 	eventQueue := NewEventQueue(exportDir)
+	// setup target event channels
+	var evChans []chan *tgtdb.Event
+	var processingDoneChans []chan bool
+	for i := 0; i < NUM_EVENT_CHANNELS; i++ {
+		evChans = append(evChans, make(chan *tgtdb.Event, EVENT_CHANNEL_SIZE))
+		processingDoneChans = append(processingDoneChans, make(chan bool, 1))
+	}
+
 	log.Infof("streaming changes from %s", eventQueue.QueueDirPath)
 	for { // continuously get next segments to stream
 		segment, err := eventQueue.GetNextSegment()
@@ -41,19 +65,25 @@ func streamChanges() error {
 		}
 		log.Infof("got next segment to stream: %v", segment)
 
-		err = streamChangesFromSegment(segment)
+		err = streamChangesFromSegment(segment, evChans, processingDoneChans)
 		if err != nil {
 			return fmt.Errorf("error streaming changes for segment %s: %v", segment.FilePath, err)
 		}
 	}
 }
 
-func streamChangesFromSegment(segment *EventQueueSegment) error {
+func streamChangesFromSegment(segment *EventQueueSegment, evChans []chan *tgtdb.Event, processingDoneChans []chan bool) error {
 	err := segment.Open()
 	if err != nil {
 		return err
 	}
 	defer segment.Close()
+
+	// start target event channel processors
+	for i := 0; i < NUM_EVENT_CHANNELS; i++ {
+		go processEvents(i, evChans[i], processingDoneChans[i])
+	}
+
 	log.Infof("streaming changes for segment %s", segment.FilePath)
 	for !segment.IsProcessed() {
 		event, err := segment.NextEvent()
@@ -65,19 +95,26 @@ func streamChangesFromSegment(segment *EventQueueSegment) error {
 			break
 		}
 
-		err = handleEvent(event)
+		err = handleEvent(event, evChans)
 		if err != nil {
 			return fmt.Errorf("error handling event: %v", err)
 		}
 	}
 
-	log.Infof("finished streaming changes from segment %s", segment.FilePath)
+	for i := 0; i < NUM_EVENT_CHANNELS; i++ {
+		evChans[i] <- END_OF_QUEUE_SEGMENT_EVENT
+	}
+
+	for i := 0; i < NUM_EVENT_CHANNELS; i++ {
+		<-processingDoneChans[i]
+	}
+
 	// TODO: printing this line until some user stats are available.
-	fmt.Printf("finished streaming changes from segment %s\n", filepath.Base(segment.FilePath))
+	utils.PrintAndLog("finished streaming changes from segment %s\n", filepath.Base(segment.FilePath))
 	return nil
 }
 
-func handleEvent(event *tgtdb.Event) error {
+func handleEvent(event *tgtdb.Event, evChans []chan *tgtdb.Event) error {
 	log.Debugf("Handling event: %v", event)
 	tableName := event.TableName
 	if sourceDBType == "postgresql" && event.SchemaName != "public" {
@@ -88,10 +125,62 @@ func handleEvent(event *tgtdb.Event) error {
 	if err != nil {
 		return fmt.Errorf("error transforming event key fields: %v", err)
 	}
-	batch := []*tgtdb.Event{event}
-	err = tdb.ExecuteBatch(batch)
-	if err != nil {
-		return fmt.Errorf("error executing batch: %v", err)
-	}
+
+	h := hashEvent(event)
+	evChans[h] <- event
 	return nil
+}
+
+// Returns a hash value between 0..NUM_EVENT_CHANNELS
+func hashEvent(e *tgtdb.Event) int {
+	hash := fnv.New64a()
+	hash.Write([]byte(e.SchemaName + e.TableName))
+
+	keyColumns := make([]string, 0)
+	for k := range e.Key {
+		keyColumns = append(keyColumns, k)
+	}
+
+	// sort to ensure input to hash is consistent.
+	sort.Strings(keyColumns)
+	for _, k := range keyColumns {
+		hash.Write([]byte(*e.Key[k]))
+	}
+	return int(hash.Sum64() % (uint64(NUM_EVENT_CHANNELS)))
+}
+
+func processEvents(chanNo int, evChan chan *tgtdb.Event, done chan bool) {
+	endOfProcessing := false
+	for !endOfProcessing {
+		batch := []*tgtdb.Event{}
+		timer := time.NewTimer(time.Duration(MAX_INTERVAL_BETWEEN_BATCHES) * time.Millisecond)
+	Batching:
+		for {
+			// read from channel until MAX_EVENTS_PER_BATCH or MAX_INTERVAL_BETWEEN_BATCHES
+			select {
+			case event := <-evChan:
+				if event == END_OF_QUEUE_SEGMENT_EVENT {
+					endOfProcessing = true
+					break Batching
+				}
+				batch = append(batch, event)
+				if len(batch) >= MAX_EVENTS_PER_BATCH {
+					break Batching
+				}
+			case <-timer.C:
+				break Batching
+			}
+		}
+		timer.Stop()
+
+		if len(batch) == 0 {
+			continue
+		}
+		err := tdb.ExecuteBatch(batch)
+		if err != nil {
+			utils.ErrExit("error executing batch: %v", err)
+		}
+		log.Debugf("processEvents from channel %v: Executed Batch of size - %v", chanNo, len(batch))
+	}
+	done <- true
 }
