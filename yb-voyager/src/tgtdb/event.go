bb9e20f69eea310331fff12968ee1698052de8e4
Sanyam Singhal <42322654+sanyamsinghal@users.noreply.github.com>
2023-08-09 18:22:05 +0530

    Implemented ExecuteBatch() for Oracle (#984)
    - in case of Oracle, simply executing the statements one by one using transactions because transactions are faster in oracle as compared to YB
    
    - added resumability for importing events into the target Oracle database

--- yb-voyager/src/tgtdb/event.go
+++ yb-voyager/src/tgtdb/event.go
@@ -18,6 +18,8 @@ package tgtdb
 import (
 	"fmt"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 type Event struct {
@@ -47,9 +49,9 @@ func (e *Event) GetSQLStmt(targetSchema string) string {
 	}
 }
 
-const insertTemplate = "INSERT INTO %s (%s) VALUES (%s);"
-const updateTemplate = "UPDATE %s SET %s WHERE %s;"
-const deleteTemplate = "DELETE FROM %s WHERE %s;"
+const insertTemplate = "INSERT INTO %s (%s) VALUES (%s)"
+const updateTemplate = "UPDATE %s SET %s WHERE %s"
+const deleteTemplate = "DELETE FROM %s WHERE %s"
 
 func (event *Event) getInsertStmt(targetSchema string) string {
 	tableName := event.SchemaName + "." + event.TableName
@@ -106,6 +108,11 @@ type EventBatch struct {
 
 func (eb EventBatch) GetLastVsn() int64 {
 	return eb.Events[len(eb.Events)-1].Vsn
+}
+
+func (eb EventBatch) GetQueryToUpdateLastAppliedVSN(migrationUUID uuid.UUID) string {
+	return fmt.Sprintf(`UPDATE %s SET last_applied_vsn=%d where migration_uuid='%s' AND channel_no=%d`,
+		EVENT_CHANNELS_METADATA_TABLE_NAME, eb.GetLastVsn(), migrationUUID, eb.ChanNo)
 }
 
 type EventChannelMetaInfo struct {
