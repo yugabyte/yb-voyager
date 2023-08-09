c08d06e36d0bb7130cbc00d1336d40c47ef9bcdc
Aneesh Makala <amakala@yugabyte.com>
2023-08-09 10:16:40 +0530

    review comments

--- yb-voyager/cmd/common.go
+++ yb-voyager/cmd/common.go
@@ -16,7 +16,6 @@ limitations under the License.
 package cmd
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 	"os/exec"
@@ -315,73 +314,6 @@ func retrieveMigrationUUID(exportDir string) error {
 	}
 	migrationUUID = uuid.MustParse(string(uuidBytes))
 	utils.PrintAndLog("migrationID: %s", migrationUUID)
-	return nil
-}
-
-func getMetaDBPath(exportDir string) string {
-	return filepath.Join(exportDir, "metainfo", "meta.db")
-}
-
-func createAndInitMetaDBIfRequired(exportDir string) error {
-	metaDBPath := getMetaDBPath(exportDir)
-	if utils.FileOrFolderExists(metaDBPath) {
-		// already created and initied.
-		return nil
-	}
-	err := createMetaDBFile(metaDBPath)
-	if err != nil {
-		return err
-	}
-	err = initMetaDB(metaDBPath)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func createMetaDBFile(path string) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return fmt.Errorf("not able to create meta db file :%w", err)
-	}
-	err = f.Close()
-	if err != nil {
-		return fmt.Errorf("error while closing meta db file: %w", err)
-	}
-	return nil
-}
-
-func initMetaDB(path string) error {
-	conn, err := sql.Open("sqlite3", path)
-	if err != nil {
-		return fmt.Errorf("error while opening meta db :%w", err)
-	}
-	cmds := []string{
-		fmt.Sprintf(`CREATE TABLE %s (segment_no INTEGER, size_committed INTEGER);`, QUEUE_SEGMENT_META_TABLE_NAME),
-	}
-	for _, cmd := range cmds {
-		_, err = conn.Exec(cmd)
-		if err != nil {
-			return fmt.Errorf("error while initializating meta db with query-%s :%w", cmd, err)
-		}
-		log.Infof("Executed query on meta db - %s", cmd)
-	}
-	return nil
-}
-
-func truncateTablesInMetaDb(exportDir string, tableNames []string) error {
-	conn, err := sql.Open("sqlite3", getMetaDBPath(exportDir))
-	if err != nil {
-		return fmt.Errorf("error while opening meta db :%w", err)
-	}
-	for _, tableName := range tableNames {
-		query := fmt.Sprintf(`DELETE FROM %s;`, tableName)
-		_, err = conn.Exec(query)
-		if err != nil {
-			return fmt.Errorf("error while running query on meta db -%s :%w", query, err)
-		}
-		log.Infof("Executed query on meta db - %s", query)
-	}
 	return nil
 }
 
