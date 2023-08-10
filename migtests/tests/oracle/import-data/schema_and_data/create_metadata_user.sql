7baf6c1917dbaf48245713381fbc41359eca21f1
Ubuntu <ubuntu@ip-10-9-117-96.us-west-2.compute.internal>
2023-08-10 14:53:56 +0000

    Fixed a few things

--- migtests/tests/oracle/import-data/schema_and_data/create_metadata_user.sql
+++ migtests/tests/oracle/import-data/schema_and_data/create_metadata_user.sql
@@ -1,13 +1,3 @@
-BEGIN
-    DECLARE
-        user_exists NUMBER;
-    BEGIN
-        SELECT COUNT(*) INTO user_exists FROM all_users WHERE username = UPPER('ybvoyager_metadata');
-        IF user_exists = 0 THEN
-            EXECUTE IMMEDIATE 'CREATE USER ybvoyager_metadata IDENTIFIED BY "password"';
-        END IF;
-    END;
-END;
-
+CREATE USER ybvoyager_metadata IDENTIFIED BY password;
 GRANT CONNECT, RESOURCE TO ybvoyager_metadata;
 ALTER USER ybvoyager_metadata QUOTA UNLIMITED ON USERS;
