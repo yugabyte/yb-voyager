4fc0756b49146133263d5ba4e9dfdf5bdc850854
Amit Jambure <ajambure@yugabyte.com>
2023-08-23 12:34:36 +0000

    Record migration status in the meta DB.

---
+++ yb-voyager/cmd/migration_status.go
@@ -0,0 +1,28 @@
+package cmd
+
+import "fmt"
+
+type MigrationStatusRecord struct {
+	MigrationUUID      string
+	SourceDBType       string
+	ExportType         string
+	FallForwarDBExists bool
+}
+
+const MIGRATION_STATUS_KEY = "migration_status"
+
+func UpdateMigrationStatusRecord(updateFn func(*MigrationStatusRecord)) error {
+	return UpdateJsonObjectInMetaDB(metaDB, MIGRATION_STATUS_KEY, updateFn)
+}
+
+func GetMigrationStatusRecord() (*MigrationStatusRecord, error) {
+	record := new(MigrationStatusRecord)
+	found, err := metaDB.GetJsonObject(MIGRATION_STATUS_KEY, record)
+	if err != nil {
+		return nil, fmt.Errorf("error while getting migration status record from meta db: %w", err)
+	}
+	if !found {
+		return nil, nil
+	}
+	return record, nil
+}
