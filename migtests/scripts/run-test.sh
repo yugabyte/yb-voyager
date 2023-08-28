ae0753803ace6edb22398125b662bbf2e477700f
Aneesh Makala <amakala@yugabyte.com>
2023-08-28 15:49:30 +0530

    correct log file name

--- migtests/scripts/run-test.sh
+++ migtests/scripts/run-test.sh
@@ -67,7 +67,7 @@ main() {
 	step "Export data."
 	# false if exit code of export_data is non-zero
 	export_data || { 
-		tail_log_file "yb-voyager.log"
+		tail_log_file "yb-voyager-export-data.log"
 		tail_log_file "debezium.log"
 		exit 1
 	}
