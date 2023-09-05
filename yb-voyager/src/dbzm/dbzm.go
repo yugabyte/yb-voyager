82eeed4b2991a33b0c71eb281b148bda195cac70
Priyanshi Gupta <pgupta@yugabyte.com>
2023-09-06 01:31:01 +0530

    set empty password in conf file instead of env variable (#1043)

--- yb-voyager/src/dbzm/dbzm.go
+++ yb-voyager/src/dbzm/dbzm.go
@@ -88,7 +88,9 @@ func (d *Debezium) Start() error {
 	d.cmd.Env = os.Environ()
 	// $TNS_ADMIN is used to set jdbc property oracle.net.tns_admin which will enable using TNS alias
 	d.cmd.Env = append(d.cmd.Env, fmt.Sprintf("TNS_ADMIN=%s", d.Config.TNSAdmin))
-	d.cmd.Env = append(d.cmd.Env, "DEBEZIUM_SOURCE_DATABASE_PASSWORD="+d.Config.Password)
+	if d.Config.Password != "" {
+		d.cmd.Env = append(d.cmd.Env, "DEBEZIUM_SOURCE_DATABASE_PASSWORD="+d.Config.Password)
+	}
 	log.Infof("Setting TNS_ADMIN=%s", d.Config.TNSAdmin)
 	if !d.Config.OracleJDBCWalletLocationSet {
 		// only specify the default value of this property if it is not already set in $TNS_ADMIN/ojdbc.properties.
