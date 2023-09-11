b176e09159df46ee2cfde41bbb2017685d4a10bd
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2023-09-11 13:08:46 +0530

    Changed voyager and dbzm version to 1.5.0 (#1095)

--- yb-voyager/src/utils/version.go
+++ yb-voyager/src/utils/version.go
@@ -17,7 +17,7 @@ package utils
 
 const (
 	// This constant must be updated on every release.
-	YB_VOYAGER_VERSION = "1.5.0-rc2"
+	YB_VOYAGER_VERSION = "1.5.0"
 
 	// @Refer: https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
 	GIT_COMMIT_HASH = "$Format:%H$"
