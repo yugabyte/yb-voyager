506297a62a15a4a03432a6772cb90bf398428bf4
Ubuntu <ubuntu@ip-10-9-15-240.us-west-2.compute.internal>
2023-10-04 06:11:14 +0000

    Minor change

--- migtests/tests/oracle/basic-live-test/env.sh
+++ migtests/tests/oracle/basic-live-test/env.sh
@@ -1,4 +1,4 @@
 export SOURCE_DB_TYPE="oracle"
 export SOURCE_DB_SCHEMA=${SOURCE_DB_SCHEMA:-"TEST_SCHEMA"}
-export TARGET_DB_NAME=${TARGET_DB_NAME:-"live_test"}
+export TARGET_DB_NAME=${TARGET_DB_NAME:-"live_test2"}
 
