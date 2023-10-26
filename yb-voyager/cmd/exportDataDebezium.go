3ff1b2b4ff1e4dbc29ca4b338dd730b0d431d623
Priyanshi Gupta <pgupta@yugabyte.com>
2023-10-26 12:35:49 +0530

    Deleting the yb cdc stream id after fall-forward/fall-back switchover (#1166)
    * Deleting the yb CDC stream ID after the fall-forward/fall-back switchover
    * Removing the stream ID deletion in automation

--- yb-voyager/cmd/exportDataDebezium.go
+++ yb-voyager/cmd/exportDataDebezium.go
@@ -39,6 +39,8 @@ import (
 	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils/sqlname"
 )
 
+var ybCDCClient *dbzm.YugabyteDBCDCClient
+
 func prepareDebeziumConfig(tableList []*sqlname.SourceName, tablesColumnList map[*sqlname.SourceName][]string) (*dbzm.Config, map[string]int64, error) {
 	runId = time.Now().String()
 	absExportDir, err := filepath.Abs(exportDir)
@@ -155,7 +157,7 @@ func prepareDebeziumConfig(tableList []*sqlname.SourceName, tablesColumnList map
 				return fmt.Sprintf("%s:%s", s, masterPort)
 			}),
 			)
-			ybCDCClient := dbzm.NewYugabyteDBCDCClient(exportDir, strings.Join(ybServers, ","), config.SSLRootCert, config.DatabaseName, config.TableList[0], metaDB)
+			ybCDCClient = dbzm.NewYugabyteDBCDCClient(exportDir, strings.Join(ybServers, ","), config.SSLRootCert, config.DatabaseName, config.TableList[0], metaDB)
 			err := ybCDCClient.Init()
 			if err != nil {
 				return nil, nil, fmt.Errorf("failed to initialize YugabyteDB CDC client: %w", err)
