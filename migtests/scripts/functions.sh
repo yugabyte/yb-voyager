b0dc18aed9b1e3f6c5a59a6e1626ba2003e7c01d
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2023-11-07 23:19:20 +0000

    Revert "temp commit to debug automation failure"
    This reverts commit fe652ee704a844d4fbca9e771e290d54fc75c535.

--- migtests/scripts/functions.sh
+++ migtests/scripts/functions.sh
@@ -370,21 +370,15 @@ end_migration() {
 	BACKUP_DIR=${EXPORT_DIR}/backup-dir
 	mkdir ${BACKUP_DIR}  # temporary place to store the backup
 
-	# setting env vars for passwords to be used for saving reports'
+	# setting env vars for passwords to be used for saving reports
 	export SOURCE_DB_PASSWORD=${SOURCE_DB_PASSWORD}
 	export TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD}
 	export FF_DB_PASSWORD=${FF_DB_PASSWORD}
-
-	echo "Priting password for saving reports part of end migration cmd"
-	echo "SOURCE_DB_PASSWORD=${SOURCE_DB_PASSWORD}"
-	echo "TARGET_DB_PASSWORD=${TARGET_DB_PASSWORD}"
-	echo "FF_DB_PASSWORD=${FF_DB_PASSWORD}"
 
 	yb-voyager end migration --export-dir ${EXPORT_DIR} \
 	--backup-dir ${BACKUP_DIR} --backup-schema-files true \
 	--backup-data-files true --backup-log-files true \
 	--save-migration-reports true $* || {
-		cat ${BACKUP_DIR}/reports/export_data_report.txt
 		cat ${EXPORT_DIR}/logs/yb-voyager-end-migration.log
 		exit 1
 	}
