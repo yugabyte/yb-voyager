31502d8c6a6995f92a3424a7c258b06cc6855d23
Shubham <centos@ip-10-9-6-84.us-west-2.compute.internal>
2023-11-09 07:43:43 +0000

    Minor change

--- yb-voyager/cmd/getDataMigrationReportCommand.go
+++ yb-voyager/cmd/getDataMigrationReportCommand.go
@@ -70,7 +70,7 @@ var getDataMigrationReportCmd = &cobra.Command{
 			}
 			getDataMigrationReportCmdFn(migrationStatus)
 		} else {
-			utils.ErrExit("Error: Data migration report is only applicable when export-type is 'snapshot-and-changes'(live migration)\nPlease run export data status/import data status")
+			utils.ErrExit("Error: Data migration report is only applicable when export-type is 'snapshot-and-changes'(live migration)\nPlease run export data status/import data status commands.")
 		}
 	},
 }
