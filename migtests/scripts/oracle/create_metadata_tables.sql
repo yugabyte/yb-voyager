7fb0cf6c0a30779dde1e5c07291a40f126dd62b3
Shubham Dabriwala <65844208+shubham-yb@users.noreply.github.com>
2023-11-17 12:00:01 +0530

    Added fall-back automation (#1201)

--- migtests/scripts/oracle/create_metadata_tables.sql
+++ migtests/scripts/oracle/create_metadata_tables.sql
@@ -1,6 +1,3 @@
-CREATE USER FF_SCHEMA IDENTIFIED BY "password";
-GRANT all privileges to FF_SCHEMA;
-
 CREATE USER ybvoyager_metadata IDENTIFIED BY "password";
 GRANT CONNECT, RESOURCE TO ybvoyager_metadata;
 ALTER USER ybvoyager_metadata QUOTA UNLIMITED ON USERS;
@@ -36,17 +33,3 @@ CREATE TABLE ybvoyager_metadata.ybvoyager_imported_event_count_by_table (
         num_deletes NUMBER(19),
         PRIMARY KEY (migration_uuid, table_name, channel_no)
     );
-
--- Grant all privileges on ybvoyager_metadata schema table to user ybvoyager
-
-DECLARE
-   v_sql VARCHAR2(4000);
-BEGIN
-   FOR table_rec IN (SELECT table_name FROM all_tables WHERE owner = 'YBVOYAGER_METADATA') LOOP
-      v_sql := 'GRANT ALL PRIVILEGES ON YBVOYAGER_METADATA.' || table_rec.table_name || ' TO C##YBVOYAGER';
-      EXECUTE IMMEDIATE v_sql;
-   END LOOP;
-END;
-/
-
-
--- migtests/scripts/oracle/ff_env.sh
+++ migtests/scripts/oracle/ff_env.sh
@@ -1,5 +1,5 @@
 export FF_DB_NAME=${FF_DB_NAME:-"ORCLPDB1"}
 export FF_DB_HOST=${FF_DB_HOST:-"localhost"}
-export FF_DB_USER=${FF_DB_USER:-"FF_SCHEMA"}
+export FF_DB_USER=${FF_DB_USER:-"YBVOYAGER_FF"}
 export FF_DB_PASSWORD=${FF_DB_PASSWORD:-"password"}
 export FF_DB_SCHEMA=${FF_DB_SCHEMA:-"FF_SCHEMA"}
