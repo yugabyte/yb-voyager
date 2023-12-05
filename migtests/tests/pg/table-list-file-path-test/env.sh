fac1143e8c455ebbc7c1ca03808f6803c33bbe4f
pgupta@yugabtye.com <centos@ip-10-9-14-120.us-west-2.compute.internal>
2023-12-05 20:11:28 +0000

    table list pg tests

---
+++ migtests/tests/pg/table-list-file-path-test/env.sh
@@ -0,0 +1,8 @@
+export SOURCE_DB_TYPE="postgresql"
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"table_list_flags_test"}
+export SOURCE_DB_SCHEMA="public"
+echo "test-dir" $TEST_DIR
+export EXPORT_TABLE_LIST_FILE_PATH=${EXPORT_TABLE_LIST_FILE_PATH:-$TEST_DIR/export-table-list.txt}
+export EXPORT_EX_TABLE_LIST_FILE_PATH=${EXPORT_EX_TABLE_LIST_FILE_PATH:-$TEST_DIR/export-ex-table-list.txt}
+export IMPORT_TABLE_LIST_FILE_PATH=${IMPORT_TABLE_LIST_FILE_PATH:-$TEST_DIR/import-table-list.txt}
+export IMPORT_EX_TABLE_LIST_FILE_PATH=${IMPORT_EX_TABLE_LIST_FILE_PATH:-$TEST_DIR/import-ex-table-list.txt}
