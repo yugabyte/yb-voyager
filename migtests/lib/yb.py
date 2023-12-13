d37f1e3109092b2511002078e33e825175dc5e78
Shubham Dabriwala <65844208+shubham-yb@users.noreply.github.com>
2023-12-13 18:26:23 +0530

    Added 'oracle' to the search_path for Orafce (#1240)

--- migtests/lib/yb.py
+++ migtests/lib/yb.py
@@ -85,10 +85,10 @@ class PostgresDB:
 		}[object_type]
 		cur = self.conn.cursor()
 		query= f"select relname from pg_class join pg_namespace on pg_class.relnamespace = pg_namespace.oid"
-		query += f" where nspname = '{schema_name}' AND relkind = '{object_type}'"			
+		query += f" where nspname = '{schema_name}' AND relkind = '{object_type}'"
 		if object_type == 'v':
 			query += f" AND relname NOT IN ({self.EXPECTED_ORAFCE_VIEWS})"
-		cur.execute(query)		
+		cur.execute(query)
 		return [obj[0] for obj in cur.fetchall()]
 	
 	def get_sum_of_column_of_table(self, table_name, column_name, schema_name="public") -> int:
