7b497b49b3b2858db7df289d893a2bb7b3ac1073
pgupta@yugabtye.com <centos@ip-10-9-14-120.us-west-2.compute.internal>
2023-12-14 12:33:36 +0000

    added automation tests

---
+++ migtests/tests/analyze-schema/dummy-export-dir/schema/extensions/extension.sql
@@ -0,0 +1,26 @@
+-- setting variables for current session
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+CREATE EXTENSION IF NOT EXISTS aws_commons WITH SCHEMA public;
+
+
+CREATE EXTENSION IF NOT EXISTS plperl WITH SCHEMA pg_catalog;
+
+
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
+
+
+CREATE EXTENSION IF NOT EXISTS hstore_plperl WITH SCHEMA public;
+
+
+CREATE EXTENSION IF NOT EXISTS pg_visibility WITH SCHEMA public;
+
