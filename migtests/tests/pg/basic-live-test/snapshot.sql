69260d0e35f2f376278d17b021e2934ceb880128
Sanyam Singhal <centos@ip-10-9-2-160.us-west-2.compute.internal>
2024-01-29 21:03:02 +0000

    Added automation test for conflict detection in case of oracle(fall-forward mig case) and pg migrations(forward mig case)

--- migtests/tests/pg/basic-live-test/snapshot.sql
+++ migtests/tests/pg/basic-live-test/snapshot.sql
@@ -2,3 +2,22 @@ create table x(id int primary key,id2 int);
 insert into x values(1,2);
 insert into x values(2,3);
 insert into x values(3,4);
+
+
+-- CREATE TABLE
+CREATE TABLE user_table (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
+    status VARCHAR(50) DEFAULT 'active'
+);
+
+-- INSERT INITIAL DATA
+INSERT INTO user_table (email) VALUES 
+    ('user1@example.com'), 
+    ('user2@example.com'), 
+    ('user3@example.com'),
+    ('user4@example.com'),
+    ('user5@example.com'),
+    ('user6@example.com'),
+    ('user7@example.com'),
+    ('user8@example.com');
