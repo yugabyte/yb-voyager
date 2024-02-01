f4e42b6ad6f06bb062bc7be277319db92f3e2139
Ubuntu <ubuntu@ip-10-9-117-96.us-west-2.compute.internal>
2024-02-01 11:47:35 +0000

    Move the tests into misc tests for pg, mysql and oracle

--- migtests/tests/mysql/misc-tests/schema.sql
+++ migtests/tests/mysql/misc-tests/schema.sql
@@ -143,3 +143,49 @@ insert into `Case_Sensitive_Columns` (`First_Name`, last_name, email, gender, ip
 insert into `Case_Sensitive_Columns` (`First_Name`, last_name, email, gender, ip_address) values ('Mitzi', 'Pidwell', 'mpidwell4@shutterfly.com', 'Female', '216.4.250.71');
 insert into `Case_Sensitive_Columns` (`First_Name`, last_name, email, gender, ip_address) values ('Milzie', 'Rohlfing', 'mrohlfing5@java.com', 'Female', '230.101.87.42');
 
+CREATE TABLE foo (
+    id   INTEGER PRIMARY KEY,
+    value TEXT
+);
+
+INSERT INTO foo (id, value) VALUES (1, '\r\nText with \r');
+INSERT INTO foo (id, value) VALUES (2, '\r\nText with \n');
+INSERT INTO foo (id, value) VALUES (3, '\r\nText with \r\n');
+INSERT INTO foo (id, value) VALUES (4, '\r\nText with \\r');
+INSERT INTO foo (id, value) VALUES (5, '\r\nText with \\n');
+INSERT INTO foo (id, value) VALUES (6, '\r\nText with \\r\\n');
+
+INSERT INTO foo (id, value) VALUES (7, 'Text with \r\n');
+INSERT INTO foo (id, value) VALUES (8, 'Text with \r\nText with \r');
+INSERT INTO foo (id, value) VALUES (9, 'Text with \r\nText with \n');
+INSERT INTO foo (id, value) VALUES (10, 'Text with \r\nText with \r\n');
+INSERT INTO foo (id, value) VALUES (11, 'Text with \r\nText with \\r');
+INSERT INTO foo (id, value) VALUES (12, 'Text with \r\nText with \\n');
+INSERT INTO foo (id, value) VALUES (13, 'Text with \r\nText with \\r\\n');
+
+INSERT INTO foo (id, value) VALUES (14, 'Text with \rText with \r\n');
+INSERT INTO foo (id, value) VALUES (15, 'Text with \rText with \n');
+INSERT INTO foo (id, value) VALUES (16, 'Text with \rText with \r\n');
+INSERT INTO foo (id, value) VALUES (17, 'Text with \rText with \\r');
+INSERT INTO foo (id, value) VALUES (18, 'Text with \rText with \\n');
+INSERT INTO foo (id, value) VALUES (19, 'Text with \rText with \\r\\n');
+
+INSERT INTO foo (id, value) VALUES (20, 'Text with \nText with \r');
+INSERT INTO foo (id, value) VALUES (21, 'Text with \nText with \n');
+INSERT INTO foo (id, value) VALUES (22, 'Text with \nText with \r\n');
+INSERT INTO foo (id, value) VALUES (23, 'Text with \nText with \\r');
+INSERT INTO foo (id, value) VALUES (24, 'Text with \nText with \\n');
+INSERT INTO foo (id, value) VALUES (25, 'Text with \nText with \\r\\n');
+
+INSERT INTO foo (id, value) VALUES (26, 'Text with \r\nText with \rText with \r\n');
+INSERT INTO foo (id, value) VALUES (27, 'Text with \r\nText with \nText with \r\n');
+INSERT INTO foo (id, value) VALUES (28, 'Text with \r\nText with \r\nText with \r\n');
+INSERT INTO foo (id, value) VALUES (29, 'Text with \r\nText with \\rText with \r\n');
+INSERT INTO foo (id, value) VALUES (30, 'Text with \r\nText with \\nText with \r\n');
+INSERT INTO foo (id, value) VALUES (31, 'Text with \r\nText with \\r\\nText with \r\n');
+
+INSERT INTO foo (id, value) VALUES (32, '\n\rText with \r');
+INSERT INTO foo (id, value) VALUES (33, 'Text with \n\r');
+INSERT INTO foo (id, value) VALUES (34, 'Text with \r\nText with \n\r');
+
+
--- migtests/tests/pg/carriage-return/env.sh
+++
@@ -1,3 +0,0 @@
-export SOURCE_DB_TYPE="postgresql"
-export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_carriage_return"}
-export SOURCE_DB_SCHEMA="public"
