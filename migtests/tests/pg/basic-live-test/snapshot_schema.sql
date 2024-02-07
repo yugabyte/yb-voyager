e351c878bb904305a2cddf503515122be8639f23
Cloud User <centos@ip-10-9-6-34.us-west-2.compute.internal>
2024-02-07 09:39:56 +0000

    Review comments

--- migtests/tests/pg/basic-live-test/snapshot_schema.sql
+++ migtests/tests/pg/basic-live-test/snapshot_schema.sql
@@ -1,9 +1,8 @@
 create table x(id int primary key,id2 int);
-ALTER TABLE user_table REPLICA IDENTITY FULL;
 
 CREATE TABLE user_table (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE,
     status VARCHAR(50) DEFAULT 'active'
 );
-ALTER TABLE user_table REPLICA IDENTITY FULL; 
+
