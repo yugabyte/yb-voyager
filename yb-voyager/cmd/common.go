da1ed5d08b2cc3b44ad00d0aea5b37ee9af898b8
Priyanshi Gupta <pgupta@yugabyte.com>
2024-02-09 11:19:47 +0530

    User facing  message change for erroring out when different voyager versions are used (#1325)

--- yb-voyager/cmd/common.go
+++ yb-voyager/cmd/common.go
@@ -352,8 +352,13 @@ func initMetaDB() {
 	}
 	if msr.VoyagerVersion != utils.YB_VOYAGER_VERSION {
 		userFacingMsg := fmt.Sprintf("Voyager requires the entire migration workflow to be executed using a single Voyager version.\n" +
-		"The export-dir %q was created using version %q and the current version is %q. Either use Voyager %q to continue the migration or start afresh" + 
+		"The export-dir %q was created using version %q and the current version is %q. Either use Voyager %q to continue the migration or start afresh " + 
 		"with a new export-dir.", exportDir, msr.VoyagerVersion, utils.YB_VOYAGER_VERSION, msr.VoyagerVersion)
+		if msr.VoyagerVersion == "" { //In case the export dir is already started from older version that will not have VoyagerVersion field in MSR
+			userFacingMsg = fmt.Sprintf("Voyager requires the entire migration workflow to be executed using a single Voyager version.\n" +
+		"The export-dir %q was created using older version and the current version is %q. Either use older version to continue the migration or start afresh " + 
+		"with a new export-dir.", exportDir, utils.YB_VOYAGER_VERSION)
+		}
 		utils.ErrExit(userFacingMsg)
 	}
 }
