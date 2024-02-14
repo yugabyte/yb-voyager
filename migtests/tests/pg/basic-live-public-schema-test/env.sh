d1c1c79a1854bf889588ddf67ec86629c05e9a7a
Sanyam Singhal <ssinghal@yugabyte.com>
2024-02-14 06:24:23 +0000

    Update PG test suite for live migration: added same test for public schema as of non public schema

---
+++ migtests/tests/pg/basic-live-public-schema-test/env.sh
@@ -0,0 +1,4 @@
+export SOURCE_DB_TYPE="postgresql"
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"live_test_pg"}
+export SOURCE_REPLICA_DB_NAME=${SOURCE_REPLICA_DB_NAME:-"ff_basic_test"}
+export SOURCE_DB_SCHEMA="public"
