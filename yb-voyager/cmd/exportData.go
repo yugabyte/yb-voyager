77197566114bf68a1c5aa297ad8a61508b323b0a
Aneesh Makala <amakala@yugabyte.com>
2024-02-29 12:12:18 +0530

    disable check

--- yb-voyager/cmd/exportData.go
+++ yb-voyager/cmd/exportData.go
@@ -159,11 +159,11 @@ func exportData() bool {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if source.DBType == POSTGRESQL && changeStreamingIsEnabled(exportType) {
-		if len(strings.Split(source.Schema, "|")) > 1 {
-			utils.ErrExit("This voyager release does not support live-migration for more than one schema in --source-db-schema.")
-		}
-	}
+	// if source.DBType == POSTGRESQL && changeStreamingIsEnabled(exportType) {
+	// 	if len(strings.Split(source.Schema, "|")) > 1 {
+	// 		utils.ErrExit("This voyager release does not support live-migration for more than one schema in --source-db-schema.")
+	// 	}
+	// }
 	finalTableList, tablesColumnList := getFinalTableColumnList()
 
 	if len(finalTableList) == 0 {
