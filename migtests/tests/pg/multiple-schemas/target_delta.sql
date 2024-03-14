12ab95097fd89ce63fbd2007d4419e1bd344cd1b
Cloud User <centos@ip-10-9-6-34.us-west-2.compute.internal>
2024-03-14 06:32:58 +0000

    Added commented failing test case

--- migtests/tests/pg/multiple-schemas/target_delta.sql
+++ migtests/tests/pg/multiple-schemas/target_delta.sql
@@ -1,11 +1,19 @@
--- insert into ext_test(password) values (crypt('cdcpassword', gen_salt('bf')));
 
 CALL tt_insert_data(10);
 DELETE from tt where i = 5;
 
+-- export from target fails for tables having enums+domains
+/*
+insert into Recipients(First_name,Last_name,Misc) values ('cdc','test','YES');
+*/
+
 ---- schema2
 set search_path to schema2;
 
--- insert into ext_test(password) values (crypt('cdcpassword', gen_salt('bf')));
-
 CALL tt_insert_data(10);
+
+-- export from target fails for tables having enums+domains
+/*
+insert into Recipients(First_name,Last_name,Misc) values ('cdc','test','YES');
+insert into Recipients(First_name,Last_name,Misc) values ('cdc2','test2','YES');
+*/
