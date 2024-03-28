22ac95095c7337076efd828ac777653f01c3a4ce
shaharuk-yb <sshaikh@yugabyte.com>
2024-03-28 19:25:10 +0530

    init2

--- yb-voyager/src/migassessment/common.go
+++ yb-voyager/src/migassessment/common.go
@@ -16,9 +16,12 @@ limitations under the License.
 package migassessment
 
 import (
+	"database/sql"
 	"encoding/csv"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/pelletier/go-toml/v2"
@@ -130,4 +133,56 @@ func LoadAssessmentParams(userInputFpath string) error {
 
 	utils.PrintAndLog("assessment params: %+v", assessmentParams)
 	return nil
+}
+
+func convertToMap(rows *sql.Rows) []map[string]interface{} {
+	columns, _ := rows.Columns()
+
+	// for each database row / record, a map with the column names and row values is added to the allMaps slice
+	var allMaps []map[string]interface{}
+
+	for rows.Next() {
+		values := make([]interface{}, len(columns))
+		pointers := make([]interface{}, len(columns))
+		for i, _ := range values {
+			pointers[i] = &values[i]
+		}
+		err := rows.Scan(pointers...)
+		checkErr(err)
+		resultMap := make(map[string]interface{})
+		for i, val := range values {
+			//fmt.Printf("Adding key=%s val=%v\n", columns[i], val)
+			resultMap[columns[i]] = val
+		}
+		allMaps = append(allMaps, resultMap)
+	}
+	return allMaps
+}
+
+func printMap(allMaps []map[string]interface{}) {
+	for _, v := range allMaps {
+		for k1, v1 := range v {
+			fmt.Printf("%v : %v\n", k1, v1)
+		}
+		fmt.Println()
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkInternetAccess() (ok bool) {
+	_, err := http.Get("http://clients3.google.com/generate_204")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func checkLocalFileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return !errors.Is(err, os.ErrNotExist)
 }
--- yb-voyager/src/migassessment/sizing.go
+++ yb-voyager/src/migassessment/sizing.go
@@ -17,17 +17,23 @@ limitations under the License.
 package migassessment
 
 import (
+	"database/sql"
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils"
+	"io"
+	"net/http"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 type SizingReport struct {
-	NodeCount        int
-	VcpuCountPerNode int
-	MemGBPerNode     int
-	StorageGBPerNode int
+	NumInstances      int
+	VcpuPerNode       int
+	MemGBPerNode      int
+	InsertConnections int
+	SelectConnections int
 }
 
 type SizingParams struct {
@@ -38,13 +44,15 @@ type SizingParams struct {
 }
 
 type SizingTableCountRecord struct {
-	Count string `json:"count"`
+	Count int `json:"count,string"`
 }
+
+var baseDownloadPath = "resources/remote/"
+var DB *sql.DB
 
 func SizingAssessment() error {
 	// load the assessment data
-	assessmentDataDir := filepath.Join(ExportDir, "assessment", "data")
-	tableCountFile := filepath.Join(assessmentDataDir, "sizing__num-tables-count.csv")
+	tableCountFile := filepath.Join(AssessmentDataDir, "sizing__num-tables-count.csv")
 
 	log.Infof("loading metadata files for sharding assessment")
 	tableCount, err := loadCSVDataFile[SizingTableCountRecord](tableCountFile)
@@ -54,16 +62,146 @@ func SizingAssessment() error {
 	}
 
 	utils.PrintAndLog("performing sizing assessment...")
+	inputs := make(map[string]int)
 	for _, tableC := range tableCount {
 		log.Infof("count: %+v", tableC)
+		inputs["count"] = tableC.Count
 	}
 	// core assessment logic goes here - maybe call some external package APIs to perform the assessment
+	Run("2.20", inputs)
+
 	FinalReport.SizingReport = &SizingReport{
 		// replace with actual assessment results
-		NodeCount:        3,
-		VcpuCountPerNode: 8,
-		MemGBPerNode:     16,
-		StorageGBPerNode: 250,
+		NumInstances:      3,
+		VcpuPerNode:       8,
+		MemGBPerNode:      16,
+		InsertConnections: 10,
+		SelectConnections: 12,
 	}
 	return nil
+}
+
+func Run(targetYbVersion string, inputs map[string]int) {
+	// read required inputs: may change from version to version
+	tables := inputs["tables"]
+	//requiredSelectThroughput := inputs["requiredSelectThroughput"]
+	//requiredInsertThroughput := inputs["requiredInsertThroughput"]
+
+	filePath := "src/migassessment/resources/yb_" + strings.ReplaceAll(targetYbVersion, ".", "_") + ".db"
+	if checkInternetAccess() {
+		remoteFileExists := checkFileExistsOnRemoteRepo(filePath)
+		if remoteFileExists {
+			// print the contents of the file
+			//fmt.Println(contents)
+			filePath = strings.ReplaceAll(filePath, "src/migassessment/resources/", baseDownloadPath)
+			fmt.Println("connect to downloaded data")
+		} else {
+			// check if local file exists
+			isFileExist := checkLocalFileExists(filePath)
+			if isFileExist {
+				fmt.Println("file exist locally")
+			} else {
+				fmt.Println("file doesn't exist locally")
+			}
+		}
+	} else {
+		// no network access
+		fmt.Println("No network access. Checking file locally...")
+		// check if local file exists
+		isFileExist := checkLocalFileExists(filePath)
+		if isFileExist {
+			fmt.Println("file exist locally")
+		} else {
+			fmt.Println("file doesn't exist locally")
+			panic("file doesn't exist locally")
+		}
+	}
+	err := ConnectDatabase(filePath)
+	checkErr(err)
+	//printRows()
+	checkTableLimits(tables)
+	//getThroughputData(2, requiredInsertThroughput, requiredSelectThroughput)
+}
+
+func printRows() {
+	rows, err := DB.Query("SELECT * from sizing limit 10")
+	if err != nil {
+		fmt.Println("no records found")
+	}
+	defer rows.Close()
+	allMaps := convertToMap(rows)
+	printMap(allMaps)
+
+	err = rows.Err()
+	if err != nil {
+		fmt.Println("error occurred")
+	}
+}
+
+func checkFileExistsOnRemoteRepo(fileName string) bool {
+	remotePath := "https://raw.githubusercontent.com/yb-voyager/sshaikh/mat/" + fileName
+	resp, _ := http.Get(remotePath)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != 200 {
+		fmt.Println("File does not exist on remote location")
+		return false
+	} else {
+		//body, err := io.ReadAll(resp.Body)
+		downloadPath := strings.ReplaceAll(fileName, "resources/", baseDownloadPath)
+		out, err := os.Create(downloadPath)
+		defer out.Close()
+		_, err = io.Copy(out, resp.Body)
+
+		if err != nil {
+			panic(err)
+		}
+		return true
+	}
+}
+
+func ConnectDatabase(file string) error {
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		return err
+	}
+	DB = db
+	return nil
+}
+
+func checkTableLimits(req_tables int) {
+	rows, err := DB.Query("select num_cores from sizing where num_tables > ? and dimension like '%TableLimits-3nodeRF=3%' order by num_cores", req_tables)
+	if err != nil {
+		fmt.Println("no records found")
+	}
+	defer rows.Close()
+	allMaps := convertToMap(rows)
+	printMap(allMaps)
+
+	err = rows.Err()
+	if err != nil {
+		fmt.Println("error occurred")
+	}
+}
+
+func getThroughputData(minCoresReq int, requiredInsertThroughput int, requiredSelectThroughput int) {
+	//rows, err := DB.Query("select foo.* from (select id, (cast(?/inserts_per_core) as int + ((?/inserts_per_core) > cast(?/inserts_per_core) as int)) insert_total_cores, (cast(?/selects_per_core) as int + ((?/selects_per_core) > cast(?/selects_per_core) as int) select_total_cores, num_cores, num_nodes from sizing where dimension='MaxThroughput' and num_cores>=?) as foo order by select_total_cores + insert_total_cores, num_cores", requiredInsertThroughput, requiredInsertThroughput, requiredInsertThroughput, requiredSelectThroughput, requiredSelectThroughput, requiredSelectThroughput, minCoresReq)
+	rows, err := DB.Query("select foo.* from (select id, ? , ?, num_cores, num_nodes from sizing where dimension='MaxThroughput' and num_cores>=?) as foo order by select_total_cores, insert_total_cores, num_cores", requiredInsertThroughput, requiredSelectThroughput, minCoresReq)
+	if err != nil {
+		fmt.Println("no records found")
+	}
+	defer rows.Close()
+	err = rows.Err()
+
+	allMaps := convertToMap(rows)
+	printMap(allMaps)
+	if err != nil {
+		fmt.Println("error occurred")
+	}
 }
