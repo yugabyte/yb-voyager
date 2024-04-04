19189fc7f37032bfde281a6a79a684ad3477e351
Cloud User <centos@ip-10-9-6-34.us-west-2.compute.internal>
2024-04-04 10:59:05 +0000

    Fixed Oracle target delta

--- migtests/tests/oracle/case-sensitivity-reserved-words/target_delta.sql
+++ migtests/tests/oracle/case-sensitivity-reserved-words/target_delta.sql
@@ -1,12 +1,12 @@
-INSERT INTO "lt_lc_uc" (id, "column_name", COLUMN_NAME2) VALUES (4, 'data7', 'data8');
-DELETE FROM "lt_lc_uc" WHERE id = 1;
-UPDATE "lt_lc_uc" SET "column_name" = 'updated_data', COLUMN_NAME2 = 'updated_data', id=1 WHERE id = 5;
+INSERT INTO lt_lc_uc (id, "column_name", COLUMN_NAME2) VALUES (4, 'data7', 'data8');
+DELETE FROM lt_lc_uc WHERE id = 1;
+UPDATE lt_lc_uc SET "column_name" = 'updated_data', COLUMN_NAME2 = 'updated_data', id=1 WHERE id = 5;
 
 
-DELETE FROM "lt_rwc" WHERE id = 3;
-INSERT INTO "lt_rwc" (id, "user", limit, "number", "Column_Name") VALUES (3, 'user3', 'description3', '98765', 'column3');
-DELETE FROM "lt_rwc" WHERE id = 4;
-UPDATE "lt_rwc" SET "user" = 'updated_user2', limit = 'updated_description2', "number" = 'updated_number2', "Column_Name" = 'updated_column2', id=4 WHERE id = 2;
+DELETE FROM lt_rwc WHERE id = 3;
+INSERT INTO lt_rwc (id, "user", "limit", number, column_name) VALUES (3, 'user3', 'description3', '98765', 'column3');
+DELETE FROM lt_rwc WHERE id = 4;
+UPDATE lt_rwc SET "user" = 'updated_user2', "limit" = 'updated_description2', number = 'updated_number2', column_name = 'updated_column2', id=4 WHERE id = 2;
 
 
 INSERT INTO UT_UC (id, COLUMN_NAME) VALUES (4, 'value4');
@@ -14,42 +14,42 @@ UPDATE UT_UC SET COLUMN_NAME = 'updated_value2',id=2 WHERE id = 3;
 DELETE FROM UT_UC WHERE id = 4;
 
 
-INSERT INTO UT_MC (id, "Column_Name") VALUES (4, 'value4');
+INSERT INTO UT_MC (id, column_name) VALUES (4, 'value4');
 DELETE FROM UT_MC WHERE id = 4;
-UPDATE UT_MC SET "Column_Name" = 'updated_value',id=4 WHERE id = 1;
+UPDATE UT_MC SET column_name = 'updated_value',id=4 WHERE id = 1;
 
 
-INSERT INTO UT_RWC (id, "user", limit, "Column_Name") VALUES (4, 'user4', 'description4', 'column4');
+INSERT INTO UT_RWC (id, "user", "limit", column_name) VALUES (4, 'user4', 'description4', 'column4');
 UPDATE UT_RWC SET "user" = 'updated_user' WHERE id = 4;
 DELETE FROM UT_RWC WHERE id = 4;
-UPDATE UT_RWC SET "user" = 'updated_user', limit = 'updated_description', "Column_Name" = 'updated_column' WHERE id = 5;
-DELETE FROM UT_RWC WHERE limit = 'updated_description';
+UPDATE UT_RWC SET "user" = 'updated_user', "limit" = 'updated_description', column_name = 'updated_column' WHERE id = 5;
+DELETE FROM UT_RWC WHERE "limit" = 'updated_description';
 
 
-INSERT INTO "Mt_Rwc" (id, "user", "column_name", limit) VALUES (4, 'user4', 'column4', 'description4');
-UPDATE "Mt_Rwc" SET "user" = 'updated_user', "column_name" = 'updated_column', limit = 'updated_description' WHERE id = 4;
-UPDATE "Mt_Rwc" SET "user" = 'updated_user', limit = 'updated_description' WHERE id = 1;
-DELETE FROM "Mt_Rwc" WHERE "user" = 'updated_user';
+INSERT INTO mt_rwc (id, "user", "column_name", "limit") VALUES (4, 'user4', 'column4', 'description4');
+UPDATE mt_rwc SET "user" = 'updated_user', "column_name" = 'updated_column', "limit" = 'updated_description' WHERE id = 4;
+UPDATE mt_rwc SET "user" = 'updated_user', "limit" = 'updated_description' WHERE id = 1;
+DELETE FROM mt_rwc WHERE "user" = 'updated_user';
 
 
-INSERT INTO "CASE" (id, "USER") VALUES (3, 'user3');
-INSERT INTO "CASE" (id, "USER", "case", limit) VALUES (4, 'user4', 'case4', 'description4');
-UPDATE "CASE" SET "USER" = NULL, "case" = NULL, limit = NULL WHERE id = 6;
-UPDATE "CASE" SET "USER" = 'updated_user', "case" = 'updated_case_again', limit = 'updated_description' WHERE id = 2;
-DELETE FROM "CASE" WHERE "case" = 'updated_case_again';
+INSERT INTO "case" (id, "user") VALUES (3, 'user3');
+INSERT INTO "case" (id, "user", "case", "limit") VALUES (4, 'user4', 'case4', 'description4');
+UPDATE "case" SET "user" = NULL, "case" = NULL, "limit" = NULL WHERE id = 6;
+UPDATE "case" SET "user" = 'updated_user', "case" = 'updated_case_again', "limit" = 'updated_description' WHERE id = 2;
+DELETE FROM "case" WHERE "case" = 'updated_case_again';
 
 
-INSERT INTO "number" (id, "Column_Name", "case", limit) VALUES (5, NULL,NULL,NULL);
-INSERT INTO "number" (id, "Column_Name", "case", limit) VALUES (1, 'column4', 'case4', 'description4');
-DELETE FROM "number" WHERE "Column_Name" = 'column4';
-UPDATE "number" SET "Column_Name" = 'updated_column', "case" = 'updated_case', limit = 'updated_description' WHERE id = 5;
+INSERT INTO number (id, column_name, "case", "limit") VALUES (5, NULL,NULL,NULL);
+INSERT INTO number (id, column_name, "case", "limit") VALUES (1, 'column4', 'case4', 'description4');
+DELETE FROM number WHERE column_name = 'column4';
+UPDATE number SET column_name = 'updated_column', "case" = 'updated_case', "limit" = 'updated_description' WHERE id = 5;
 
 
-DELETE FROM limit WHERE "case" IS NULL;
-INSERT INTO limit (id, "Column_Name", "case", limit) VALUES (5, 'column4', 'case4', 'description4');
-UPDATE limit SET "Column_Name" = 'updated_column', limit = 'updated_description',id=3 WHERE id = 8;
-UPDATE limit SET "Column_Name" = 'updated_column', "case" = 'updated_case', limit = 'updated_description' WHERE id = 2;
-INSERT INTO limit (id, "Column_Name", "case", limit) VALUES (15, NULL,'case',NULL);
+DELETE FROM "limit" WHERE "case" IS NULL;
+INSERT INTO "limit" (id, column_name, "case", "limit") VALUES (5, 'column4', 'case4', 'description4');
+UPDATE "limit" SET column_name = 'updated_column', "limit" = 'updated_description',id=3 WHERE id = 8;
+UPDATE "limit" SET column_name = 'updated_column', "case" = 'updated_case', "limit" = 'updated_description' WHERE id = 2;
+INSERT INTO "limit" (id, column_name, "case", "limit") VALUES (15, NULL,'case',NULL);
 
 
 INSERT INTO "RowId" (id, "TABLE") VALUES (3, 'table3');
