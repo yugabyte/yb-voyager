4dca92d9721d46e2bdd07217870e708550e8ad9c
shaharuk-yb <sshaikh@yugabyte.com>
2024-06-06 16:16:14 +0530

    fixing variable naming

--- yb-voyager/src/migassessment/sizing.go
+++ yb-voyager/src/migassessment/sizing.go
@@ -322,7 +322,7 @@ func findNumNodesNeededBasedOnTabletsRequired(sourceIndexMetadata []SourceDBMeta
 	shardedLimits []ExpDataShardedLimit,
 	recommendation map[int]IntermediateRecommendation) map[int]IntermediateRecommendation {
 	// Iterate over each intermediate recommendation where failureReasoning is empty
-	for index, rec := range recommendation {
+	for i, rec := range recommendation {
 		totalTabletsRequired := 0
 		if len(rec.ShardedTables) != 0 && rec.FailureReasoning == "" {
 			// Iterate over each table and its indexes to find out how many tablets are needed
@@ -347,7 +347,7 @@ func findNumNodesNeededBasedOnTabletsRequired(sourceIndexMetadata []SourceDBMeta
 					nodesRequired := math.Ceil(float64(totalTabletsRequired*3) / float64(record.maxSupportedNumTables.Int64))
 					// update recommendation to use the maximum of the existing recommended nodes and nodes calculated based on tablets
 					rec.NumNodes = math.Max(rec.NumNodes, nodesRequired)
-					recommendation[index] = rec
+					recommendation[i] = rec
 				}
 			}
 		}
