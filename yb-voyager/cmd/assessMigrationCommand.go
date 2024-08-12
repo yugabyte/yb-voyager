yugabyte/yb-voyager

commit dfc43241aad44f2a4072d2f2a90f14cd1db34a96

Bug Fix[DB-12365]: Validate Schema Existence Before Migration Assessment and Export Schema/Data (#1620)
- Assess Migration: Fixed unclear output displaying that metadata was collected when schema is missing.
- Export Schema [Oracle]: Improved erroring handling and messaging for nonexistent schema.
- Export Data [Oracle]: Prevented misleading 'no tables present to export, exiting...' message by checking schema existence first.

Ensures early schema validation, clearer error messages, and avoids unnecessary operations.

yb-voyager/cmd/assessMigrationCommand.go
@@ -271,15 +271,27 @@ func assessMigration() (err error) {
 	assessmentDir := filepath.Join(exportDir, "assessment")
 	migassessment.AssessmentDir = assessmentDir
 	migassessment.SourceDBType = source.DBType
+
+	if assessmentMetadataDirFlag == "" { // only in case of source connectivity
+		err := source.DB().Connect()
+		if err != nil {
+			utils.ErrExit("error connecting source db: %v", err)
+		}
+
+		res := source.DB().CheckSchemaExists()
+		if !res {
+			return fmt.Errorf("schema %q does not exist", source.Schema)
+		}
+		fetchSourceInfo()
+
+		source.DB().Disconnect()
+	}
+
 	initAssessmentDB() // Note: migassessment.AssessmentDir needs to be set beforehand
 
 	err = gatherAssessmentMetadata()
 	if err != nil {
 		return fmt.Errorf("failed to gather assessment metadata: %w", err)
-	}
-
-	if assessmentMetadataDirFlag == "" { // only in case of source connectivity
-		fetchSourceInfo()
 	}
 
 	parseExportedSchemaFileForAssessmentIfRequired()
@@ -310,16 +322,12 @@ func assessMigration() (err error) {
 }
 
 func fetchSourceInfo() {
-	err := source.DB().Connect()
-	if err != nil {
-		utils.ErrExit("error connecting source db: %v", err)
-	}
+	var err error
 	source.DBVersion = source.DB().GetVersion()
 	source.DBSize, err = source.DB().GetDatabaseSize()
 	if err != nil {
 		log.Errorf("error getting database size: %v", err) //can just log as this is used for call-home only
 	}
-	source.DB().Disconnect()
 }
 
 func SetMigrationAssessmentDoneInMSR() error {

yb-voyager/src/srcdb/oracle.go
@@ -65,6 +65,19 @@ func (ora *Oracle) Disconnect() {
 	}
 }
 
+func (ora *Oracle) CheckSchemaExists() bool {
+	schemaName := ora.source.Schema
+	query := fmt.Sprintf(`SELECT username FROM ALL_USERS WHERE username = '%s'`, strings.ToUpper(schemaName))
+	var schema string
+	err := ora.db.QueryRow(query).Scan(&schema)
+	if err == sql.ErrNoRows {
+		return false
+	} else if err != nil {
+		log.Fatalf("error in querying source database for schema %q: %v\n", schemaName, err)
+	}
+	return true
+}
+
 func (ora *Oracle) CheckRequiredToolsAreInstalled() {
 	checkTools("ora2pg", "sqlplus")
 }

yb-voyager/cmd/exportData.go
@@ -211,12 +211,19 @@ func exportData() bool {
 	if err != nil {
 		utils.ErrExit("Failed to connect to the source db: %s", err)
 	}
+	defer source.DB().Disconnect()
+
 	source.DBVersion = source.DB().GetVersion()
 	source.DBSize, err = source.DB().GetDatabaseSize()
 	if err != nil {
 		log.Errorf("error getting database size: %v", err) //can just log as this is used for call-home only
 	}
-	defer source.DB().Disconnect()
+
+	res := source.DB().CheckSchemaExists()
+	if !res {
+		utils.ErrExit("schema %q does not exist", source.Schema)
+	}
+
 	clearMigrationStateIfRequired()
 	checkSourceDBCharset()
 	source.DB().CheckRequiredToolsAreInstalled()

yb-voyager/cmd/exportSchema.go
@@ -96,8 +96,8 @@ func exportSchema() error {
 		log.Errorf("failed to connect to the source db: %s", err)
 		return fmt.Errorf("failed to connect to the source db: %w", err)
 	}
-
 	defer source.DB().Disconnect()
+
 	checkSourceDBCharset()
 	source.DB().CheckRequiredToolsAreInstalled()
 	sourceDBVersion := source.DB().GetVersion()
@@ -107,6 +107,12 @@ func exportSchema() error {
 		log.Errorf("error getting database size: %v", err) //can just log as this is used for call-home only
 	}
 	utils.PrintAndLog("%s version: %s\n", source.DBType, sourceDBVersion)
+
+	res := source.DB().CheckSchemaExists()
+	if !res {
+		return fmt.Errorf("schema %q does not exist", source.Schema)
+	}
+
 	err = retrieveMigrationUUID()
 	if err != nil {
 		log.Errorf("failed to get migration UUID: %v", err)

yb-voyager/src/srcdb/mysql.go
@@ -65,6 +65,12 @@ func (ms *MySQL) Disconnect() {
 	}
 }
 
+func (ms *MySQL) CheckSchemaExists() bool {
+	// no concept of schema in MySQL, only database
+	// also if Connect() passed already that means database is present
+	return true
+}
+
 func (ms *MySQL) CheckRequiredToolsAreInstalled() {
 	checkTools("ora2pg")
 }

yb-voyager/src/srcdb/postgres.go
@@ -145,6 +145,11 @@ func (pg *PostgreSQL) GetVersion() string {
 	return version
 }
 
+func (pg *PostgreSQL) CheckSchemaExists() bool {
+	schemaList := pg.checkSchemasExists()
+	return schemaList != nil
+}
+
 func (pg *PostgreSQL) checkSchemasExists() []string {
 	list := strings.Split(pg.source.Schema, "|")
 	var trimmedList []string

yb-voyager/src/srcdb/srcdb.go
@@ -29,6 +29,7 @@ import (
 type SourceDB interface {
 	Connect() error
 	Disconnect()
+	CheckSchemaExists() bool
 	GetConnectionUriWithoutPassword() string
 	GetTableRowCount(tableName sqlname.NameTuple) int64
 	GetTableApproxRowCount(tableName sqlname.NameTuple) int64

yb-voyager/src/srcdb/yugabytedb.go
@@ -109,6 +109,11 @@ func (yb *YugabyteDB) GetVersion() string {
 	return version
 }
 
+func (yb *YugabyteDB) CheckSchemaExists() bool {
+	schemaList := yb.checkSchemasExists()
+	return schemaList != nil
+}
+
 func (yb *YugabyteDB) checkSchemasExists() []string {
 	list := strings.Split(yb.source.Schema, "|")
 	var trimmedList []string

commit a2d1579c35c0d2c730b4fcdad787850f64440cc2

Fix: Regression cause by commit dfc43241aad44f2a4072d2f2a90f14cd1db34a96
 This commit fixed a regression introduced in commit dfc43241aad44f2a4072d2f2a90f14cd1db34a96 because of which the assess-migration command was exiting without error msg if SOURCE_DB_PASSWORD is not provided as flag

- with this fix it will prompt for the password if not provided through flag of env var

yb-voyager/cmd/assessMigrationCommand.go
@@ -272,6 +272,13 @@ func assessMigration() (err error) {
 	migassessment.AssessmentDir = assessmentDir
 	migassessment.SourceDBType = source.DBType
 
+	if source.Password == "" {
+		source.Password, err = askPassword("source DB", source.User, "SOURCE_DB_PASSWORD")
+		if err != nil {
+			return fmt.Errorf("failed to get source DB password: %w", err)
+		}
+	}
+
 	if assessmentMetadataDirFlag == "" { // only in case of source connectivity
 		err := source.DB().Connect()
 		if err != nil {
@@ -501,13 +508,6 @@ func gatherAssessmentMetadata() (err error) {
 	// setting schema objects types to export before creating the project directories
 	source.ExportObjectTypeList = utils.GetExportSchemaObjectList(source.DBType)
 	CreateMigrationProjectIfNotExists(source.DBType, exportDir)
-
-	if source.Password == "" {
-		source.Password, err = askPassword("source DB", source.User, "SOURCE_DB_PASSWORD")
-		if err != nil {
-			return fmt.Errorf("failed to get source DB password: %w", err)
-		}
-	}
 
 	utils.PrintAndLog("gathering metadata and stats from '%s' source database...", source.DBType)
 	switch source.DBType {

yb-voyager/src/srcdb/oracle.go
@@ -73,7 +73,7 @@ func (ora *Oracle) CheckSchemaExists() bool {
 	if err == sql.ErrNoRows {
 		return false
 	} else if err != nil {
-		log.Fatalf("error in querying source database for schema %q: %v\n", schemaName, err)
+		utils.ErrExit("error in querying source database for schema %q: %v\n", schemaName, err)
 	}
 	return true
 }