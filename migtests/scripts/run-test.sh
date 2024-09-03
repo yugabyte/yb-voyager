3657cb2588be3fc8f4e75fa8f1ee5bbe90cf8f5e
priyanshi-yb <pgupta@yugabyte.com>
2024-09-03 23:36:38 +0530

    added script in the scripts dir

--- migtests/scripts/run-test.sh
+++ migtests/scripts/run-test.sh
@@ -127,9 +127,9 @@ main() {
 		run_ysql yugabyte "CREATE DATABASE ${TARGET_DB_NAME}"
 	fi
 
-	if [ -x "${TEST_DIR}/add-pk-from-alter-to-create" ]
+	if [ -x "${SCRIPTS}/add-pk-from-alter-to-create" ]
 	then
-		"${TEST_DIR}/add-pk-from-alter-to-create"
+		"${SCRIPTS}/add-pk-from-alter-to-create"
 	fi
 
 	step "Import schema."
