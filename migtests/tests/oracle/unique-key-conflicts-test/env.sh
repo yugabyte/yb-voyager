54c39683350a418807fbeb849ab806d2850e7f5e
Sanyam Singhal <ssinghal@yugabyte.com>
2024-09-10 19:18:09 +0000

    Updated env.sh for oracle-unique-key-conflicts-test

--- migtests/tests/oracle/unique-key-conflicts-test/env.sh
+++ migtests/tests/oracle/unique-key-conflicts-test/env.sh
@@ -1,4 +1,4 @@
-export SOURCE_DB_TYPE="postgresql"
-export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"unique_key_conflict_cases"}
-export SOURCE_DB_SCHEMA="public,non_public"
-export SOURCE_REPLICA_DB_NAME="unique_key_conflict_cases_replica"
+export SOURCE_DB_TYPE="oracle"
+export SOURCE_DB_SCHEMA=${SOURCE_DB_SCHEMA:-"TEST_SCHEMA"}
+export TARGET_DB_NAME=${TARGET_DB_NAME:-"live_test"}
+export TARGET_DB_SCHEMA="test_schema"
