bc1b0b2f3cc41eb77b925faa39f83c804d38192e
Sanyam Singhal <ssinghal@yugabyte.com>
2024-09-11 00:12:09 +0000

    fixed the length of table names more than 30 characters hitting Logminer limit

--- migtests/tests/oracle/unique-key-conflicts-test/snapshot_data.sql
+++ migtests/tests/oracle/unique-key-conflicts-test/snapshot_data.sql
@@ -1,41 +1,43 @@
--- Insert data for single_unique_constraint
-INSERT INTO single_unique_constraint (email) VALUES ('user1@example.com');
-INSERT INTO single_unique_constraint (email) VALUES ('user2@example.com');
-INSERT INTO single_unique_constraint (email) VALUES ('user3@example.com');
-INSERT INTO single_unique_constraint (email) VALUES ('user4@example.com');
-INSERT INTO single_unique_constraint (email) VALUES ('user5@example.com');
+-- Insert data for single_unique_constr
+INSERT INTO single_unique_constr (email) VALUES ('user1@example.com');
+INSERT INTO single_unique_constr (email) VALUES ('user2@example.com');
+INSERT INTO single_unique_constr (email) VALUES ('user3@example.com');
+INSERT INTO single_unique_constr (email) VALUES ('user4@example.com');
+INSERT INTO single_unique_constr (email) VALUES ('user5@example.com');
 
--- Insert data for multi_unique_constraint
-INSERT INTO multi_unique_constraint (first_name, last_name) VALUES ('John', 'Doe');
-INSERT INTO multi_unique_constraint (first_name, last_name) VALUES ('Jane', 'Smith');
-INSERT INTO multi_unique_constraint (first_name, last_name) VALUES ('Bob', 'Johnson');
-INSERT INTO multi_unique_constraint (first_name, last_name) VALUES ('Alice', 'Williams');
-INSERT INTO multi_unique_constraint (first_name, last_name) VALUES ('Tom', 'Clark');
+-- Insert data for multi_unique_constr
+INSERT INTO multi_unique_constr (first_name, last_name) VALUES ('John', 'Doe');
+INSERT INTO multi_unique_constr (first_name, last_name) VALUES ('Jane', 'Smith');
+INSERT INTO multi_unique_constr (first_name, last_name) VALUES ('Bob', 'Johnson');
+INSERT INTO multi_unique_constr (first_name, last_name) VALUES ('Alice', 'Williams');
+INSERT INTO multi_unique_constr (first_name, last_name) VALUES ('Tom', 'Clark');
 
--- Insert data for single_unique_index
-INSERT INTO single_unique_index (ssn) VALUES ('SSN1');
-INSERT INTO single_unique_index (ssn) VALUES ('SSN2');
-INSERT INTO single_unique_index (ssn) VALUES ('SSN3');
-INSERT INTO single_unique_index (ssn) VALUES ('SSN4');
-INSERT INTO single_unique_index (ssn) VALUES ('SSN5');
+-- Insert data for single_unique_idx
+INSERT INTO single_unique_idx (ssn) VALUES ('SSN1');
+INSERT INTO single_unique_idx (ssn) VALUES ('SSN2');
+INSERT INTO single_unique_idx (ssn) VALUES ('SSN3');
+INSERT INTO single_unique_idx (ssn) VALUES ('SSN4');
+INSERT INTO single_unique_idx (ssn) VALUES ('SSN5');
 
--- Insert data for multi_unique_index
-INSERT INTO multi_unique_index (first_name, last_name) VALUES ('John', 'Doe');
-INSERT INTO multi_unique_index (first_name, last_name) VALUES ('Jane', 'Smith');
-INSERT INTO multi_unique_index (first_name, last_name) VALUES ('Bob', 'Johnson');
-INSERT INTO multi_unique_index (first_name, last_name) VALUES ('Alice', 'Williams');
-INSERT INTO multi_unique_index (first_name, last_name) VALUES ('Tom', 'Clark');
+-- Insert data for multi_unique_idx
+INSERT INTO multi_unique_idx (first_name, last_name) VALUES ('John', 'Doe');
+INSERT INTO multi_unique_idx (first_name, last_name) VALUES ('Jane', 'Smith');
+INSERT INTO multi_unique_idx (first_name, last_name) VALUES ('Bob', 'Johnson');
+INSERT INTO multi_unique_idx (first_name, last_name) VALUES ('Alice', 'Williams');
+INSERT INTO multi_unique_idx (first_name, last_name) VALUES ('Tom', 'Clark');
 
--- Insert data for different_columns_unique_constraint_and_index
-INSERT INTO different_columns_unique_constraint_and_index (email, phone_number) VALUES ('user1@example.com', '555-555-5551');
-INSERT INTO different_columns_unique_constraint_and_index (email, phone_number) VALUES ('user2@example.com', '555-555-5552');
-INSERT INTO different_columns_unique_constraint_and_index (email, phone_number) VALUES ('user3@example.com', '555-555-5553');
-INSERT INTO different_columns_unique_constraint_and_index (email, phone_number) VALUES ('user4@example.com', '555-555-5554');
-INSERT INTO different_columns_unique_constraint_and_index (email, phone_number) VALUES ('user5@example.com', '555-555-5555');
+-- Insert data for diff_columns_constr_idx
+INSERT INTO diff_columns_constr_idx (email, phone_number) VALUES ('user1@example.com', '555-555-5551');
+INSERT INTO diff_columns_constr_idx (email, phone_number) VALUES ('user2@example.com', '555-555-5552');
+INSERT INTO diff_columns_constr_idx (email, phone_number) VALUES ('user3@example.com', '555-555-5553');
+INSERT INTO diff_columns_constr_idx (email, phone_number) VALUES ('user4@example.com', '555-555-5554');
+INSERT INTO diff_columns_constr_idx (email, phone_number) VALUES ('user5@example.com', '555-555-5555');
 
--- Insert data for subset_columns_unique_constraint_and_index
-INSERT INTO subset_columns_unique_constraint_and_index (first_name, last_name, phone_number) VALUES ('John', 'Doe', '123-456-7890');
-INSERT INTO subset_columns_unique_constraint_and_index (first_name, last_name, phone_number) VALUES ('Jane', 'Smith', '123-456-7891');
-INSERT INTO subset_columns_unique_constraint_and_index (first_name, last_name, phone_number) VALUES ('Bob', 'Johnson', '123-456-7892');
-INSERT INTO subset_columns_unique_constraint_and_index (first_name, last_name, phone_number) VALUES ('Alice', 'Williams', '123-456-7893');
-INSERT INTO subset_columns_unique_constraint_and_index (first_name, last_name, phone_number) VALUES ('Tom', 'Clark', '123-456-7894');
+-- Insert data for subset_columns_constr_idx
+INSERT INTO subset_columns_constr_idx (first_name, last_name, phone_number) VALUES ('John', 'Doe', '123-456-7890');
+INSERT INTO subset_columns_constr_idx (first_name, last_name, phone_number) VALUES ('Jane', 'Smith', '123-456-7891');
+INSERT INTO subset_columns_constr_idx (first_name, last_name, phone_number) VALUES ('Bob', 'Johnson', '123-456-7892');
+INSERT INTO subset_columns_constr_idx (first_name, last_name, phone_number) VALUES ('Alice', 'Williams', '123-456-7893');
+INSERT INTO subset_columns_constr_idx (first_name, last_name, phone_number) VALUES ('Tom', 'Clark', '123-456-7894');
+
+COMMIT;
--- migtests/tests/oracle/unique-key-conflicts-test/source_delta.sql
+++ migtests/tests/oracle/unique-key-conflicts-test/source_delta.sql
@@ -1,77 +1,101 @@
--- Conflicts for single_unique_constraint
-DELETE FROM single_unique_constraint WHERE id = 1;
-INSERT INTO single_unique_constraint (id, email) VALUES (6, 'user1@example.com');  -- DELETE-INSERT conflict
+-- Conflicts for single_unique_constr
+DELETE FROM single_unique_constr WHERE id = 1;
+INSERT INTO single_unique_constr (id, email) VALUES (6, 'user1@example.com');  -- DELETE-INSERT conflict
+COMMIT;
 
-DELETE FROM single_unique_constraint WHERE id = 2;
-UPDATE single_unique_constraint SET email = 'user2@example.com' WHERE id = 3;  -- DELETE-UPDATE conflict
+DELETE FROM single_unique_constr WHERE id = 2;
+UPDATE single_unique_constr SET email = 'user2@example.com' WHERE id = 3;  -- DELETE-UPDATE conflict
+COMMIT;
 
-UPDATE single_unique_constraint SET email = 'updated_user4@example.com' WHERE id = 4;
-INSERT INTO single_unique_constraint (id, email) VALUES (7, 'user4@example.com');  -- UPDATE-INSERT conflict
+UPDATE single_unique_constr SET email = 'updated_user4@example.com' WHERE id = 4;
+INSERT INTO single_unique_constr (id, email) VALUES (7, 'user4@example.com');  -- UPDATE-INSERT conflict
+COMMIT;
 
-UPDATE single_unique_constraint SET email = 'updated_user5@example.com' WHERE id = 5;
-UPDATE single_unique_constraint SET email = 'user5@example.com' WHERE id = 6;  -- UPDATE-UPDATE conflict
+UPDATE single_unique_constr SET email = 'updated_user5@example.com' WHERE id = 5;
+UPDATE single_unique_constr SET email = 'user5@example.com' WHERE id = 6;  -- UPDATE-UPDATE conflict
+COMMIT;
 
--- Conflicts for multi_unique_constraint
-DELETE FROM multi_unique_constraint WHERE id = 1;
-INSERT INTO multi_unique_constraint (id, first_name, last_name) VALUES (6, 'John', 'Doe');  -- DELETE-INSERT conflict
+-- Conflicts for multi_unique_constr
+DELETE FROM multi_unique_constr WHERE id = 1;
+INSERT INTO multi_unique_constr (id, first_name, last_name) VALUES (6, 'John', 'Doe');  -- DELETE-INSERT conflict
+COMMIT;
 
-DELETE FROM multi_unique_constraint WHERE id = 2;
-UPDATE multi_unique_constraint SET first_name = 'Jane', last_name = 'Smith' WHERE id = 4;  -- DELETE-UPDATE conflict
+DELETE FROM multi_unique_constr WHERE id = 2;
+UPDATE multi_unique_constr SET first_name = 'Jane', last_name = 'Smith' WHERE id = 4;  -- DELETE-UPDATE conflict
+COMMIT;
 
-UPDATE multi_unique_constraint SET first_name = 'Updated_Tom' WHERE id = 5;
-INSERT INTO multi_unique_constraint (id, first_name, last_name) VALUES (7, 'Tom', 'Clark');  -- UPDATE-INSERT conflict
+UPDATE multi_unique_constr SET first_name = 'Updated_Tom' WHERE id = 5;
+INSERT INTO multi_unique_constr (id, first_name, last_name) VALUES (7, 'Tom', 'Clark');  -- UPDATE-INSERT conflict
+COMMIT;
 
-UPDATE multi_unique_constraint SET first_name = 'Updated_Alice' WHERE id = 4;
-UPDATE multi_unique_constraint SET first_name = 'Alice', last_name = 'Williams' WHERE id = 5;  -- UPDATE-UPDATE conflict
+UPDATE multi_unique_constr SET first_name = 'Updated_Alice' WHERE id = 4;
+UPDATE multi_unique_constr SET first_name = 'Alice', last_name = 'Williams' WHERE id = 5;  -- UPDATE-UPDATE conflict
+COMMIT;
 
--- Conflicts for single_unique_index
-DELETE FROM single_unique_index WHERE id = 1;
-INSERT INTO single_unique_index (id, ssn) VALUES (6, 'SSN1');  -- DELETE-INSERT conflict
+-- Conflicts for single_unique_idx
+DELETE FROM single_unique_idx WHERE id = 1;
+INSERT INTO single_unique_idx (id, ssn) VALUES (6, 'SSN1');  -- DELETE-INSERT conflict
+COMMIT;
 
-DELETE FROM single_unique_index WHERE id = 2;
-UPDATE single_unique_index SET ssn = 'SSN2' WHERE id = 3;  -- DELETE-UPDATE conflict
+DELETE FROM single_unique_idx WHERE id = 2;
+UPDATE single_unique_idx SET ssn = 'SSN2' WHERE id = 3;  -- DELETE-UPDATE conflict
+COMMIT;
 
-UPDATE single_unique_index SET ssn = 'updated_SSN4' WHERE id = 4;
-INSERT INTO single_unique_index (id, ssn) VALUES (7, 'SSN4');  -- UPDATE-INSERT conflict
+UPDATE single_unique_idx SET ssn = 'updated_SSN4' WHERE id = 4;
+INSERT INTO single_unique_idx (id, ssn) VALUES (7, 'SSN4');  -- UPDATE-INSERT conflict
+COMMIT;
 
-UPDATE single_unique_index SET ssn = 'updated_SSN5' WHERE id = 5;
-UPDATE single_unique_index SET ssn = 'SSN5' WHERE id = 6;  -- UPDATE-UPDATE conflict
+UPDATE single_unique_idx SET ssn = 'updated_SSN5' WHERE id = 5;
+UPDATE single_unique_idx SET ssn = 'SSN5' WHERE id = 6;  -- UPDATE-UPDATE conflict
+COMMIT;
 
--- Conflicts for multi_unique_index
-DELETE FROM multi_unique_index WHERE id = 1;
-INSERT INTO multi_unique_index (id, first_name, last_name) VALUES (6, 'John', 'Doe');  -- DELETE-INSERT conflict
+-- Conflicts for multi_unique_idx
+DELETE FROM multi_unique_idx WHERE id = 1;
+INSERT INTO multi_unique_idx (id, first_name, last_name) VALUES (6, 'John', 'Doe');  -- DELETE-INSERT conflict
+COMMIT;
 
-DELETE FROM multi_unique_index WHERE id = 2;
-UPDATE multi_unique_index SET first_name = 'Jane', last_name = 'Smith' WHERE id = 3;  -- DELETE-UPDATE conflict
+DELETE FROM multi_unique_idx WHERE id = 2;
+UPDATE multi_unique_idx SET first_name = 'Jane', last_name = 'Smith' WHERE id = 3;  -- DELETE-UPDATE conflict
+COMMIT;
 
-UPDATE multi_unique_index SET first_name = 'Updated_Tom' WHERE id = 5;
-INSERT INTO multi_unique_index (id, first_name, last_name) VALUES (7, 'Tom', 'Clark');  -- UPDATE-INSERT conflict
+UPDATE multi_unique_idx SET first_name = 'Updated_Tom' WHERE id = 5;
+INSERT INTO multi_unique_idx (id, first_name, last_name) VALUES (7, 'Tom', 'Clark');  -- UPDATE-INSERT conflict
+COMMIT;
 
-UPDATE multi_unique_index SET first_name = 'Updated_Alice' WHERE id = 4;
-UPDATE multi_unique_index SET first_name = 'Alice', last_name = 'Williams' WHERE id = 6;  -- UPDATE-UPDATE conflict
+UPDATE multi_unique_idx SET first_name = 'Updated_Alice' WHERE id = 4;
+UPDATE multi_unique_idx SET first_name = 'Alice', last_name = 'Williams' WHERE id = 6;  -- UPDATE-UPDATE conflict
+COMMIT;
 
--- Conflicts for different_columns_unique_constraint_and_index
-DELETE FROM different_columns_unique_constraint_and_index WHERE id = 1;
-INSERT INTO different_columns_unique_constraint_and_index (id, email, phone_number) VALUES (6, 'user1@example.com', '555-555-5560');  -- DELETE-INSERT conflict on email
+-- Conflicts for diff_columns_constr_idx
+DELETE FROM diff_columns_constr_idx WHERE id = 1;
+INSERT INTO diff_columns_constr_idx (id, email, phone_number) VALUES (6, 'user1@example.com', '555-555-5560');  -- DELETE-INSERT conflict
+COMMIT; on email
 
-DELETE FROM different_columns_unique_constraint_and_index WHERE id = 2;
-UPDATE different_columns_unique_constraint_and_index SET phone_number = '555-555-5552' WHERE id = 3;  -- DELETE-UPDATE conflict on phone_number
+DELETE FROM diff_columns_constr_idx WHERE id = 2;
+UPDATE diff_columns_constr_idx SET phone_number = '555-555-5552' WHERE id = 3;  -- DELETE-UPDATE conflict
+COMMIT; on phone_number
 
-UPDATE different_columns_unique_constraint_and_index SET email = 'updated_user4@example.com' WHERE id = 4;
-INSERT INTO different_columns_unique_constraint_and_index (id, email, phone_number) VALUES (7, 'user4@example.com', '555-555-5561');  -- UPDATE-INSERT conflict on email
+UPDATE diff_columns_constr_idx SET email = 'updated_user4@example.com' WHERE id = 4;
+INSERT INTO diff_columns_constr_idx (id, email, phone_number) VALUES (7, 'user4@example.com', '555-555-5561');  -- UPDATE-INSERT conflict
+COMMIT; on email
 
-UPDATE different_columns_unique_constraint_and_index SET phone_number = '555-555-5558' WHERE id = 5;
-UPDATE different_columns_unique_constraint_and_index SET phone_number = '555-555-5555' WHERE id = 6;  -- UPDATE-UPDATE conflict on phone_number
+UPDATE diff_columns_constr_idx SET phone_number = '555-555-5558' WHERE id = 5;
+UPDATE diff_columns_constr_idx SET phone_number = '555-555-5555' WHERE id = 6;  -- UPDATE-UPDATE conflict
+COMMIT; on phone_number
 
--- Conflicts for subset_columns_unique_constraint_and_index
-DELETE FROM subset_columns_unique_constraint_and_index WHERE id = 1;
-INSERT INTO subset_columns_unique_constraint_and_index (id, first_name, last_name, phone_number) VALUES (6, 'John', 'Doe', '123-456-7890');  -- DELETE-INSERT conflict
+-- Conflicts for subset_columns_constr_idx
+DELETE FROM subset_columns_constr_idx WHERE id = 1;
+INSERT INTO subset_columns_constr_idx (id, first_name, last_name, phone_number) VALUES (6, 'John', 'Doe', '123-456-7890');  -- DELETE-INSERT conflict
+COMMIT;
 
-DELETE FROM subset_columns_unique_constraint_and_index WHERE id = 2;
-UPDATE subset_columns_unique_constraint_and_index SET first_name = 'Jane', last_name = 'Smith' WHERE id = 3;  -- DELETE-UPDATE conflict
+DELETE FROM subset_columns_constr_idx WHERE id = 2;
+UPDATE subset_columns_constr_idx SET first_name = 'Jane', last_name = 'Smith' WHERE id = 3;  -- DELETE-UPDATE conflict
+COMMIT;
 
-UPDATE subset_columns_unique_constraint_and_index SET first_name = 'Updated_Bob' WHERE id = 3;
-INSERT INTO subset_columns_unique_constraint_and_index (id, first_name, last_name, phone_number) VALUES (7, 'Bob', 'Johnson', '123-456-7892');  -- UPDATE-INSERT conflict
+UPDATE subset_columns_constr_idx SET first_name = 'Updated_Bob' WHERE id = 3;
+INSERT INTO subset_columns_constr_idx (id, first_name, last_name, phone_number) VALUES (7, 'Bob', 'Johnson', '123-456-7892');  -- UPDATE-INSERT conflict
+COMMIT;
 
-UPDATE subset_columns_unique_constraint_and_index SET first_name = 'Updated_Alice' WHERE id = 4;
-UPDATE subset_columns_unique_constraint_and_index SET first_name = 'Alice', last_name = 'Williams', phone_number = '123-456-7893' WHERE id = 5;  -- UPDATE-UPDATE conflict
+UPDATE subset_columns_constr_idx SET first_name = 'Updated_Alice' WHERE id = 4;
+UPDATE subset_columns_constr_idx SET first_name = 'Alice', last_name = 'Williams', phone_number = '123-456-7893' WHERE id = 5;  -- UPDATE-UPDATE conflict
+COMMIT;
