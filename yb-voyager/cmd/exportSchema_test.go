870a6d9c8c93119f3f10af7c12cfaa7f0ea09904
Hemant Bhanawat <hemant9379@gmail.com>
2024-09-24 21:27:46 +0530

    Consider PG materialized views and indexes on mviews for sizing in assess-migration (#1698)
    Updated the following queries that collect the sizing data from PG.
    object_type_mapping.psql
    table-columns-count.psql
    table-index-iops.psql
    table-index-sizes.psql
    
    The following two were already reporting the materialized view information:
    index-to-table-mapping.psql
    table-columns-data-types.psql
    
    With this change PG materialized views along with indexes on materialized views will now be considered for sizing.

---
+++ yb-voyager/cmd/exportSchema_test.go
@@ -0,0 +1,78 @@
+/*
+Copyright (c) YugabyteDB, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestShardingRecommendations(t *testing.T) {
+	sqlInfo_mview1 := sqlInfo{
+		objName:       "m1",
+		stmt:          "CREATE MATERIALIZED VIEW m1 AS SELECT * FROM t1 WHERE a = 3",
+		formattedStmt: "CREATE MATERIALIZED VIEW m1 AS SELECT * FROM t1 WHERE a = 3",
+		fileName:      "",
+	}
+	sqlInfo_mview2 := sqlInfo{
+		objName:       "m1",
+		stmt:          "CREATE MATERIALIZED VIEW m1 AS SELECT * FROM t1 WHERE a = 3 with no data",
+		formattedStmt: "CREATE MATERIALIZED VIEW m1 AS SELECT * FROM t1 WHERE a = 3 with no data",
+		fileName:      "",
+	}
+	source.DBType = POSTGRESQL
+	modifiedSqlStmt, match, _ := applyShardingRecommendationIfMatching(&sqlInfo_mview1, []string{"m1"}, MVIEW)
+	assert.Equal(t, strings.ToLower(modifiedSqlStmt),
+		strings.ToLower("create materialized view m1 with (colocation=false) as select * from t1 where a = 3;"))
+	assert.Equal(t, match, true)
+
+	modifiedSqlStmt, match, _ = applyShardingRecommendationIfMatching(&sqlInfo_mview2, []string{"m1"}, MVIEW)
+	assert.Equal(t, strings.ToLower(modifiedSqlStmt),
+		strings.ToLower("create materialized view m1 with (colocation=false) as select * from t1 where a = 3 with no data;"))
+	assert.Equal(t, match, true)
+
+	modifiedSqlStmt, match, _ = applyShardingRecommendationIfMatching(&sqlInfo_mview2, []string{"m1_notfound"}, MVIEW)
+	assert.Equal(t, modifiedSqlStmt, "CREATE MATERIALIZED VIEW m1 AS SELECT * FROM t1 WHERE a = 3 with no data")
+	assert.Equal(t, match, false)
+
+	sqlInfo_table1 := sqlInfo{
+		objName:       "m1",
+		stmt:          "create table a (a int, b int)",
+		formattedStmt: "create table a (a int, b int)",
+		fileName:      "",
+	}
+	sqlInfo_table2 := sqlInfo{
+		objName:       "m1",
+		stmt:          "create table a (a int, b int) WITH (fillfactor=70)",
+		formattedStmt: "create table a (a int, b int) WITH (fillfactor=70)",
+		fileName:      "",
+	}
+	modifiedTableStmt, matchTable, _ := applyShardingRecommendationIfMatching(&sqlInfo_table1, []string{"a"}, TABLE)
+	assert.Equal(t, strings.ToLower(modifiedTableStmt),
+		strings.ToLower("create table a (a int, b int) WITH (colocation=false);"))
+	assert.Equal(t, matchTable, true)
+
+	modifiedTableStmt, matchTable, _ = applyShardingRecommendationIfMatching(&sqlInfo_table2, []string{"a"}, TABLE)
+	assert.Equal(t, strings.ToLower(modifiedTableStmt),
+		strings.ToLower("create table a (a int, b int) WITH (fillfactor=70, colocation=false);"))
+	assert.Equal(t, matchTable, true)
+
+	modifiedSqlStmt, matchTable, _ = applyShardingRecommendationIfMatching(&sqlInfo_table2, []string{"m1_notfound"}, TABLE)
+	assert.Equal(t, modifiedSqlStmt, "create table a (a int, b int) WITH (fillfactor=70)")
+	assert.Equal(t, matchTable, false)
+}
