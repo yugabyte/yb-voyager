59b767aa42df45c98b61d1800afe30242a06bcd9
Aneesh Makala <amakala@yugabyte.com>
2024-09-25 15:54:15 +0530

    base CPU based logic for adapting parallelism

---
+++ yb-voyager/src/adaptiveparallelism/adaptive_parallelism.go
@@ -0,0 +1,92 @@
+/*
+Copyright (c) YugabyteDB, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package adaptiveparallelism
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils"
+)
+
+const (
+	CPU_USAGE_USER    = "cpu_usage_user"
+	CPU_USAGE_SYSTEM  = "cpu_usage_system"
+	MAX_CPU_THRESHOLD = 70
+)
+
+type TargetYugabyteDBWithConnectionPool interface {
+	IsAdaptiveParallelismSupported() bool
+	GetClusterMetrics() (map[string]map[string]string, error) // node_uuid:metric_name:metric_value
+	GetNumConnectionsInPool() int
+	UpdateNumConnectionsInPool(int) bool
+}
+
+func AdaptParalleism(yb TargetYugabyteDBWithConnectionPool) error {
+	if !yb.IsAdaptiveParallelismSupported() {
+		return fmt.Errorf("adaptive parallelism not supported in target YB database.")
+	}
+	for {
+		utils.PrintAndLog("--------------------------------------------------------")
+		clusterMetrics, err := yb.GetClusterMetrics()
+		if err != nil {
+			utils.PrintAndLog("error getting cluster metrics: %v", err)
+		}
+		utils.PrintAndLog("PARALLELISM: cluster metrics: %v", clusterMetrics)
+
+		// max cpu
+		maxCpuUsage, err := getMaxCpuUsageInCluster(clusterMetrics)
+		if err != nil {
+			return fmt.Errorf("getting max cpu usage in cluster: %w", err)
+		}
+
+		if maxCpuUsage > MAX_CPU_THRESHOLD {
+			utils.PrintAndLog("PARALLELISM: found CPU usage = %d > %d, reducing parallelism to %d", maxCpuUsage, MAX_CPU_THRESHOLD, yb.GetNumConnectionsInPool()-1)
+			updated := yb.UpdateNumConnectionsInPool(yb.GetNumConnectionsInPool() - 1)
+			if !updated {
+				utils.PrintAndLog("PARALLELISM: no update. pending change request or change out of bounds")
+			}
+		} else {
+			utils.PrintAndLog("PARALLELISM: found CPU usage = %d <= %d, increasing parallelism to %d", maxCpuUsage, MAX_CPU_THRESHOLD, yb.GetNumConnectionsInPool()+1)
+			updated := yb.UpdateNumConnectionsInPool(yb.GetNumConnectionsInPool() + 1)
+			if !updated {
+				utils.PrintAndLog("PARALLELISM: no update. pending change request or change out of bounds")
+			}
+		}
+		time.Sleep(20 * time.Second)
+	}
+}
+
+func getMaxCpuUsageInCluster(clusterMetrics map[string]map[string]string) (int, error) {
+	var maxCpuPct int
+	for _, nodeMetrics := range clusterMetrics {
+		cpuUsageUser, err := strconv.ParseFloat(nodeMetrics[CPU_USAGE_USER], 64)
+		if err != nil {
+			return -1, fmt.Errorf("parsing cpu usage user as float: %w", err)
+		}
+		cpuUsageSystem, err := strconv.ParseFloat(nodeMetrics[CPU_USAGE_SYSTEM], 64)
+		if err != nil {
+			return -1, fmt.Errorf("parsing cpu usage system as float: %w", err)
+		}
+
+		cpuUsagePct := int((cpuUsageUser + cpuUsageSystem) * 100)
+		if cpuUsagePct > maxCpuPct {
+			maxCpuPct = cpuUsagePct
+		}
+	}
+	return maxCpuPct, nil
+}
