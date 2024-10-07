0488b7acb94f0305846038c3f374582e0f63162a
priyanshi-yb <pgupta@yugabyte.com>
2024-10-07 15:50:14 +0530

    miss in expected files of omnibus and pgtbrus

--- migtests/tests/pg/pgtbrus/expected_files/expected_failed.sql
+++ migtests/tests/pg/pgtbrus/expected_files/expected_failed.sql
@@ -41,7 +41,6 @@ File :/home/ubuntu/yb-voyager/migtests/tests/pg/pgtbrus/export-dir/schema/tables
 */
 ALTER FOREIGN TABLE ONLY public.f_t ALTER COLUMN i SET DEFAULT nextval('public.f_t_i_seq'::regclass);
 
-
 /*
 ERROR: relation "public.f_c" does not exist (SQLSTATE 42P01)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/pgtbrus/export-dir/schema/sequences/sequence.sql
