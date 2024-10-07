4a8d686d3fdc56637552d62c0dbb63762aa8db07
Hemant Bhanawat <hemant9379@gmail.com>
2024-10-07 16:28:15 +0530

    Incorrect statements after adding mviews to the sizing calculations (#1737)
    - Fix for "Export schema CLI output contains incorrect / additional statements after MViews sizing implementation". See DB
    - Added tests for Mviews that already have storage parameters.
    - Added tests for alter table in the exportSchema

--- yb-voyager/cmd/exportSchema.go
+++ yb-voyager/cmd/exportSchema.go
@@ -300,7 +300,11 @@ func applyShardedTablesRecommendation(shardedTables []string, objType string) er
 
 	filePath := utils.GetObjectFilePath(schemaDir, objType)
 	if !utils.FileOrFolderExists(filePath) {
-		utils.PrintAndLog("Required schema file %s does not exists, returning without applying Colocated/Sharded Tables recommendation", filePath)
+		// Report if the file does not exist for tables. No need to report it for mviews
+		if objType == TABLE {
+			utils.PrintAndLog("Required schema file %s does not exists, "+
+				"returning without applying colocated/sharded tables recommendation", filePath)
+		}
 		return nil
 	}
 
@@ -356,8 +360,18 @@ func applyShardedTablesRecommendation(shardedTables []string, objType string) er
 	if err = file.Close(); err != nil {
 		return fmt.Errorf("error closing file '%q' storing the modified recommended schema: %w", filePath, err)
 	}
+	var objTypeName = ""
+	switch objType {
+	case MVIEW:
+		objTypeName = "MATERIALIZED VIEW"
+	case TABLE:
+		objTypeName = "TABLE"
+	default:
+		panic(fmt.Sprintf("Object type not supported %s", objType))
+	}
 
-	utils.PrintAndLog("Modified CREATE TABLE statements in %q according to the colocation and sharding recommendations of the assessment report.",
+	utils.PrintAndLog("Modified CREATE %s statements in %q according to the colocation and sharding recommendations of the assessment report.",
+		objTypeName,
 		utils.GetRelativePathFromCwd(filePath))
 	utils.PrintAndLog("The original DDLs have been preserved in %q for reference.", utils.GetRelativePathFromCwd(backupPath))
 	return nil
