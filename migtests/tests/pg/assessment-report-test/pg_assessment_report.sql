a93f565adfa83b0670887d5183ce36f5d777de68
Ubuntu <ubuntu@ip-10-9-2-132.us-west-2.compute.internal>
2024-11-08 11:23:08 +0000

    Segregated Unsupported Query Constructs

--- migtests/tests/pg/assessment-report-test/pg_assessment_report.sql
+++ migtests/tests/pg/assessment-report-test/pg_assessment_report.sql
@@ -202,51 +202,6 @@ CREATE INDEX idx7 on public.combined_tbl (address);
 
 CREATE UNLOGGED TABLE tbl_unlogged (id int, val text);
 
--- Unsupported Query Constructs 
-
-drop extension if exists pg_stat_statements;
-
-create extension pg_stat_statements;
-
-SELECT pg_stat_statements_reset();
-
-SELECT * FROM pg_stat_statements;
-
--- System Columns 
-
-SELECT ctid, tableoid, xmin, xmax, cmin, cmax
-FROM employees2;
-
--- XML Functions
-
-SELECT table_to_xml('employees2', true, false, '');
-
-SELECT xmlparse(document '<data><item>A</item></data>') as xmldata;
-
-SELECT xmlforest(first_name AS element1, last_name AS element2) FROM employees2;
-
-SELECT xmlelement(name root, xmlelement(name child, 'value'));
-
-SELECT xpath('/wrapper/root/child', '<wrapper><root><child>value</child></root></wrapper>'::xml);
-
--- Not Reported Currently
-
--- SELECT *
--- FROM xmltable(
---     '/employees/employee'
---     PASSING '<employees><employee><name>John</name></employee></employees>'
---     COLUMNS 
---         name TEXT PATH 'name'
--- );
-
--- Advisory Locks
-
-SELECT pg_advisory_lock(1,2); 
-SELECT pg_advisory_unlock(1,2); 
-SELECT pg_advisory_xact_lock(1,2); 
-SELECT pg_advisory_unlock_all();
-
-
 CREATE OR REPLACE FUNCTION public.check_sales_region()
 RETURNS TRIGGER AS $$
 BEGIN
