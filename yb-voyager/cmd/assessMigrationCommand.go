9ba217745b0eac0f5379ae7b8724cba9186dfcce
Priyanshi Gupta <pgupta@yugabyte.com>
2024-11-20 18:12:28 +0530

    Send objectName for Extension Issue / Unsupported feature to callhome in analyze/assess payload (#1910)

--- yb-voyager/cmd/assessMigrationCommand.go
+++ yb-voyager/cmd/assessMigrationCommand.go
@@ -105,6 +105,11 @@ var assessMigrationCmd = &cobra.Command{
 	},
 }
 
+// Assessment feature names to send the object names for to callhome
+var featuresToSendObjectsToCallhome = []string{
+	EXTENSION_FEATURE,
+}
+
 func packAndSendAssessMigrationPayload(status string, errMsg string) {
 	if !shouldSendCallhome() {
 		return
@@ -152,18 +157,26 @@ func packAndSendAssessMigrationPayload(status string, errMsg string) {
 	assessPayload := callhome.AssessMigrationPhasePayload{
 		MigrationComplexity: assessmentReport.MigrationComplexity,
 		UnsupportedFeatures: callhome.MarshalledJsonString(lo.Map(assessmentReport.UnsupportedFeatures, func(feature UnsupportedFeature, _ int) callhome.UnsupportedFeature {
-			return callhome.UnsupportedFeature{
-				FeatureName: feature.FeatureName,
-				ObjectCount: len(feature.Objects),
+			var objects []string
+			if slices.Contains(featuresToSendObjectsToCallhome, feature.FeatureName) {
+				objects = lo.Map(feature.Objects, func(o ObjectInfo, _ int) string {
+					return o.ObjectName
+				})
+			}
+			res := callhome.UnsupportedFeature{
+				FeatureName:      feature.FeatureName,
+				ObjectCount:      len(feature.Objects),
+				Objects:          objects,
 				TotalOccurrences: len(feature.Objects),
 			}
+			return res
 		})),
 		UnsupportedQueryConstructs: callhome.MarshalledJsonString(countByConstructType),
 		UnsupportedDatatypes:       callhome.MarshalledJsonString(unsupportedDatatypesList),
 		MigrationCaveats: callhome.MarshalledJsonString(lo.Map(assessmentReport.MigrationCaveats, func(feature UnsupportedFeature, _ int) callhome.UnsupportedFeature {
 			return callhome.UnsupportedFeature{
-				FeatureName: feature.FeatureName,
-				ObjectCount: len(feature.Objects),
+				FeatureName:      feature.FeatureName,
+				ObjectCount:      len(feature.Objects),
 				TotalOccurrences: len(feature.Objects),
 			}
 		})),
