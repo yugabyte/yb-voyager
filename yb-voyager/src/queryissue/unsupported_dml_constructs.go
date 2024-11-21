e4d9142b0c9693e96f8a5ebe2e34e1e88acdc159
Sanyam Singhal <ssinghal@yugabyte.com>
2024-11-21 09:30:23 +0000

    review comments

--- yb-voyager/src/queryissue/unsupported_dml_constructs.go
+++ yb-voyager/src/queryissue/unsupported_dml_constructs.go
@@ -134,94 +134,9 @@ func NewRangeTableFuncDetector() *RangeTableFuncDetector {
 // Detect checks if a RangeTableFunc node is present for a XMLTABLE() function
 func (d *RangeTableFuncDetector) Detect(msg protoreflect.Message) ([]string, error) {
 	if queryparser.GetMsgFullName(msg) == queryparser.PG_QUERY_RANGETABLEFUNC_NODE {
-		if isXMLTable(msg) {
+		if queryparser.IsXMLTable(msg) {
 			return []string{XML_FUNCTIONS}, nil
 		}
 	}
 	return nil, nil
-}
-
-/*
-Note: XMLTABLE() is not a simple function(stored in FuncCall node), its a table function
-which generates set of rows using the info(about rows, columns, content) provided to it
-Hence its requires a more complex node structure(RangeTableFunc node) to represent.
-
-XMLTABLE transforms XML data into relational table format, making it easier to query XML structures.
-Detection in RangeTableFunc Node:
-- docexpr: Refers to the XML data source, usually a column storing XML.
-- rowexpr: XPath expression (starting with '/' or '//') defining the rows in the XML.
-- columns: Specifies the data extraction from XML into relational columns.
-
-Example: Converting XML data about books into a table:
-SQL Query:
-
-	SELECT x.*
-	FROM XMLTABLE(
-		'/bookstore/book'
-		PASSING xml_column
-		COLUMNS
-			title TEXT PATH 'title',
-			author TEXT PATH 'author'
-	) AS x;
-
-Parsetree: stmt:{select_stmt:{target_list:{res_target:{val:{column_ref:{fields:{string:{sval:"x"}}  fields:{a_star:{}}  location:7}}  location:7}}
-from_clause:{range_table_func:
-	{docexpr:{column_ref:{fields:{string:{sval:"xml_column"}}  location:57}}
-	rowexpr:{a_const:{sval:{sval:"/bookstore/book"}  location:29}}
-	columns:{range_table_func_col:{colname:"title"  type_name:{names:{string:{sval:"text"}}  typemod:-1  location:87}  colexpr:{a_const:{sval:{sval:"title"}  location:97}}  location:81}}
-	columns:{range_table_func_col:{colname:"author"  type_name:{names:{string:{sval:"text"}}  typemod:-1  location:116}  colexpr:{a_const:{sval:{sval:"author"}  location:126}}  location:109}}
-alias:{aliasname:"x"}  location:17}}  limit_option:LIMIT_OPTION_DEFAULT  op:SETOP_NONE}}  stmt_len:142
-
-Here, 'docexpr' points to 'xml_column' containing XML data, 'rowexpr' selects each 'book' node, and 'columns' extract 'title' and 'author' from each book.
-Hence Presence of XPath in 'rowexpr' and structured 'columns' typically indicates XMLTABLE usage.
-*/
-// Function to detect if a RangeTableFunc node represents XMLTABLE()
-func isXMLTable(rangeTableFunc protoreflect.Message) bool {
-	log.Infof("checking if range table func node is for XMLTABLE()")
-	// Check for 'docexpr' field
-	docexprField := rangeTableFunc.Descriptor().Fields().ByName("docexpr")
-	if docexprField == nil {
-		return false
-	}
-	docexprNode := rangeTableFunc.Get(docexprField).Message()
-	if docexprNode == nil {
-		return false
-	}
-
-	// Check for 'rowexpr' field
-	rowexprField := rangeTableFunc.Descriptor().Fields().ByName("rowexpr")
-	if rowexprField == nil {
-		return false
-	}
-	rowexprNode := rangeTableFunc.Get(rowexprField).Message()
-	if rowexprNode == nil {
-		return false
-	}
-
-	xpath := queryparser.GetStringValueFromNode(rowexprNode)
-	log.Debugf("xpath expression in the node: %s\n", xpath)
-	// Keep both cases check(param placeholder and absolute check)
-	if xpath == "" {
-		// Attempt to check if 'rowexpr' is a parameter placeholder like '$1'
-		isPlaceholder := queryparser.IsParameterPlaceholder(rowexprNode)
-		if !isPlaceholder {
-			return false
-		}
-	} else if !queryparser.IsXPathExprForXmlTable(xpath) {
-		return false
-	}
-
-	// Check for 'columns' field
-	columnsField := rangeTableFunc.Descriptor().Fields().ByName("columns")
-	if columnsField == nil {
-		return false
-	}
-
-	columnsList := rangeTableFunc.Get(columnsField).List()
-	if columnsList.Len() == 0 {
-		return false
-	}
-
-	// this means all the required fields of RangeTableFunc node for being a XMLTABLE() are present
-	return true
 }
--- yb-voyager/src/queryparser/helpers.go
+++ yb-voyager/src/queryparser/helpers.go
@@ -18,6 +18,7 @@ package queryparser
 import (
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -161,6 +162,95 @@ func traverseAndExtractAConst(nodeMsg protoreflect.Message, fieldName string) st
 	}
 
 	return GetStringValueFromNode(childMsg)
+}
+
+/*
+Note: XMLTABLE() is not a simple function(stored in FuncCall node), its a table function
+which generates set of rows using the info(about rows, columns, content) provided to it
+Hence its requires a more complex node structure(RangeTableFunc node) to represent.
+
+XMLTABLE transforms XML data into relational table format, making it easier to query XML structures.
+Detection in RangeTableFunc Node:
+- docexpr: Refers to the XML data source, usually a column storing XML.
+- rowexpr: XPath expression (starting with '/' or '//') defining the rows in the XML.
+- columns: Specifies the data extraction from XML into relational columns.
+
+Example: Converting XML data about books into a table:
+SQL Query:
+
+	SELECT x.*
+	FROM XMLTABLE(
+		'/bookstore/book'
+		PASSING xml_column
+		COLUMNS
+			title TEXT PATH 'title',
+			author TEXT PATH 'author'
+	) AS x;
+
+Parsetree: stmt:{select_stmt:{target_list:{res_target:{val:{column_ref:{fields:{string:{sval:"x"}}  fields:{a_star:{}}  location:7}}  location:7}}
+from_clause:{range_table_func:
+	{docexpr:{column_ref:{fields:{string:{sval:"xml_column"}}  location:57}}
+	rowexpr:{a_const:{sval:{sval:"/bookstore/book"}  location:29}}
+	columns:{range_table_func_col:{colname:"title"  type_name:{names:{string:{sval:"text"}}  typemod:-1  location:87}  colexpr:{a_const:{sval:{sval:"title"}  location:97}}  location:81}}
+	columns:{range_table_func_col:{colname:"author"  type_name:{names:{string:{sval:"text"}}  typemod:-1  location:116}  colexpr:{a_const:{sval:{sval:"author"}  location:126}}  location:109}}
+alias:{aliasname:"x"}  location:17}}  limit_option:LIMIT_OPTION_DEFAULT  op:SETOP_NONE}}  stmt_len:142
+
+Here, 'docexpr' points to 'xml_column' containing XML data, 'rowexpr' selects each 'book' node, and 'columns' extract 'title' and 'author' from each book.
+Hence Presence of XPath in 'rowexpr' and structured 'columns' typically indicates XMLTABLE usage.
+*/
+// Function to detect if a RangeTableFunc node represents XMLTABLE()
+func IsXMLTable(rangeTableFunc protoreflect.Message) bool {
+	if GetMsgFullName(rangeTableFunc) != PG_QUERY_RANGETABLEFUNC_NODE {
+		return false
+	}
+
+	log.Debug("checking if range table func node is for XMLTABLE()")
+	// Check for 'docexpr' field
+	docexprField := rangeTableFunc.Descriptor().Fields().ByName("docexpr")
+	if docexprField == nil {
+		return false
+	}
+	docexprNode := rangeTableFunc.Get(docexprField).Message()
+	if docexprNode == nil {
+		return false
+	}
+
+	// Check for 'rowexpr' field
+	rowexprField := rangeTableFunc.Descriptor().Fields().ByName("rowexpr")
+	if rowexprField == nil {
+		return false
+	}
+	rowexprNode := rangeTableFunc.Get(rowexprField).Message()
+	if rowexprNode == nil {
+		return false
+	}
+
+	xpath := GetStringValueFromNode(rowexprNode)
+	log.Debugf("xpath expression in the node: %s\n", xpath)
+	// Keep both cases check(param placeholder and absolute check)
+	if xpath == "" {
+		// Attempt to check if 'rowexpr' is a parameter placeholder like '$1'
+		isPlaceholder := IsParameterPlaceholder(rowexprNode)
+		if !isPlaceholder {
+			return false
+		}
+	} else if !IsXPathExprForXmlTable(xpath) {
+		return false
+	}
+
+	// Check for 'columns' field
+	columnsField := rangeTableFunc.Descriptor().Fields().ByName("columns")
+	if columnsField == nil {
+		return false
+	}
+
+	columnsList := rangeTableFunc.Get(columnsField).List()
+	if columnsList.Len() == 0 {
+		return false
+	}
+
+	// this means all the required fields of RangeTableFunc node for being a XMLTABLE() are present
+	return true
 }
 
 /*
