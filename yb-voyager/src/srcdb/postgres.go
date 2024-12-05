7aa0d00ef8c633269227ba07bd72242f94f0de10
Sanyam Singhal <ssinghal@yugabyte.com>
2024-12-05 22:10:13 +0000

    code cleanup

--- yb-voyager/src/srcdb/postgres.go
+++ yb-voyager/src/srcdb/postgres.go
@@ -367,9 +367,6 @@ func (pg *PostgreSQL) ExportData(ctx context.Context, exportDir string, tableLis
 
 func (pg *PostgreSQL) ExportDataPostProcessing(exportDir string, tablesProgressMetadata map[string]*utils.TableProgressMetadata) {
 	renameDataFiles(tablesProgressMetadata)
-	if tablesProgressMetadata == nil {
-		return
-	}
 	dfd := datafile.Descriptor{
 		FileFormat:                 datafile.TEXT,
 		DataFileList:               getExportedDataFileList(tablesProgressMetadata),
