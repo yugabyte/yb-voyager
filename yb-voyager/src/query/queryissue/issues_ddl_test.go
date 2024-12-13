b7ddb6110420ed54e853ebb846ea4ef599babacb
Aneesh Makala <amakala@yugabyte.com>
2024-12-13 11:16:45 +0530

    remove debug

--- yb-voyager/src/query/queryissue/issues_ddl_test.go
+++ yb-voyager/src/query/queryissue/issues_ddl_test.go
@@ -128,7 +128,6 @@ func testStoredGeneratedFunctionsIssue(t *testing.T) {
 func testUnloggedTableIssue(t *testing.T) {
 	noticeFound := false
 	noticeHandler := func(conn *pgconn.PgConn, notice *pgconn.Notice) {
-		assert.Nil(t, notice)
 		if notice != nil && notice.Message != "" {
 			assert.Equal(t, "unlogged option is currently ignored in YugabyteDB, all non-temp tables will be logged", notice.Message)
 			noticeFound = true
