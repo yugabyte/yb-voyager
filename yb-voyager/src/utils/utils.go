66efa33b89c3ac0fb80cb2c2c03eb7f7d87b8de5
Priyanshi Gupta <pgupta@yugabyte.com>
2024-12-17 15:40:33 +0530

    Redacting password while logging connection params in import data to target (#2097)

--- yb-voyager/src/utils/utils.go
+++ yb-voyager/src/utils/utils.go
@@ -431,7 +431,8 @@ func GetRedactedURLs(urlList []string) []string {
 	for _, u := range urlList {
 		obj, err := url.Parse(u)
 		if err != nil {
-			ErrExit("invalid URL: %q", u)
+			log.Error("error redacting connection url: invalid connection URL")
+			fmt.Printf("error redacting connection url: invalid connection URL: %v", u)
 		}
 		result = append(result, obj.Redacted())
 	}
