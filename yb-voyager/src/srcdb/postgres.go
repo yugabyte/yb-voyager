3735d9ab70ee326bb116d5d5d268bc2e7a63f717
Sanyam Singhal <ssinghal@yugabyte.com>
2024-12-18 17:31:25 +0530

    [DB-14412] Skip collection of assessment metadata for unsupported query constructs if guardrails checks fail (#2096)
    * Added 'pgss_enabled' argument to yb-voyager-pg-gather-assessment-metadata script
    - argument is based on the guardrails check passed or not in assess migration cmd
    
    * Updated gather-assessment-metadata.tar.gz

--- yb-voyager/src/srcdb/postgres.go
+++ yb-voyager/src/srcdb/postgres.go
@@ -1136,13 +1136,13 @@ func (pg *PostgreSQL) GetMissingExportDataPermissions(exportType string, finalTa
 	return combinedResult, nil
 }
 
-func (pg *PostgreSQL) GetMissingAssessMigrationPermissions() ([]string, error) {
+func (pg *PostgreSQL) GetMissingAssessMigrationPermissions() ([]string, bool, error) {
 	var combinedResult []string
 
 	// Check if tables have SELECT permission
 	missingTables, err := pg.listTablesMissingSelectPermission("")
 	if err != nil {
-		return nil, fmt.Errorf("error checking table select permissions: %w", err)
+		return nil, false, fmt.Errorf("error checking table select permissions: %w", err)
 	}
 	if len(missingTables) > 0 {
 		combinedResult = append(combinedResult, fmt.Sprintf("\n%s[%s]", color.RedString("Missing SELECT permission for user %s on Tables: ", pg.source.User), strings.Join(missingTables, ", ")))
@@ -1150,13 +1150,15 @@ func (pg *PostgreSQL) GetMissingAssessMigrationPermissions() ([]string, error) {
 
 	result, err := pg.checkPgStatStatementsSetup()
 	if err != nil {
-		return nil, fmt.Errorf("error checking pg_stat_statement extension installed with read permissions: %w", err)
+		return nil, false, fmt.Errorf("error checking pg_stat_statement extension installed with read permissions: %w", err)
 	}
 
+	pgssEnabled := true
 	if result != "" {
+		pgssEnabled = false
 		combinedResult = append(combinedResult, result)
 	}
-	return combinedResult, nil
+	return combinedResult, pgssEnabled, nil
 }
 
 const (
