901d7741977021edba3906b6e3c7f739b985d680
shubham-yb <shubhamdabriwala01@gmail.com>
2024-12-18 21:29:11 +0000

    Fix for missing operand basename error

--- yb-voyager/src/srcdb/data/gather-assessment-metadata/oracle/yb-voyager-oracle-gather-assessment-metadata.sh
+++ yb-voyager/src/srcdb/data/gather-assessment-metadata/oracle/yb-voyager-oracle-gather-assessment-metadata.sh
@@ -14,8 +14,8 @@
 
 set -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE:-$0}" )" &> /dev/null && pwd )
-SCRIPT_NAME=$(basename $0)
+SCRIPT_DIR=$(cd -- "$(dirname "${BASH_SOURCE[0]:-$0}")" &> /dev/null && pwd)
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]:-$0}")
 
 HELP_TEXT="
 Usage: $SCRIPT_NAME <oracle_connection_string> <schema_name> <assessment_metadata_dir>
