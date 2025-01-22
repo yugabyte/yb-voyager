064f01bfd8579e423d980835187a1d0000eb81b3
priyanshi-yb <pgupta@yugabyte.com>
2025-01-22 15:45:09 +0530

    fix

--- yb-voyager/src/query/queryissue/issues_dml_test.go
+++ yb-voyager/src/query/queryissue/issues_dml_test.go
@@ -350,8 +350,7 @@ func testCTEWithMaterializedIssue(t *testing.T) {
 	)
 	SELECT * FROM w AS w1 JOIN w AS w2 ON w1.key = w2.ref
 	WHERE w2.key = '123';`: `syntax error at or near "NOT"`,
-		`CREATE TABLE big_table(key text, ref text, c1 int, c2 int);
-		WITH w AS MATERIALIZED (
+		`WITH w AS MATERIALIZED (
 		SELECT * FROM big_table
 	)
 	SELECT * FROM w AS w1 JOIN w AS w2 ON w1.key = w2.ref
