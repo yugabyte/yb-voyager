2e8d241f7a7896a49a9ef1bdb3d9298fe47cd129
Aneesh Makala <amakala@yugabyte.com>
2025-01-23 11:03:33 +0530

    basic test

---
+++ yb-voyager/cmd/importDataFileTaskImporter_test.go
@@ -0,0 +1,104 @@
+/*
+Copyright (c) YugabyteDB, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/sourcegraph/conc/pool"
+	"github.com/stretchr/testify/assert"
+	"github.com/yugabyte/yb-voyager/yb-voyager/src/tgtdb"
+	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils"
+	"github.com/yugabyte/yb-voyager/yb-voyager/src/utils/sqlname"
+	testcontainers "github.com/yugabyte/yb-voyager/yb-voyager/test/containers"
+	testutils "github.com/yugabyte/yb-voyager/yb-voyager/test/utils"
+)
+
+type TestDB struct {
+	testcontainers.TestContainer
+	tgtdb.TargetDB
+}
+
+func TestBasicTaskImport(t *testing.T) {
+	ldataDir, lexportDir, state, err := setupExportDirAndImportDependencies(2, 1024)
+	testutils.FatalIfError(t, err)
+
+	if ldataDir != "" {
+		defer os.RemoveAll(fmt.Sprintf("%s/", ldataDir))
+	}
+	if lexportDir != "" {
+		defer os.RemoveAll(fmt.Sprintf("%s/", lexportDir))
+	}
+
+	// yb
+	yugabytedbContainer := testcontainers.NewTestContainer("yugabytedb", nil)
+	err = yugabytedbContainer.Start(context.Background())
+	testutils.FatalIfError(t, err)
+	host, port, err := yugabytedbContainer.GetHostPort()
+	testutils.FatalIfError(t, err)
+	testYugabyteDBTarget := &TestDB{
+		TestContainer: yugabytedbContainer,
+		TargetDB: tgtdb.NewTargetDB(&tgtdb.TargetConf{
+			TargetDBType: "yugabytedb",
+			DBVersion:    yugabytedbContainer.GetConfig().DBVersion,
+			User:         yugabytedbContainer.GetConfig().User,
+			Password:     yugabytedbContainer.GetConfig().Password,
+			Schema:       yugabytedbContainer.GetConfig().Schema,
+			DBName:       yugabytedbContainer.GetConfig().DBName,
+			Host:         host,
+			Port:         port,
+		}),
+	}
+	testYugabyteDBTarget.TestContainer.ExecuteSqls(
+		`CREATE TABLE test_table (id INT PRIMARY KEY, val TEXT);`,
+	)
+
+	err = testYugabyteDBTarget.Init()
+	testutils.FatalIfError(t, err)
+	defer testYugabyteDBTarget.Finalize()
+	tdb = testYugabyteDBTarget.TargetDB
+	err = tdb.CreateVoyagerSchema()
+	testutils.FatalIfError(t, err)
+	err = tdb.InitConnPool()
+	testutils.FatalIfError(t, err)
+
+	TableNameToSchema = utils.NewStructMap[sqlname.NameTuple, map[string]map[string]string]()
+
+	// file import
+	fileContents := `id,val
+1, "hello"
+2, "world"`
+	_, task, err := createFileAndTask(lexportDir, fileContents, ldataDir, "test_table")
+	testutils.FatalIfError(t, err)
+
+	progressReporter := NewImportDataProgressReporter(true)
+	workerPool := pool.New().WithMaxGoroutines(2)
+	taskImporter, err := NewFileTaskImporter(task, state, workerPool, progressReporter)
+
+	for !taskImporter.AllBatchesSubmitted() {
+		err := taskImporter.SubmitNextBatch()
+		assert.NoError(t, err)
+	}
+
+	workerPool.Wait()
+	var rowCount int64
+	err = tdb.QueryRow("SELECT count(*) FROM test_table").Scan(&rowCount)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(2), rowCount)
+}
