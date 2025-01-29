4183f0f6426ead3e21eaa152098003388195c9ee
Aneesh Makala <amakala@yugabyte.com>
2025-01-29 17:26:01 +0530

    test idea

--- yb-voyager/cmd/importDataFileTaskPicker_test.go
+++ yb-voyager/cmd/importDataFileTaskPicker_test.go
@@ -238,6 +238,10 @@ maxTasksInProgressGreaterThanTotalTasks
 MixOfColocatedAndShardedTables - ensure all are getting picked with proper weights
 AllColocatedTables - ensure all are getting picked with proper weights
 AllShardedTables - ensure all are getting picked with proper weights
+dynamicTasks - create 100s of tasks randmoly colocated/shared,
+	get next tasks in a large loop,
+	mark them as done occasionally,
+	ensure all are getting picked at the end
 
 all of the above cases for
 multipleTasksPerTable (importDataFileCase)
