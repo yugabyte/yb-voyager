95178f9d4f40f0c05e304826d30394a0c6adb491
Sanyam Singhal <ssinghal@yugabyte.com>
2025-03-06 10:16:01 +0000

    Revert "Miscellaneous changes in export schema transformations (#2378)"
    This reverts commit 7d21b3e5586a9a1174772991c0753c4dde31966d.

--- yb-voyager/src/query/queryparser/query_parser.go
+++ yb-voyager/src/query/queryparser/query_parser.go
@@ -55,7 +55,7 @@ func ParsePLPGSQLToJson(query string) (string, error) {
 }
 
 func ParseSqlFile(filePath string) ([]*pg_query.RawStmt, error) {
-	log.Infof("parsing the file %q", filePath)
+	log.Debugf("parsing the file [%s]", filePath)
 	bytes, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("reading file failed: %v", err)
