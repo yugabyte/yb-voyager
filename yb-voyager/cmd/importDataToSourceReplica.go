a99293a8461b5af40b5133b8848dd0487073eb3b
Priyanshi Gupta <pgupta@yugabyte.com>
2025-03-06 13:08:21 +0530

    Fix: nil pointer exception while sending the callhome payload for import data to source/source-replica (#2371)
    Fixing the nil pointer exception while sending the call home payload for exit/error case during initial phase of import data to source/source-replica.
    fixes #2364

--- yb-voyager/cmd/importDataToSourceReplica.go
+++ yb-voyager/cmd/importDataToSourceReplica.go
@@ -100,9 +100,11 @@ func packAndSendImportDataToSrcReplicaPayload(status string, errorMsg string) {
 	payload.MigrationType = LIVE_MIGRATION
 
 	sourceDBDetails := callhome.SourceDBDetails{
-		DBType:    tconf.TargetDBType,
-		DBVersion: targetDBDetails.DBVersion,
-		Role:      "replica",
+		DBType: tconf.TargetDBType,
+		Role:   "replica",
+	}
+	if targetDBDetails != nil {
+		sourceDBDetails.DBVersion = targetDBDetails.DBVersion
 	}
 	payload.SourceDBDetails = callhome.MarshalledJsonString(sourceDBDetails)
 
@@ -128,7 +130,7 @@ func packAndSendImportDataToSrcReplicaPayload(status string, errorMsg string) {
 
 	importDataPayload.Phase = importPhase
 
-	if importPhase != dbzm.MODE_SNAPSHOT {
+	if importPhase != dbzm.MODE_SNAPSHOT && statsReporter != nil {
 		importDataPayload.EventsImportRate = statsReporter.EventsImportRateLast3Min
 		importDataPayload.TotalImportedEvents = statsReporter.TotalEventsImported
 	}
