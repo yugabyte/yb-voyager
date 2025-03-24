b8e59b913a94179048651949c81d904b4ef165b8
Sanyam Singhal <ssinghal@yugabyte.com>
2025-03-24 19:47:37 +0530

    Remove add-pk-from-alter-to-create from test framework (#2428)
    - required enhancement is already incorporated in the export schema command

--- migtests/tests/pg/case-sensitivity-reserved-words/env.sh
+++ migtests/tests/pg/case-sensitivity-reserved-words/env.sh
@@ -1,4 +1,3 @@
 export SOURCE_DB_TYPE="postgresql"
 export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"case_sensitive_reserved_words"}
 export SOURCE_DB_SCHEMA="public,schema2"
-export MOVE_PK_FROM_ALTER_TO_CREATE="true"
