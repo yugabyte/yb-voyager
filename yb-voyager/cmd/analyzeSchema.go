6df005fdb367ab2ac6f61841ecae20866bff1e8e
ShivanshGahlot <shivanshgahlot@gmail.com>
2025-03-24 09:24:30 +0000

    Made suggested changes

--- yb-voyager/cmd/analyzeSchema.go
+++ yb-voyager/cmd/analyzeSchema.go
@@ -632,7 +632,7 @@ func convertIssueInstanceToAnalyzeIssue(issueInstance queryissue.QueryIssue, fil
 		issueType = UNSUPPORTED_PLPGSQL_OBJECTS_CATEGORY
 	case slices.ContainsFunc(migrationCaveatsIssues, func(i string) bool {
 		//Adding the MIGRATION_CAVEATS issueType(category) of the utils.Issue for these issueInstances in MigrationCaveatsIssues
-		return strings.EqualFold(issueInstance.Type, i)
+		return issueInstance.Type == i
 	}):
 		issueType = MIGRATION_CAVEATS_CATEGORY
 	case strings.HasPrefix(issueInstance.Name, UNSUPPORTED_DATATYPE):
