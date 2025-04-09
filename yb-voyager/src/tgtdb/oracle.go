3fdf7e7f47c155ada9a3f8844b3343898d12528c
Sanyam Singhal <ssinghal@yugabyte.com>
2025-04-09 13:34:16 +0000

    Changed the terminology in code to be terms of non-txn vs txn path - removed usage of fast path keyword

--- yb-voyager/src/tgtdb/oracle.go
+++ yb-voyager/src/tgtdb/oracle.go
@@ -249,9 +249,9 @@ func (tdb *TargetOracleDB) RestoreSequences(sequencesLastVal map[string]int64) e
 	return nil
 }
 
-func (tdb *TargetOracleDB) ImportBatch(batch Batch, args *ImportBatchArgs, exportDir string, tableSchema map[string]map[string]string, fastPath bool) (int64, error) {
-	if fastPath {
-		panic("fastPath is not supported for import batch in Oracle")
+func (tdb *TargetOracleDB) ImportBatch(batch Batch, args *ImportBatchArgs, exportDir string, tableSchema map[string]map[string]string, nonTxnPath bool) (int64, error) {
+	if nonTxnPath {
+		panic("non-transactional path for import batch is not supported in Oracle")
 	}
 
 	tdb.Lock()
