0ab90564962b8174875b49fcdc85d4221616c05a
priyanshi-yb <pgupta@yugabyte.com>
2025-04-21 14:20:53 +0530

    Renaming the issue names of Timestamp issues and add column name to the description

--- yb-voyager/src/query/queryissue/parser_issue_detector_test.go
+++ yb-voyager/src/query/queryissue/parser_issue_detector_test.go
@@ -1424,23 +1424,33 @@ func TestCompressionClause(t *testing.T) {
 
 }
 
-func TestRangeShardingIndexes(t *testing.T) {
+func TestTimestampOrDateIndexesIssues(t *testing.T) {
 	stmts := []string{
-		`CREATE TABLE test(id int, created_at timestamp with time zone, val text);`,
+		`CREATE TABLE test(id int, created_at timestamp with time zone, val text, ordered_date date);`,
 		`CREATE INDEX idx_val on test(val); `,
 		`CREATE INDEX idx_id_created_at on test(id, created_at);`,
 		`CREATE INDEX idx_created_at on test(created_at);`,
 		`CREATE INDEX idx_id_created_at1 on test(id, created_at ASC);`,
+		`CREATE INDEX idx_created_at_ordered_date on test(created_at, ordered_date);`,
+		`CREATE INDEX idx_id_created_at_ordered_date on test(id, created_at, ordered_date);`,
 	}
 	sqlsWithExpectedIssues := map[string][]QueryIssue{
 		stmts[1]: []QueryIssue{},
 		stmts[2]: []QueryIssue{
-			NewSuggestionOnTimestampIndexesForRangeSharding(INDEX_OBJECT_TYPE, "idx_id_created_at ON test", stmts[2]),
+			NewSuggestionOnTimestampIndexesForRangeSharding(INDEX_OBJECT_TYPE, "idx_id_created_at ON test", stmts[2], "created_at"),
 		},
 		stmts[3]: []QueryIssue{
-			NewHotspotOnTimestampIndexIssue(INDEX_OBJECT_TYPE, "idx_created_at ON test", stmts[3]),
+			NewHotspotOnTimestampIndexIssue(INDEX_OBJECT_TYPE, "idx_created_at ON test", stmts[3], "created_at"),
 		},
 		stmts[4]: []QueryIssue{},
+		stmts[5]: []QueryIssue{
+			NewHotspotOnTimestampIndexIssue(INDEX_OBJECT_TYPE, "idx_created_at_ordered_date ON test", stmts[5], "created_at"),
+			NewSuggestionOnDateIndexesForRangeSharding(INDEX_OBJECT_TYPE, "idx_created_at_ordered_date ON test", stmts[5], "ordered_date"),
+		},
+		stmts[6]: []QueryIssue{
+			NewSuggestionOnDateIndexesForRangeSharding(INDEX_OBJECT_TYPE, "idx_id_created_at_ordered_date ON test", stmts[6], "ordered_date"),
+			NewSuggestionOnTimestampIndexesForRangeSharding(INDEX_OBJECT_TYPE, "idx_id_created_at_ordered_date ON test", stmts[6], "created_at"),
+		},
 	}
 	parserIssueDetector := NewParserIssueDetector()
 	err := parserIssueDetector.ParseAndProcessDDL(stmts[0])
@@ -1449,6 +1459,7 @@ func TestRangeShardingIndexes(t *testing.T) {
 		issues, err := parserIssueDetector.GetAllIssues(stmt, ybversion.LatestStable)
 		assert.NoError(t, err, "Error detecting issues for statement: %s", stmt)
 		assert.Equal(t, len(expectedIssues), len(issues), "Mismatch in issue count for statement: %s", stmt)
+		fmt.Printf("%v\n", issues)
 		for _, expectedIssue := range expectedIssues {
 			found := slices.ContainsFunc(issues, func(queryIssue QueryIssue) bool {
 				return cmp.Equal(expectedIssue, queryIssue)
