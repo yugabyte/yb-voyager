18e4a327a2a78436dba76f0353dfa90edee22172
Aneesh Makala <amakala@yugabyte.com>
2025-05-13 11:12:02 +0530

    converter for mysql

---
+++ debezium-server-voyager/debezium-server-voyagerexporter/src/main/java/io/debezium/server/ybexporter/MySQLToYbValueConverter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.ybexporter;
+
+import java.sql.JDBCType;
+import java.util.Properties;
+
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.debezium.spi.converter.CustomConverter;
+import io.debezium.spi.converter.RelationalColumn;
+
+public class MySQLToYbValueConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MySQLToYbValueConverter.class);
+
+    @Override
+    public void configure(Properties props) {
+        return;
+    }
+
+    @Override
+    public void converterFor(RelationalColumn column,
+            ConverterRegistration<SchemaBuilder> registration) {
+
+        LOGGER.info("Processing converter for column: {}, type: {}, JDBC type: {}", column.name(), column.typeName(), column.jdbcType());
+        JDBCType jdbcType = JDBCType.valueOf(column.jdbcType());
+        switch (jdbcType) {
+            case BIT:
+                LOGGER.info("Configuring stringify converter for column: {}, type: {}, JDBC type: {}", column.name(), column.typeName(), column.jdbcType());
+                registration.register(SchemaBuilder.string(), this::stringify);
+                break;
+
+        }
+    }
+
+    private Object stringify(Object x) {
+        if (x == null) {
+            return null;
+        } else {
+            return x.toString();
+        }
+    }
+
+}
