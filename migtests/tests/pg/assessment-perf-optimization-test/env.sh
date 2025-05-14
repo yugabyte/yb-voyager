e1c3af49bb92b3a04823730ef9a3be6dd00963c3
priyanshi-yb <pgupta@yugabyte.com>
2025-05-14 17:58:30 +0530

    End-to-End test for perf optimizations

---
+++ migtests/tests/pg/assessment-perf-optimization-test/env.sh
@@ -0,0 +1,4 @@
+export SOURCE_DB_TYPE="postgresql"
+export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_assessment_perf_optimizations"}
+export SOURCE_DB_SCHEMA="public,test"
+export SKIP_HTML_VALIDATION="true"
