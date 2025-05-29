22d83b7fd81b1aa5587abd6f8110f80336c9cc15
Sanyam Singhal <ssinghal@yugabyte.com>
2025-05-29 18:36:15 +0530

    Miscellaneous changes for Import data fast path (#2663)
    * Add test for all datatypes for INSERT ON CONFLICT - batch recovery mode
    
    * Restrict using enable-upsert flag with on-primary-key-conflict as IGNORE
    
    * Restrict changing the conflict action flag value during resumption of data import.
    
    * Enhance testcontainers: take locks while start/stop/terminate the cluster
    - since the cluster is shared among multiple tests which might run parallel depending on go test default/changes params
    
    * Restrict changing of --on-primary-key-conflict flag during import
    
    * minor miss
    
    * review comments
    
    * review comment: import some of rows in YB and some will be done by import data cmd
    
    * use testutils.FataIfError
    
    * Modify testutils.FatalIfError to take context for error msg
    
    * test fix

--- yb-voyager/cmd/import.go
+++ yb-voyager/cmd/import.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/slices"
 
@@ -95,7 +96,11 @@ func validateImportFlags(cmd *cobra.Command, importerRole string) error {
 	}
 	validateParallelismFlags()
 	validateTruncateTablesFlag()
-	validateOnPrimaryKeyConflictFlag()
+
+	err = validateOnPrimaryKeyConflictFlag()
+	if err != nil {
+		return fmt.Errorf("error validating --on-primary-key-conflict flag: %w", err)
+	}
 	return nil
 }
 
@@ -473,11 +478,39 @@ var onPrimaryKeyConflictActions = []string{
 	// constants.PRIMARY_KEY_CONFLICT_ACTION_UPDATE,
 }
 
-func validateOnPrimaryKeyConflictFlag() {
-	conflictAction := strings.ToUpper(tconf.OnPrimaryKeyConflictAction)
-	if conflictAction != "" {
-		if !slices.Contains(onPrimaryKeyConflictActions, conflictAction) {
-			utils.ErrExit("Error: Invalid value for --on-primary-key-conflict. Allowed values are: [%s]", strings.Join(onPrimaryKeyConflictActions, ", "))
+func validateOnPrimaryKeyConflictFlag() error {
+	log.Infof("passed value for --on-primary-key-conflict: %s", tconf.OnPrimaryKeyConflictAction)
+	tconf.OnPrimaryKeyConflictAction = strings.ToUpper(tconf.OnPrimaryKeyConflictAction)
+
+	// Check if the provided OnPrimaryKeyConflictAction is valid
+	if tconf.OnPrimaryKeyConflictAction != "" {
+		if !slices.Contains(onPrimaryKeyConflictActions, tconf.OnPrimaryKeyConflictAction) {
+			return fmt.Errorf("invalid value for --on-primary-key-conflict. Allowed values are: [%s]", strings.Join(onPrimaryKeyConflictActions, ", "))
 		}
 	}
+
+	// ensure that OnPrimaryKeyConflictAction is not changed in case of resumption
+	if !startClean {
+		msr, err := metaDB.GetMigrationStatusRecord()
+		if err != nil {
+			return fmt.Errorf("error getting migration status record: %v", err)
+		} else if msr == nil {
+			return fmt.Errorf("migration status record not found.")
+		}
+
+		if msr.OnPrimaryKeyConflictAction != "" && msr.OnPrimaryKeyConflictAction != tconf.OnPrimaryKeyConflictAction {
+			return fmt.Errorf("--on-primary-key-conflict flag cannot be changed after the import has started. "+
+				"Previous value was %s, current value is %s", msr.OnPrimaryKeyConflictAction, tconf.OnPrimaryKeyConflictAction)
+		}
+	}
+
+	// restrict setting --enable-upsert as true if --on-primary-key-conflict is not set to ERROR
+	if tconf.EnableUpsert && tconf.OnPrimaryKeyConflictAction != constants.PRIMARY_KEY_CONFLICT_ACTION_ERROR {
+		return fmt.Errorf("--enable-upsert=true can only be used with --on-primary-key-conflict=ERROR")
+	}
+
+	// once all validations passed we can save the action value in MSR
+	saveOnPrimaryKeyConflictActionInMSR()
+
+	return nil
 }
--- yb-voyager/cmd/importData.go
+++ yb-voyager/cmd/importData.go
@@ -596,6 +596,10 @@ func importData(importFileTasks []*ImportFileTask) {
 	var pendingTasks, completedTasks []*ImportFileTask
 	state := NewImportDataState(exportDir)
 	if startClean {
+		err := cleanMSRForImportDataStartClean()
+		if err != nil {
+
+		}
 		cleanImportState(state, importFileTasks)
 		pendingTasks = importFileTasks
 	} else {
@@ -1403,4 +1407,31 @@ func createInitialImportDataTableMetrics(tasks []*ImportFileTask) []*cp.UpdateIm
 	}
 
 	return result
+}
+
+func saveOnPrimaryKeyConflictActionInMSR() {
+	metaDB.UpdateMigrationStatusRecord(func(record *metadb.MigrationStatusRecord) {
+		record.OnPrimaryKeyConflictAction = tconf.OnPrimaryKeyConflictAction
+	})
+}
+
+func cleanMSRForImportDataStartClean() error {
+	if !startClean {
+		log.Infof("skipping cleaning migration status record for import data command start clean")
+		return nil
+	}
+
+	msr, err := metaDB.GetMigrationStatusRecord()
+	if err != nil {
+		return fmt.Errorf("failed to get migration status record: %s", err)
+	}
+
+	if msr == nil {
+		return fmt.Errorf("migration status record not found.")
+	} else {
+		metaDB.UpdateMigrationStatusRecord(func(record *metadb.MigrationStatusRecord) {
+			msr.OnPrimaryKeyConflictAction = ""
+		})
+	}
+	return nil
 }
