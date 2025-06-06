a15bd7e750353eead2647474bc032ac70112f0db
Sanyam Singhal <ssinghal@yugabyte.com>
2025-06-06 13:50:50 +0000

    Fix output order

--- yb-voyager/cmd/integration_test.go
+++ yb-voyager/cmd/integration_test.go
@@ -76,11 +76,10 @@ func TestOracle_ReportUnsupportedIndexTypes(t *testing.T) {
 	}
 
 	// Check if the report contains unsupported index types
-	expectedJsonString := `There are some GIN indexes present in the schema, but GIN indexes are partially supported in YugabyteDB as mentioned in (https://github.com/yugabyte/yugabyte-db/issues/7850) so take a look and modify them if not supported.
-Indexes which are neither exported by yb-voyager as they are unsupported in YB and needs to be handled manually:
+	expectedJsonString := `Indexes which are neither exported by yb-voyager as they are unsupported in YB and needs to be handled manually:
 		Index Name=IDX_ADDRESS_TEXT, Index Type=DOMAIN
 		Index Name=IDX_PHONE_REVERSE, Index Type=NORMAL/REV
-`
+There are some GIN indexes present in the schema, but GIN indexes are partially supported in YugabyteDB as mentioned in (https://github.com/yugabyte/yugabyte-db/issues/7850) so take a look and modify them if not supported.`
 
 	var actualString string
 	for _, dbObject := range report.SchemaSummary.DBObjects {
