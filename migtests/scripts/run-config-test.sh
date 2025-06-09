523ec368fd3e409738993c55cd9fe14792b66690
shubham-yb <shubhamdabriwala01@gmail.com>
2025-06-09 18:42:54 +0000

    Framework conception and initial changes

---
+++ migtests/scripts/run-config-test.sh
@@ -0,0 +1,211 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -gt 2 ]; then
+    echo "Usage: $0 TEST_NAME [env.sh]"
+    exit 1
+fi
+
+set -x
+
+export YB_VOYAGER_SEND_DIAGNOSTICS=false
+export TEST_NAME=$1
+
+export REPO_ROOT="${PWD}"
+export SCRIPTS="${REPO_ROOT}/migtests/scripts"
+export TESTS_DIR="${REPO_ROOT}/migtests/tests"
+export TEST_DIR="${TESTS_DIR}/${TEST_NAME}"
+export PYTHONPATH="${REPO_ROOT}/migtests/lib"
+
+CONFIG_TEMPLATE="${SCRIPTS}/config-templates/offline-migration.yaml"
+GENERATED_CONFIG="${TEST_DIR}/generated-config.yaml"
+CONFIG_GEN_SCRIPT="${SCRIPTS}/generate_config.py"
+
+# Order of env.sh import matters.
+if [ $2 != "" ] #if env.sh is passed as an argument, source it
+then
+    if [ ! -f "${TEST_DIR}/$2" ]
+	then
+		echo "$2 file not found in the test directory"
+		exit 1
+	fi
+	source ${TEST_DIR}/$2
+else
+	source ${TEST_DIR}/env.sh
+fi
+
+source ${SCRIPTS}/${SOURCE_DB_TYPE}/env.sh
+
+
+source ${SCRIPTS}/functions.sh
+
+normalize_and_export_vars "offline"
+
+source ${SCRIPTS}/yugabytedb/env.sh
+
+
+main() {
+	echo "Deleting the parent export-dir present in the test directory"
+	rm -rf ${EXPORT_DIR}	
+	echo "Creating export-dir in the parent test directory"
+	mkdir -p ${EXPORT_DIR}
+	echo "Assigning permissions to the export-dir to execute init-db, cleanup-db scripts"
+	chmod +x ${TEST_DIR}/init-db ${TEST_DIR}/cleanup-db
+
+	step "START: ${TEST_NAME}"
+	print_env
+
+    # Generate the config dynamically
+    python3 "$CONFIG_GEN_SCRIPT" --template "$CONFIG_TEMPLATE" --output "$GENERATED_CONFIG"
+
+	pushd ${TEST_DIR}
+
+	step "Initialise source database."
+	if [[ "${SKIP_DB_CREATION}" != "true" ]]; then
+	    if [[ "${SOURCE_DB_TYPE}" == "postgresql" || "${SOURCE_DB_TYPE}" == "mysql" ]]; then
+	        create_source_db "${SOURCE_DB_NAME}"
+	    elif [[ "${SOURCE_DB_TYPE}" == "oracle" ]]; then
+	        create_source_db "${SOURCE_DB_SCHEMA}"
+	    else
+	        echo "ERROR: Unsupported SOURCE_DB_TYPE: ${SOURCE_DB_TYPE}"
+	        exit 1
+	    fi
+	else
+	    echo "Skipping database creation as SKIP_DB_CREATION is set to true."
+	fi
+	./init-db
+
+	step "Grant source database user permissions"
+	grant_permissions ${SOURCE_DB_NAME} ${SOURCE_DB_TYPE} ${SOURCE_DB_SCHEMA}
+
+	step "Check the Voyager version installed"
+	yb-voyager version
+
+	step "Assess Migration"
+	if [ "${SOURCE_DB_TYPE}" = "postgresql" ] || [ "${SOURCE_DB_TYPE}" == "oracle" ]; then
+		yb-voyager assess-migration -c "${GENERATED_CONFIG}" --yes || {
+			cat_log_file "yb-voyager-assess-migration.log"
+			cat_file ${EXPORT_DIR}/assessment/metadata/yb-voyager-assessment.log
+		}
+
+		step "Validate Assessment Reports"
+		# Checking if the assessment reports were created
+		if [ -f "${EXPORT_DIR}/assessment/reports/migration_assessment_report.html" ] && [ -f "${EXPORT_DIR}/assessment/reports/migration_assessment_report.json" ]; then
+			echo "Assessment reports created successfully."
+			validate_failure_reasoning "${EXPORT_DIR}/assessment/reports/migration_assessment_report.json"
+		else
+			echo "Error: Assessment reports were not created successfully."
+			cat_log_file "yb-voyager-assess-migration.log"
+			exit 1
+		fi
+
+		post_assess_migration
+	fi
+
+	step "Export schema."
+	yb-voyager export schema -c "${GENERATED_CONFIG}" --yes
+	find ${EXPORT_DIR}/schema -name '*.sql' -printf "'%p'\n"| xargs grep -wh CREATE
+
+	step "Analyze schema."
+    yb-voyager analyze-schema -c "${GENERATED_CONFIG}" --yes
+	tail -20 ${EXPORT_DIR}/reports/schema_analysis_report.json
+
+	step "Fix schema."
+	if [ -x "${TEST_DIR}/fix-schema" ]
+	then
+		 "${TEST_DIR}/fix-schema"
+	fi
+
+	step "Analyze schema."
+	yb-voyager analyze-schema -c "${GENERATED_CONFIG}" --yes
+	tail -20 ${EXPORT_DIR}/reports/schema_analysis_report.json
+
+	step "Export data."
+	# false if exit code of export_data is non-zero
+	yb-voyager export data -c "${GENERATED_CONFIG}" --yes || { 
+		cat_log_file "yb-voyager-export-data.log"
+		cat_log_file "debezium-source_db_exporter.log"
+		exit 1
+	}
+
+	ls -R ${EXPORT_DIR}/data | sed 's/:$//' | sed -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+
+	cat ${EXPORT_DIR}/data/export_status.json || echo "No export_status.json found."
+	cat ${EXPORT_DIR}/metainfo/dataFileDescriptor.json
+
+	step "Verify the pg_dump version being used"
+	if [ "${SOURCE_DB_TYPE}" = "postgresql" ] && { [ -z "${BETA_FAST_DATA_EXPORT}" ] || [ "${BETA_FAST_DATA_EXPORT}" = "0" ]; }; then
+	    if ! grep "Dumped by pg_dump version:" "${EXPORT_DIR}/logs/yb-voyager-export-data.log"; then
+	        echo "Error: pg_dump version not found in the log file." >&2
+	    fi
+	fi
+
+	step "Fix data."
+	if [ -x "${TEST_DIR}/fix-data" ]
+	then
+		"${TEST_DIR}/fix-data" ${EXPORT_DIR}
+	fi
+
+	step "Create target database."
+	run_ysql yugabyte "DROP DATABASE IF EXISTS ${TARGET_DB_NAME};"
+	if [ "${SOURCE_DB_TYPE}" = "postgresql" ] || [ "${SOURCE_DB_TYPE}" = "oracle" ]; then
+		run_ysql yugabyte "CREATE DATABASE ${TARGET_DB_NAME} with COLOCATION=TRUE"
+	else
+		run_ysql yugabyte "CREATE DATABASE ${TARGET_DB_NAME}"
+	fi
+
+	step "Import schema."
+	yb-voyager import schema -c "${GENERATED_CONFIG}" --yes
+	run_ysql ${TARGET_DB_NAME} "\dt"
+
+	step "Run Schema validations."
+	if [ -x "${TEST_DIR}/validate-schema" ]
+	then
+		 "${TEST_DIR}/validate-schema"
+	fi
+
+	step "Import data."
+	yb-voyager import data -c "${GENERATED_CONFIG}" --yes
+	
+	step "Import remaining schema (FK, index, and trigger) and Refreshing MViews if present."
+	yb-voyager finalize-schema-post-data-import -c "${GENERATED_CONFIG}" --yes
+	
+	run_ysql ${TARGET_DB_NAME} "\di"
+	run_ysql ${TARGET_DB_NAME} "\dft" 
+
+	step "Run validations."
+	if [[ "${EXPORT_TABLE_LIST}" != "" && -x "${TEST_DIR}/validate-with-table-list" ]]; then
+		"${TEST_DIR}/validate-with-table-list"
+	elif [[ -x "${TEST_DIR}/validate" ]]; then
+		"${TEST_DIR}/validate"
+	fi
+
+	step "Run export-data-status"
+	yb-voyager export data status -c "${GENERATED_CONFIG}" --yes --output-format json
+
+	expected_file="${TEST_DIR}/export_data_status-report.json"
+	actual_file="${EXPORT_DIR}/reports/export-data-status-report.json"
+
+	step "Verify export-data-status report"
+	verify_report ${expected_file} ${actual_file}
+
+	step "Run import-data-status"
+	yb-voyager import data status -c "${GENERATED_CONFIG}" --yes --output-format json
+
+	expected_file="${TEST_DIR}/import_data_status-report.json"
+	actual_file="${EXPORT_DIR}/reports/import-data-status-report.json"
+
+	verify_report ${expected_file} ${actual_file}
+
+	step "End Migration: clearing metainfo about state of migration from everywhere."
+	yb-voyager end migration -c "${GENERATED_CONFIG}" --yes
+
+	step "Clean up"
+	./cleanup-db
+	rm -rf "${EXPORT_DIR}"
+	run_ysql yugabyte "DROP DATABASE IF EXISTS ${TARGET_DB_NAME};"
+	rm -rf "${GENERATED_CONFIG}"
+}
+
+main
---
+++ migtests/scripts/run-live-migration-config-test.sh
@@ -0,0 +1,233 @@
+#!/usr/bin/env bash
+
+set -e
+set -m
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 TEST_NAME"
+	exit 1
+fi
+
+set -x
+
+export YB_VOYAGER_SEND_DIAGNOSTICS=false
+export TEST_NAME=$1
+
+export REPO_ROOT="${PWD}"
+export SCRIPTS="${REPO_ROOT}/migtests/scripts"
+export TESTS_DIR="${REPO_ROOT}/migtests/tests"
+export TEST_DIR="${TESTS_DIR}/${TEST_NAME}"
+export QUEUE_SEGMENT_MAX_BYTES=400
+
+export PYTHONPATH="${REPO_ROOT}/migtests/lib"
+
+CONFIG_TEMPLATE="${SCRIPTS}/config-templates/live-migration.yaml"
+GENERATED_CONFIG="${TEST_DIR}/generated-config.yaml"
+CONFIG_GEN_SCRIPT="${SCRIPTS}/generate_config.py"
+
+# Order of env.sh import matters.
+if [ -f "${TEST_DIR}/live_env.sh" ]; then
+    source "${TEST_DIR}/live_env.sh"
+else
+    source "${TEST_DIR}/env.sh"
+fi
+
+if [ "${SOURCE_DB_TYPE}" = "oracle" ]
+then
+	source ${SCRIPTS}/${SOURCE_DB_TYPE}/live_env.sh 
+else
+	source ${SCRIPTS}/${SOURCE_DB_TYPE}/env.sh
+fi
+
+source ${SCRIPTS}/functions.sh
+
+normalize_and_export_vars "live"
+
+source ${SCRIPTS}/yugabytedb/env.sh
+
+main() {
+
+	echo "Deleting the parent export-dir present in the test directory"
+	rm -rf ${EXPORT_DIR}	
+	echo "Creating export-dir in the parent test directory"
+	mkdir -p ${EXPORT_DIR}
+	echo "Assigning permissions to the export-dir to execute init-db, cleanup-db scripts"
+	chmod +x ${TEST_DIR}/init-db ${TEST_DIR}/cleanup-db
+
+	step "START: ${TEST_NAME}"
+	print_env
+
+	# Generate the config dynamically
+    python3 "$CONFIG_GEN_SCRIPT" --template "$CONFIG_TEMPLATE" --output "$GENERATED_CONFIG"
+
+	pushd ${TEST_DIR}
+
+	step "Initialise source database."
+	if [ "${SOURCE_DB_TYPE}" = "oracle" ]
+	then
+		create_source_db ${SOURCE_DB_SCHEMA}
+	fi
+	./init-db
+
+	step "Grant source database user permissions for live migration"
+	grant_permissions_for_live_migration
+
+	step "Check the Voyager version installed"
+	yb-voyager version
+
+	step "Assess Migration"
+	if [ "${SOURCE_DB_TYPE}" = "postgresql" ] || [ "${SOURCE_DB_TYPE}" == "oracle" ]; then
+		yb-voyager assess-migration -c "${GENERATED_CONFIG}" --yes || {
+			cat_log_file "yb-voyager-assess-migration.log"
+			cat_file ${EXPORT_DIR}/assessment/metadata/yb-voyager-assessment.log
+		}
+
+		step "Validate Assessment Reports"
+		# Checking if the assessment reports were created
+		if [ -f "${EXPORT_DIR}/assessment/reports/migration_assessment_report.html" ] && [ -f "${EXPORT_DIR}/assessment/reports/migration_assessment_report.json" ]; then
+			echo "Assessment reports created successfully."
+			validate_failure_reasoning "${EXPORT_DIR}/assessment/reports/migration_assessment_report.json"
+			#TODO: Further validation to be added
+		else
+			echo "Error: Assessment reports were not created successfully."
+			cat_log_file "yb-voyager-assess-migration.log"
+			exit 1
+		fi
+
+		post_assess_migration
+	fi
+
+	step "Export schema."
+	yb-voyager export schema -c "${GENERATED_CONFIG}" --yes
+	find ${EXPORT_DIR}/schema -name '*.sql' -printf "'%p'\n"| xargs grep -wh CREATE
+
+	step "Analyze schema."
+	yb-voyager analyze-schema -c "${GENERATED_CONFIG}" --yes
+	tail -20 ${EXPORT_DIR}/reports/schema_analysis_report.json
+
+	step "Fix schema."
+	if [ -x "${TEST_DIR}/fix-schema" ]
+	then
+		 "${TEST_DIR}/fix-schema"
+	fi
+
+	step "Analyze schema."
+	yb-voyager analyze-schema -c "${GENERATED_CONFIG}" --yes
+	tail -20 ${EXPORT_DIR}/reports/schema_analysis_report.json
+
+	step "Create target database."
+	run_ysql yugabyte "DROP DATABASE IF EXISTS ${TARGET_DB_NAME};"
+	if [ "${SOURCE_DB_TYPE}" = "postgresql" ] || [ "${SOURCE_DB_TYPE}" = "oracle" ]; then
+		run_ysql yugabyte "CREATE DATABASE ${TARGET_DB_NAME} with COLOCATION=TRUE"
+	else
+		run_ysql yugabyte "CREATE DATABASE ${TARGET_DB_NAME}"
+	fi
+
+	step "Import schema."
+	yb-voyager import schema -c "${GENERATED_CONFIG}" --yes
+	run_ysql ${TARGET_DB_NAME} "\dt"
+
+	step "Run Schema validations."
+	if [ -x "${TEST_DIR}/validate-schema" ]
+	then
+		 "${TEST_DIR}/validate-schema"
+	fi
+
+	step "Export data."
+	# false if exit code of export_data is non-zero
+	yb-voyager export data -c "${GENERATED_CONFIG}" --yes || { 
+		tail_log_file "yb-voyager-export-data.log"
+		tail_log_file "debezium-source_db_exporter.log"
+		exit 1
+	} &
+
+	# Storing the pid for the export data command
+	exp_pid=$!
+
+	# Killing the export process in case of failure
+	trap "kill_process -${exp_pid} ; exit 1" SIGINT SIGTERM EXIT SIGSEGV SIGHUP
+
+	# Waiting for snapshot to complete
+	timeout 100 bash -c -- 'while [ ! -f ${EXPORT_DIR}/metainfo/dataFileDescriptor.json ]; do sleep 3; done'
+
+	ls -R ${EXPORT_DIR}/data | sed 's/:$//' | sed -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+
+	cat ${EXPORT_DIR}/data/export_status.json || echo "No export_status.json found."
+	cat ${EXPORT_DIR}/metainfo/dataFileDescriptor.json
+
+	step "Import data."
+	yb-voyager import data -c "${GENERATED_CONFIG}" --yes || { 
+		tail_log_file "yb-voyager-import-data.log"
+		exit 1
+	} &
+
+	# Storing the pid for the import data command
+	imp_pid=$!
+
+	# Updating the trap command to include the importer
+	trap "kill_process -${exp_pid} ; kill_process -${imp_pid} ; exit 1" SIGINT SIGTERM EXIT SIGSEGV SIGHUP
+
+	step "Archive Changes."
+	yb-voyager archive changes -c "${GENERATED_CONFIG}" --yes &
+
+	sleep 60 
+
+	step "Import remaining schema (FK, index, and trigger) and Refreshing MViews if present."
+	yb-voyager finalize-schema-post-data-import -c "${GENERATED_CONFIG}" --yes
+
+	step "Run snapshot validations."
+	"${TEST_DIR}/validate" --live_migration 'true' --ff_enabled 'false' --fb_enabled 'false'
+
+	step "Inserting new events"
+	run_sql_file source_delta.sql
+
+	sleep 120
+
+	# Resetting the trap command
+	trap - SIGINT SIGTERM EXIT SIGSEGV SIGHUP
+
+	step "Initiating cutover"
+	yb-voyager initiate cutover to target -c "${GENERATED_CONFIG}" --yes
+
+	for ((i = 0; i < 10; i++)); do
+    if [ "$(yb-voyager cutover status --export-dir "${EXPORT_DIR}" | grep "cutover to target status" | cut -d ':'  -f 2 | tr -d '[:blank:]')" != "COMPLETED" ]; then
+        echo "Waiting for cutover to be COMPLETED..."
+        sleep 20
+        if [ "$i" -eq 9 ]; then
+            tail_log_file "yb-voyager-export-data.log"
+            tail_log_file "yb-voyager-import-data.log"
+			tail_log_file "debezium-source_db_exporter.log"		
+			exit 1
+        fi
+    else
+        break
+    fi
+	done
+
+	step "Run final validations."
+	if [ -x "${TEST_DIR}/validateAfterChanges" ]
+	then
+	"${TEST_DIR}/validateAfterChanges" --ff_enabled 'false' --fb_enabled 'false'
+	fi
+
+	step "Run get data-migration-report"
+	get_data_migration_report
+
+	expected_file="${TEST_DIR}/data-migration-report-live-migration.json"
+	actual_file="${EXPORT_DIR}/reports/data-migration-report.json"
+
+	step "Verify data-migration-report report"
+	verify_report ${expected_file} ${actual_file}
+
+	step "End Migration: clearing metainfo about state of migration from everywhere"
+	yb-voyager end migration -c "${GENERATED_CONFIG}" --yes
+
+	step "Clean up"
+	./cleanup-db
+	rm -rf "${EXPORT_DIR}"
+	run_ysql yugabyte "DROP DATABASE IF EXISTS ${TARGET_DB_NAME};"
+	rm -rf "${GENERATED_CONFIG}"
+}
+
+main
