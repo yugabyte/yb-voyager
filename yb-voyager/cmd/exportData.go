8ac92622a6de6390ebe8f6f332107855edc13fab
Aneesh Makala <amakala@yugabyte.com>
2025-06-11 10:45:48 +0530

    wip

--- yb-voyager/cmd/exportData.go
+++ yb-voyager/cmd/exportData.go
@@ -1279,6 +1279,8 @@ func exportDataOffline(ctx context.Context, cancel context.CancelFunc, finalTabl
 		exportDataTableMetrics := createUpdateExportedRowCountEventList(
 			utils.GetSortedKeys(tablesProgressMetadata))
 		controlPlane.UpdateExportedRowCount(exportDataTableMetrics)
+		// utils.PrintAndLog("sleeping")
+		// time.Sleep(time.Second * 1000)
 	}
 
 	updateFilePaths(&source, exportDir, tablesProgressMetadata)
@@ -1648,10 +1650,12 @@ func createUpdateExportedRowCountEventList(tableNames []string) []*cp.UpdateExpo
 					MigrationUUID: migrationUUID,
 					SchemaNames:   []string{schemaName},
 				},
-				TableName:         tableName2,
-				Status:            cp.EXPORT_OR_IMPORT_DATA_STATUS_INT_TO_STR[tableMetadata.Status],
-				TotalRowCount:     tableMetadata.CountTotalRows,
-				CompletedRowCount: tableMetadata.CountLiveRows,
+				TableName: tableName2,
+				Status:    cp.EXPORT_OR_IMPORT_DATA_STATUS_INT_TO_STR[tableMetadata.Status],
+				// TotalRowCount:     tableMetadata.CountTotalRows,
+				// CompletedRowCount: tableMetadata.CountLiveRows,
+				TotalRowCount:     43335675,
+				CompletedRowCount: 83335675,
 			},
 		}
 		result = append(result, &tableMetrics)
