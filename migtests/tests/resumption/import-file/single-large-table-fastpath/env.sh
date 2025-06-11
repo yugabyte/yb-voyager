b2a11820c6cf0bbfdf0b547348a7df9c3ca1545b
Sanyam Singhal <ssinghal@yugabyte.com>
2025-06-11 13:12:54 +0000

    proper db naming

--- migtests/tests/resumption/import-file/single-large-table-fastpath/env.sh
+++ migtests/tests/resumption/import-file/single-large-table-fastpath/env.sh
@@ -1,4 +1,4 @@
-export TARGET_DB_NAME=${TARGET_DB_NAME:-"resumption_target"}
+export TARGET_DB_NAME=${TARGET_DB_NAME:-"single_large_table_fastpath"}
 export TARGET_DB_SCHEMA="public"
 export DATA_DIR="s3://yb-voyager-test-data"
 export AWS_DEFAULT_REGION="us-west-2"
