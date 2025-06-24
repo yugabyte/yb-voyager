ce0232751b7ba7aa1498e07f94f764354bca1bff
priyanshi-yb <pgupta@yugabyte.com>
2025-06-24 15:02:30 +0530

    set db versions from workflow

--- yb-voyager/test/containers/testcontainers.go
+++ yb-voyager/test/containers/testcontainers.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/samber/lo"
 	log "github.com/sirupsen/logrus"
-
-	"github.com/yugabyte/yb-voyager/yb-voyager/src/ybversion"
 )
 
 // containerRegistry to ensure one container per database(dbtype+version) [Singleton Pattern]
@@ -128,33 +127,52 @@ func TerminateAllContainers() {
 func setContainerConfigDefaultsIfNotProvided(dbType string, config *ContainerConfig) {
 	// TODO: discuss and decide the default DBVersion values for each dbtype
 
+	ybVersion := os.Getenv("YB_VERSION")
+	if ybVersion == "" {
+		panic("YB_VERSION env variable is not set. ")
+	}
+
+	pgVersion := os.Getenv("PG_VERSION")
+	if pgVersion == "" {
+		panic("PG_VERSION env variable is not set. ")
+	}
+
+	oracleVersion := os.Getenv("ORACLE_VERSION")
+	if oracleVersion == "" {
+		panic("ORACLE_VERSION env variable is not set. ")
+	}
+	mysqlVersion := os.Getenv("MYSQL_VERSION")
+	if mysqlVersion == "" {
+		panic("MYSQL_VERSION env variable is not set. ")
+	}
+
 	config.DBType = dbType
 	switch dbType {
 	case POSTGRESQL:
 		config.User = lo.Ternary(config.User == "", "ybvoyager", config.User)
 		config.Password = lo.Ternary(config.Password == "", "passsword", config.Password)
-		config.DBVersion = lo.Ternary(config.DBVersion == "", "11", config.DBVersion)
+		config.DBVersion = lo.Ternary(config.DBVersion == "", pgVersion, config.DBVersion)
 		config.Schema = lo.Ternary(config.Schema == "", "public", config.Schema)
 		config.DBName = lo.Ternary(config.DBName == "", "postgres", config.DBName)
 
 	case YUGABYTEDB:
 		config.User = lo.Ternary(config.User == "", "yugabyte", config.User) // ybdb docker doesn't create specified user
 		config.Password = lo.Ternary(config.Password == "", "passsword", config.Password)
-		config.DBVersion = lo.Ternary(config.DBVersion == "", ybversion.LatestStable.String()+"-b3", config.DBVersion)
+		config.DBVersion = lo.Ternary(config.DBVersion == "", ybVersion, config.DBVersion)
 		config.Schema = lo.Ternary(config.Schema == "", "public", config.Schema)
 		config.DBName = lo.Ternary(config.DBName == "", "yugabyte", config.DBName)
 
 	case ORACLE:
 		config.User = lo.Ternary(config.User == "", "ybvoyager", config.User)
 		config.Password = lo.Ternary(config.Password == "", "passsword", config.Password)
-		config.DBVersion = lo.Ternary(config.DBVersion == "", "21", config.DBVersion)
+		config.DBVersion = lo.Ternary(config.DBVersion == "", oracleVersion, config.DBVersion)
 		config.Schema = lo.Ternary(config.Schema == "", "YBVOYAGER", config.Schema)
 		config.DBName = lo.Ternary(config.DBName == "", "DMS", config.DBName)
 
 	case MYSQL:
 		config.User = lo.Ternary(config.User == "", "ybvoyager", config.User)
 		config.Password = lo.Ternary(config.Password == "", "passsword", config.Password)
-		config.DBVersion = lo.Ternary(config.DBVersion == "", "8.4", config.DBVersion)
+		config.DBVersion = lo.Ternary(config.DBVersion == "", mysqlVersion, config.DBVersion)
 		config.DBName = lo.Ternary(config.DBName == "", "dms", config.DBName)
 
 	default:
