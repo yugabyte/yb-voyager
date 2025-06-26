5543960031ee1550a1e707a5dbcadc4d48b18a48
priyanshi-yb <pgupta@yugabyte.com>
2025-06-26 17:28:54 +0530

    Updated the tests

--- migtests/tests/pg/assessment-report-test-uqc/env.sh
+++ migtests/tests/pg/assessment-report-test-uqc/env.sh
@@ -1,3 +1,4 @@
 export SOURCE_DB_TYPE="postgresql"
 export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_assessment_report_uqc"}
 export SOURCE_DB_SCHEMA="sales,analytics"
+export SKIP_HTML_VALIDATION="true"
--- migtests/tests/pg/assessment-report-test-with-tdb/env.sh
+++ migtests/tests/pg/assessment-report-test-with-tdb/env.sh
@@ -2,3 +2,4 @@ export SOURCE_DB_TYPE="postgresql"
 export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_assessment_report"}
 export SOURCE_DB_SCHEMA="public,schema2,test_views"
 export TARGET_DB_VERSION="2.25.1.0"
+export SKIP_HTML_VALIDATION="true"
