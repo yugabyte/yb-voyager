20275ca93aee31ee07eae48dfbfadc2934433e58
Sanyam Singhal <ssinghal@yugabyte.com>
2025-06-30 12:20:57 +0000

    remove implementation of GetPrimaryKeyConstraintNames() for Oracle and PG as not required

--- yb-voyager/src/tgtdb/postgres.go
+++ yb-voyager/src/tgtdb/postgres.go
@@ -323,41 +323,9 @@ outer:
 	return nil
 }
 
-// GetPrimaryKeyColumns returns the subset of `columns` that belong to the
-// primary‑key definition of the given table.
-// Implementing this for completion but not used in Postgres fall-forward/fall-back
-// This info is only used in fast path import of batches(Target YugabyteDB)
+// No need to implement GetPrimaryKeyColumns for Postgres fall-forward/fall-back as fast path is not valid there
 func (pg *TargetPostgreSQL) GetPrimaryKeyColumns(table sqlname.NameTuple) ([]string, error) {
-	var primaryKeyColumns []string
-	schemaName, tableName := table.ForCatalogQuery()
-	query := fmt.Sprintf(`
-		SELECT a.attname
-		FROM pg_index i
-		JOIN pg_class      c ON c.oid = i.indrelid
-		JOIN pg_namespace  n ON n.oid = c.relnamespace
-		JOIN pg_attribute  a ON a.attrelid = c.oid AND a.attnum = ANY(i.indkey)
-		WHERE n.nspname = '%s'
-			AND c.relname  = '%s'
-			AND i.indisprimary;`, schemaName, tableName)
-
-	rows, err := pg.Query(query)
-	if err != nil {
-		return nil, fmt.Errorf("query PK columns for %s.%s: %w", schemaName, tableName, err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var col string
-		if err := rows.Scan(&col); err != nil {
-			return nil, fmt.Errorf("scan PK column: %w", err)
-		}
-		primaryKeyColumns = append(primaryKeyColumns, col)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return primaryKeyColumns, nil
+	return nil, nil
 }
 
 // Implementing this for completion but not used in Postgres fall-forward/fall-back
