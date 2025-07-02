3f46c33cc71a5c82ee2a1d82973a3ec5715c7870
ShivanshGahlot <shivanshgahlot@gmail.com>
2025-07-02 13:37:55 +0000

    Added refactoring

--- yb-voyager/cmd/analyzeSchema.go
+++ yb-voyager/cmd/analyzeSchema.go
@@ -352,7 +352,10 @@ func checkStmtsUsingParser(sqlInfoArr []sqlInfo, fpath string, objType string, d
 	// Finalize and populate column metadata that depends on complete schema context.
 	// This includes metadata that can only be resolved after all SQL statements are parsed,
 	// such as foreign key constraints, inherited columns, and partitioned table columns.
-	parserIssueDetector.FinalizeColumnMetadata()
+	// Run this only if object type is TABLE, as it is the only one that has columns.
+	if objType == "TABLE" {
+		parserIssueDetector.FinalizeColumnMetadata()
+	}
 
 	for _, sqlStmtInfo := range sqlInfoArr {
 		ddlIssues, err := parserIssueDetector.GetDDLIssues(sqlStmtInfo.formattedStmt, targetDbVersion)
