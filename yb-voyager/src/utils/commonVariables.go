866ef3de022fe72a71c550477cc1b6e61e79e442
Sanyam Singhal <ssinghal@yugabyte.com>
2025-07-31 14:38:23 +0530

    Add Anonymized DDL to callhome payload in assessment phase. (#2892)
    - field AnonymizedDDLs in AssessMigrationPhasePayload struct
    - bumped up payload version to 1.5

--- yb-voyager/src/utils/commonVariables.go
+++ yb-voyager/src/utils/commonVariables.go
@@ -65,7 +65,9 @@ var oracleSchemaObjectListForExport = []string{"TYPE", "SEQUENCE", "TABLE", "PAC
 var postgresSchemaObjectList = []string{"SCHEMA", "COLLATION", "EXTENSION", "TYPE", "DOMAIN", "SEQUENCE",
 	"TABLE", "INDEX", "FUNCTION", "AGGREGATE", "PROCEDURE", "VIEW", "TRIGGER",
 	"MVIEW", "RULE", "COMMENT" /* GRANT, ROLE*/, "CONVERSION", "FOREIGN TABLE", "POLICY", "OPERATOR"}
-var postgresSchemaObjectListForExport = []string{"TYPE", "DOMAIN", "SEQUENCE", "TABLE", "FUNCTION", "PROCEDURE", "AGGREGATE", "VIEW", "MVIEW", "TRIGGER", "COMMENT", "CONVERSION", "FOREIGN TABLE", "ROW SECURITY", "POLICY", "OPERATOR", "OPERATOR FAMILY", "OPERATOR CLASS"}
+var postgresSchemaObjectListForExport = []string{"TYPE", "DOMAIN", "SEQUENCE", "TABLE", "FUNCTION", "PROCEDURE",
+	"AGGREGATE", "VIEW", "MVIEW", "TRIGGER", "COMMENT", "CONVERSION", "FOREIGN TABLE", "ROW SECURITY", "POLICY",
+	"OPERATOR", "OPERATOR FAMILY", "OPERATOR CLASS"}
 
 // In MYSQL, TYPE and SEQUENCE are not supported
 var mysqlSchemaObjectList = []string{"TABLE", "PARTITION", "INDEX", "VIEW", /*"GRANT*/
