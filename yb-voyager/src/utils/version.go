2a36d3ecc56499c82d3d05a5eb5a7788de3c862a
Jenkins CI <jenkins@example.com>
2025-08-13 14:01:41 +0000

    Update YB_VOYAGER_VERSION to 0rc1.2025.8.2 and DEBEZIUM_VERSION to 0rc1.2.5.2-2025.8.2

--- yb-voyager/src/utils/version.go
+++ yb-voyager/src/utils/version.go
@@ -17,7 +17,7 @@ package utils
 
 const (
 	// This constant must be updated on every release.
-	YB_VOYAGER_VERSION = "main"
+	YB_VOYAGER_VERSION = "0rc1.2025.8.2"
 
 	// This constant must be updated after every breaking change.
 	PREVIOUS_BREAKING_CHANGE_VERSION = "1.8.5"
