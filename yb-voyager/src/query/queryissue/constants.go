50546b4bf054943094104220c7c2c256888ec9c8
ShivanshGahlot <shivanshgahlot@gmail.com>
2025-08-28 11:01:48 +0000

    Made suggested changes

--- yb-voyager/src/query/queryissue/constants.go
+++ yb-voyager/src/query/queryissue/constants.go
@@ -477,7 +477,7 @@ Note: If the table is created as colocated, this hotspot concern can safely be i
 	// Recommend PK when UNIQUE + all NOT NULL but no PK exists
 	MISSING_PRIMARY_KEY_WHEN_UNIQUE_NOT_NULL             = "MISSING_PRIMARY_KEY_WHEN_UNIQUE_NOT_NULL"
 	MISSING_PRIMARY_KEY_WHEN_UNIQUE_NOT_NULL_ISSUE_NAME  = "Missing primary key for table when unique and not null columns exist"
-	MISSING_PRIMARY_KEY_WHEN_UNIQUE_NOT_NULL_DESCRIPTION = "PostgreSQL's table storage is heap-oriented, however, YugabyteDB's table storage is index-oriented. So, a table without a primary key is viable in PostgreSQL but isn't in YugabyteDB. If you don't specify a primary key, YugabyteDB will use the internal ybrowid column as PRIMARY KEY and the table will be sharded on ybrowid HASH. However, if there are candidate primary keys (unique + NOT NULL), it is recommended to define them as a Primary Key to avoid secondary index structures."
+	MISSING_PRIMARY_KEY_WHEN_UNIQUE_NOT_NULL_DESCRIPTION = "YugabyteDB's table storage is index-oriented. So, a table without a primary key is not viable in YugabyteDB. If you don't specify a primary key, YugabyteDB will use the internal ybrowid column as PRIMARY KEY and the table will be sharded on ybrowid HASH. However, if there are candidate primary keys (unique + NOT NULL), it is recommended to define them as a Primary Key to avoid secondary index structures."
 )
 
 // Object types
