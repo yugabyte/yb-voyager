5bf57d33878968da9e64b63dff6208afa6a4e2be
priyanshi-yb <pgupta@yugabyte.com>
2025-09-10 14:06:39 +0530

    rename to max-retries-streaming and added the flag to config templates

--- yb-voyager/cmd/import.go
+++ yb-voyager/cmd/import.go
@@ -214,7 +214,7 @@ func registerImportDataCommonFlags(cmd *cobra.Command) {
 	BoolVar(cmd.Flags(), &disablePb, "disable-pb", false,
 		"Disable progress bar/stats during data import (default false)")
 
-	cmd.Flags().IntVar(&EVENT_BATCH_MAX_RETRY_COUNT, "max-streaming-retries", 15, "Maximum number of retries for failed event batch in live migration")
+	cmd.Flags().IntVar(&EVENT_BATCH_MAX_RETRY_COUNT, "max-retries-streaming", 15, "Maximum number of retries for failed event batch in live migration")
 
 	cmd.Flags().StringVar(&tconf.ExcludeTableList, "exclude-table-list", "",
 		"comma-separated list of the source db table names to exclude while import data.\n"+
