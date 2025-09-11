ddffed3b31ca899411766469b95b43e88962df15
shaharuk-yb <sshaikh@yugabyte.com>
2025-09-11 13:03:26 +0530

    test case fix

--- migtests/tests/pg/omnibus/expected_files/expected_failed.sql
+++ migtests/tests/pg/omnibus/expected_files/expected_failed.sql
@@ -12,37 +12,37 @@ CREATE TYPE range_type_example.float8_range AS RANGE (
 ERROR: syntax error at or near "(" (SQLSTATE 42601)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE composite_type_examples.ordinary_table (basic_ composite_type_examples.basic_comp_type, _basic composite_type_examples.basic_comp_type GENERATED ALWAYS AS (basic_) STORED, nested composite_type_examples.nested, _nested composite_type_examples.nested GENERATED ALWAYS AS (nested) STORED, CONSTRAINT check_f1_gt_1 CHECK ((basic_).f1 > 1), CONSTRAINT check_f1_gt_1_again CHECK ((_basic).f1 > 1), CONSTRAINT check_nested_f1_gt_1 CHECK ((nested).foo.f1 > 1), CONSTRAINT check_nested_f1_gt_1_again CHECK ((_nested).foo.f1 > 1));
+CREATE TABLE composite_type_examples.ordinary_table (basic_ composite_type_examples.basic_comp_type, _basic composite_type_examples.basic_comp_type GENERATED ALWAYS AS (basic_) STORED, nested composite_type_examples.nested, _nested composite_type_examples.nested GENERATED ALWAYS AS (nested) STORED, CONSTRAINT check_f1_gt_1 CHECK ((basic_).f1 > 1), CONSTRAINT check_f1_gt_1_again CHECK ((_basic).f1 > 1), CONSTRAINT check_nested_f1_gt_1 CHECK ((nested).foo.f1 > 1), CONSTRAINT check_nested_f1_gt_1_again CHECK ((_nested).foo.f1 > 1)) WITH (colocation=false);
 
 /*
 ERROR: INHERITS not supported yet (SQLSTATE 0A000)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE composite_type_examples.inherited_table () INHERITS (composite_type_examples.ordinary_table);
+CREATE TABLE composite_type_examples.inherited_table () INHERITS (composite_type_examples.ordinary_table) WITH (colocation=false);
 
 /*
 ERROR: syntax error at or near "(" (SQLSTATE 42601)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE enum_example.bugs (id int NOT NULL, description text, status enum_example.bug_status, _status enum_example.bug_status GENERATED ALWAYS AS (status) STORED, severity enum_example.bug_severity, _severity enum_example.bug_severity GENERATED ALWAYS AS (severity) STORED, info enum_example.bug_info GENERATED ALWAYS AS (enum_example.make_bug_info(status, severity)) STORED);
+CREATE TABLE enum_example.bugs (id int NOT NULL, description text, status enum_example.bug_status, _status enum_example.bug_status GENERATED ALWAYS AS (status) STORED, severity enum_example.bug_severity, _severity enum_example.bug_severity GENERATED ALWAYS AS (severity) STORED, info enum_example.bug_info GENERATED ALWAYS AS (enum_example.make_bug_info(status, severity)) STORED) WITH (colocation=false);
 
 /*
 ERROR: INHERITS not supported yet (SQLSTATE 0A000)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE enum_example.bugs_clone () INHERITS (enum_example.bugs);
+CREATE TABLE enum_example.bugs_clone () INHERITS (enum_example.bugs) WITH (colocation=false);
 
 /*
 ERROR: INHERITS not supported yet (SQLSTATE 0A000)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE regress_rls_schema.t2 (c double precision) INHERITS (regress_rls_schema.t1);
+CREATE TABLE regress_rls_schema.t2 (c double precision) INHERITS (regress_rls_schema.t1) WITH (colocation=false);
 
 /*
 ERROR: INHERITS not supported yet (SQLSTATE 0A000)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE regress_rls_schema.t3_3 (id int NOT NULL, c text, b text, a int, CONSTRAINT t3_3_pkey PRIMARY KEY (id)) INHERITS (regress_rls_schema.t1_3);
+CREATE TABLE regress_rls_schema.t3_3 (id int NOT NULL, c text, b text, a int, CONSTRAINT t3_3_pkey PRIMARY KEY (id)) INHERITS (regress_rls_schema.t1_3) WITH (colocation=false);
 
 /*
 ERROR: syntax error at or near "(" (SQLSTATE 42601)
@@ -69,11 +69,11 @@ ERROR: VIEW WITH CASCADED CHECK OPTION not supported yet (SQLSTATE 0A000)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/views/view.sql
 */
 CREATE VIEW regress_rls_schema.bv1 WITH (security_barrier='true') AS
- SELECT a,
-    b
-   FROM regress_rls_schema.b1
-  WHERE (a > 0)
-  WITH CASCADED CHECK OPTION;
+SELECT a,
+       b
+FROM regress_rls_schema.b1
+WHERE (a > 0)
+    WITH CASCADED CHECK OPTION;
 
 /*
 ERROR: CREATE CONVERSION not supported yet (SQLSTATE 0A000)
@@ -111,7 +111,7 @@ CREATE OPERATOR public.<% (
 ERROR: type "range_type_example.float8_range" does not exist (SQLSTATE 42704)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/tables/table.sql
 */
-CREATE TABLE range_type_example.example_tbl (col range_type_example.float8_range);
+CREATE TABLE range_type_example.example_tbl (col range_type_example.float8_range) WITH (colocation=false);
 
 /*
 ERROR: relation "enum_example.bugs" does not exist (SQLSTATE 42P01)
@@ -158,7 +158,7 @@ File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/functi
 CREATE FUNCTION range_type_example.return_depends_on_range_type() RETURNS range_type_example.float8_range
     LANGUAGE sql IMMUTABLE
     AS $$
-    SELECT '[1.2, 3.4]'::range_type_example.float8_range
+SELECT '[1.2, 3.4]'::range_type_example.float8_range
   $$;
 
 /*
@@ -166,40 +166,40 @@ ERROR: relation "composite_type_examples.ordinary_table" does not exist (SQLSTAT
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/views/view.sql
 */
 CREATE VIEW composite_type_examples.basic_view AS
- SELECT basic_,
-    _basic,
-    nested,
-    _nested
-   FROM composite_type_examples.ordinary_table;
+SELECT basic_,
+       _basic,
+       nested,
+       _nested
+FROM composite_type_examples.ordinary_table;
 
 /*
 ERROR: relation "enum_example.bugs" does not exist (SQLSTATE 42P01)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/views/view.sql
 */
 CREATE VIEW enum_example._bugs AS
- SELECT id,
-    status
-   FROM enum_example.bugs;
+SELECT id,
+       status
+FROM enum_example.bugs;
 
 /*
 ERROR: relation "foreign_db_example.technically_doesnt_exist" does not exist (SQLSTATE 42P01)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/views/view.sql
 */
 CREATE VIEW public.foreign_db_example AS
- SELECT id,
-    uses_type,
-    _uses_type,
-    positive_number,
-    _positive_number
-   FROM foreign_db_example.technically_doesnt_exist;
+SELECT id,
+       uses_type,
+       _uses_type,
+       positive_number,
+       _positive_number
+FROM foreign_db_example.technically_doesnt_exist;
 
 /*
 ERROR: relation "range_type_example.example_tbl" does not exist (SQLSTATE 42P01)
 File :/home/ubuntu/yb-voyager/migtests/tests/pg/omnibus/export-dir/schema/views/view.sql
 */
 CREATE VIEW range_type_example.depends_on_col_using_type AS
- SELECT col
-   FROM range_type_example.example_tbl;
+SELECT col
+FROM range_type_example.example_tbl;
 
 /*
 ERROR: role "regress_rls_eve" does not exist (SQLSTATE 42704)
