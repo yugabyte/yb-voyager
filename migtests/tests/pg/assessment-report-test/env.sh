143d7e2bc4609e853b0a4895d1733342a70830b0
yashtyagi-yb <yashtyagis9578@gmail.com>
2025-10-09 16:29:13 +0530

    Added normalisation for target_db_version for callhome automation script (#3122)

--- migtests/tests/pg/assessment-report-test/env.sh
+++ migtests/tests/pg/assessment-report-test/env.sh
@@ -1,5 +1,4 @@
 export SOURCE_DB_TYPE="postgresql"
-export SOURCE_DB_NAME=${SOURCE_DB_NAME:-"pg_assessment_report"}
 export SOURCE_DB_SCHEMA="public,schema2,test_views"
 export SKIP_HTML_VALIDATION="true"
 export ANON_SALT="a9f8c2d4e7b6f1a3d9c0b7e4f5a6c3d2"
