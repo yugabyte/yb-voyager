9c8248402d6cc795d80774979f2f585c965d8b66
shubham-yb <shubhamdabriwala01@gmail.com>
2025-10-17 13:02:08 +0000

    Cleanup and bug fix

--- migtests/scripts/run-validate-assessment-report.sh
+++ migtests/scripts/run-validate-assessment-report.sh
@@ -56,18 +56,16 @@ main() {
 	pushd ${TEST_DIR}
 
 	step "Initialise source database."
-	if [[ "${SKIP_DB_CREATION}" != "true" ]]; then
-	    if [[ "${SOURCE_DB_TYPE}" = "postgresql" || "${SOURCE_DB_TYPE}" = "mysql" ]]; then
-	        create_source_db "${SOURCE_DB_NAME}"
-	    elif [[ "${SOURCE_DB_TYPE}" = "oracle" ]]; then
-	        create_source_db "${SOURCE_DB_SCHEMA}"
-	    else
-	        echo "ERROR: Unsupported SOURCE_DB_TYPE: ${SOURCE_DB_TYPE}"
-	        exit 1
-	    fi
-	else
-	    echo "Skipping database creation as SKIP_DB_CREATION is set to true."
-	fi
+
+    if [[ "${SOURCE_DB_TYPE}" = "postgresql" ]]; then
+        create_source_db "${SOURCE_DB_NAME}"
+    elif [[ "${SOURCE_DB_TYPE}" = "oracle" ]]; then
+        create_source_db "${SOURCE_DB_SCHEMA}"
+    else
+        echo "ERROR: Unsupported SOURCE_DB_TYPE: ${SOURCE_DB_TYPE}"
+        exit 1
+    fi
+
 	./init-db
 
 	step "Grant source database user permissions"
