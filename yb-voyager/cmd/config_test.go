0e46da6c5e9257cc63fe1e26fcfffdc272b92bf7
Aneesh Makala <amakala@yugabyte.com>
2025-10-27 10:41:57 +0530

    Error Policy Miscellaneous Changes (#3148)
    * Changed on-primary-key-conflict option from ERROR -> ERROR-POLICY to make the expected behavior clear.
    Context: The expected behavior is that when user configures ERROR/ERROR-POLICY, the conflict error will be handled as per error-policy (abort or stash-and-continue). The current value (ERROR) can be misleading in case error-policy is stash-and-continue; therefore, we are changing it to ERROR-POLICY.
    * In case we abort due to any error, we additionally print a msg to the on the console recommending them to use stash-and-continue if they want.

--- yb-voyager/cmd/config_test.go
+++ yb-voyager/cmd/config_test.go
@@ -1844,7 +1844,7 @@ func TestImportDataConfigBinding_CLIOverridesConfig(t *testing.T) {
 		"--truncate-tables", "false",
 		"--skip-node-health-checks", "false",
 		"--skip-disk-usage-health-checks", "false",
-		"--on-primary-key-conflict", "ERROR",
+		"--on-primary-key-conflict", "ERROR-POLICY",
 		"--disable-transactional-writes", "false",
 		"--truncate-splits", "true",
 		"--error-policy-snapshot", "abort",
@@ -1906,7 +1906,7 @@ func TestImportDataConfigBinding_CLIOverridesConfig(t *testing.T) {
 	assert.Equal(t, utils.BoolStr(false), truncateTables, "Truncate tables for importing data should be overridden by CLI")
 	assert.Equal(t, utils.BoolStr(false), skipNodeHealthChecks, "Skip node health checks for importing data should be overridden by CLI")
 	assert.Equal(t, utils.BoolStr(false), skipDiskUsageHealthChecks, "Skip disk usage health checks for importing data should be overridden by CLI")
-	assert.Equal(t, "ERROR", tconf.OnPrimaryKeyConflictAction, "On primary key conflict for importing data should be overridden by CLI")
+	assert.Equal(t, "ERROR-POLICY", tconf.OnPrimaryKeyConflictAction, "On primary key conflict for importing data should be overridden by CLI")
 	assert.Equal(t, utils.BoolStr(false), tconf.DisableTransactionalWrites, "Disable transaction writes for importing data should be overridden by CLI")
 	assert.Equal(t, utils.BoolStr(true), truncateSplits, "Truncate splits for importing data should be overridden by CLI")
 	assert.Equal(t, "10485760", os.Getenv("CSV_READER_MAX_BUFFER_SIZE_BYTES"), "CSV reader max buffer size bytes should match the config")
@@ -2455,7 +2455,7 @@ func TestImportDataFileConfigBinding_CLIOverridesConfig(t *testing.T) {
 		"--skip-replication-checks", "false",
 		"--skip-node-health-checks", "false",
 		"--skip-disk-usage-health-checks", "false",
-		"--on-primary-key-conflict", "ERROR",
+		"--on-primary-key-conflict", "ERROR-POLICY",
 		"--error-policy", "abort",
 		"--target-db-host", "localhost2",
 		"--target-db-port", "5433",
@@ -2522,7 +2522,7 @@ func TestImportDataFileConfigBinding_CLIOverridesConfig(t *testing.T) {
 	assert.Equal(t, utils.BoolStr(false), skipReplicationChecks, "Skip replication checks for importing data should be overridden by CLI")
 	assert.Equal(t, utils.BoolStr(false), skipNodeHealthChecks, "Skip node health checks for importing data should be overridden by CLI")
 	assert.Equal(t, utils.BoolStr(false), skipDiskUsageHealthChecks, "Skip disk usage health checks for importing data should be overridden by CLI")
-	assert.Equal(t, "ERROR", tconf.OnPrimaryKeyConflictAction, "On primary key conflict for importing data should be overridden by CLI")
+	assert.Equal(t, "ERROR-POLICY", tconf.OnPrimaryKeyConflictAction, "On primary key conflict for importing data should be overridden by CLI")
 	assert.Equal(t, importdata.AbortErrorPolicy, errorPolicySnapshotFlag, "error-policy should be overridden by the CLI")
 
 	assert.Equal(t, "10485760", os.Getenv("CSV_READER_MAX_BUFFER_SIZE_BYTES"), "CSV reader max buffer size bytes should be overridden by CLI")
