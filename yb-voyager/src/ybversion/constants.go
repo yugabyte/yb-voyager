4deea8dba984ebd33ba3aa79e312be3e9a4fab86
priyanshi-yb <pgupta@yugabyte.com>
2025-12-12 16:06:53 +0530

    fix tests

--- yb-voyager/src/ybversion/constants.go
+++ yb-voyager/src/ybversion/constants.go
@@ -43,6 +43,7 @@ var V2024_2_3_0 *YBVersion
 var V2024_2_3_1 *YBVersion
 var V2024_2_4_0 *YBVersion
 var V2025_1_0_0 *YBVersion
+var V2025_2_0_0 *YBVersion
 var V2_23_0_0 *YBVersion
 var V2025_1_1_1 *YBVersion
 var V2_25_0_0 *YBVersion
@@ -114,6 +115,11 @@ func init() {
 	V2025_1_0_0, err = NewYBVersion("2025.1.0.0")
 	if err != nil {
 		panic("could not create version 2025.1.0.0")
+	}
+
+	V2025_2_0_0, err = NewYBVersion("2025.2.0.0")
+	if err != nil {
+		panic("could not create version 2025.2.0.0")
 	}
 
 	// Whenever latest_stable version is updated in yb-versions.json, it will be reflected here as well.
