b7c4149324d4510e3ab0d59de29c2973668550cb
Jenkins CI <jenkins@example.com>
2025-12-30 06:51:01 +0000

    Update YB_VOYAGER_VERSION to 2025.12.2 and DEBEZIUM_VERSION to 2.5.2-2025.12.2

--- yb-voyager/src/dbzm/dbzm.go
+++ yb-voyager/src/dbzm/dbzm.go
@@ -37,7 +37,7 @@ import (
 var DEBEZIUM_DIST_DIR, DEBEZIUM_CONF_FILEPATH string
 
 // These versions need to be changed at the time of a release
-const DEBEZIUM_VERSION = "0rc3.2.5.2-2025.12.2"
+const DEBEZIUM_VERSION = "2.5.2-2025.12.2"
 
 type Debezium struct {
 	*Config
