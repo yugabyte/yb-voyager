fb2901dbba8217cf0304058db38d0ee26a701ebe
yashtyagi-yb <yash.tyagi@yugabyte.com>
2026-01-06 08:16:11 +0000

    added seperate folder for large row and col test

---
+++ migtests/tests/resumption/live-migration/fallf-large-row-resumption-test/init.sql
@@ -0,0 +1,26 @@
+drop table if exists large_row_table;
+
+create table large_row_table (
+    id SERIAL PRIMARY KEY,
+    data TEXT
+);
+
+\d large_row_table
+
+-- table for cutover/backlog checks
+DROP TABLE IF EXISTS public.cutover_table;
+CREATE TABLE public.cutover_table (
+	id SERIAL PRIMARY KEY,
+    status TEXT
+);
+
+-- set replica identity full for all tables
+DO $CUSTOM$ 
+    DECLARE
+		r record;
+    BEGIN
+        FOR r IN (SELECT table_schema,table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE') 
+        LOOP
+            EXECUTE 'ALTER TABLE ' || r.table_schema || '."' || r.table_name || '" REPLICA IDENTITY FULL';
+        END LOOP;
+    END $CUSTOM$;
