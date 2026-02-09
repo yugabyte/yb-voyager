b405785afb0cc5d8378061070e2b76af82fc45e9
ShivanshGahlot <shivanshgahlot@gmail.com>
2026-02-09 12:51:17 +0000

    add CDC transform failure injection resume test

---
+++ yb-voyager/cmd/test_log_helpers.go
@@ -0,0 +1,29 @@
+//go:build failpoint
+
+package cmd
+
+import (
+	"os"
+	"testing"
+
+	"github.com/fatih/color"
+)
+
+func init() {
+	// Force colors even when go test output isn't a TTY.
+	color.NoColor = false
+	color.Output = os.Stdout
+}
+
+const testLogPrefix = "[TEST] "
+
+func logTest(t *testing.T, msg string) {
+	t.Helper()
+	t.Log(color.HiCyanString("%s%s", testLogPrefix, msg))
+}
+
+func logTestf(t *testing.T, format string, args ...any) {
+	t.Helper()
+	t.Log(color.HiCyanString(testLogPrefix+format, args...))
+}
+
