d48249ff04a2bf65ebdb8fed4dc9e2d90f143499
yashtyagi-yb <yashtyagis9578@gmail.com>
2026-02-11 23:09:30 +0530

    Adding large schema live migration resumption tests (#3300)
    Adding large schema live migration resumption tests

--- migtests/tests/resumption/live-migration/common-sql/init.sql
+++ migtests/tests/resumption/live-migration/common-sql/init.sql
@@ -59,21 +59,3 @@ CREATE TABLE tsvector_table (
     title_tsv TSVECTOR,
     content_tsv TSVECTOR
 );
-
--- table for cutover/backlog checks
-DROP TABLE IF EXISTS public.cutover_table;
-CREATE TABLE public.cutover_table (
-	id SERIAL PRIMARY KEY,
-    status TEXT
-);
-
--- set replica identity full for all tables
-DO $CUSTOM$ 
-    DECLARE
-		r record;
-    BEGIN
-        FOR r IN (SELECT table_schema,table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE') 
-        LOOP
-            EXECUTE 'ALTER TABLE ' || r.table_schema || '."' || r.table_name || '" REPLICA IDENTITY FULL';
-        END LOOP;
-    END $CUSTOM$;
