b7178af05bf489dbafc3d7e0fd174d3b907155e8
priyanshi-yb <pgupta@yugabyte.com>
2026-04-03 14:38:38 +0530

    fix import schema to only use parser for specific cases like ALTER TABLE and SET variables

--- yb-voyager/cmd/importSchemaYugabyteDB.go
+++ yb-voyager/cmd/importSchemaYugabyteDB.go
@@ -101,6 +101,13 @@ func generateAnalyzeReport(targetYBDBVersion string) (string, error) {
 }
 
 func isNotValidConstraint(stmt string) (bool, error) {
+	if !strings.HasPrefix(strings.ToUpper(stmt), "ALTER TABLE") {
+		//We should not use parser for every statement as some DDL statement can have YB specific syntax like SPLIT INTO x tablets, PRIMARY KEY (x HASH)
+		//but we right now use PG parser to parse the statement so it fails with syntax error for such statements so we are skipping the parser for such statements
+		//and only parsing the ALTER statements as ALTER most doesn't have support for any YB specific syntax as per docs, but one case where it is possible is
+		//ALTER TABLE ADD PRIMARY KEY (x HASH), but in most cases we don't have ADD PK DDL via voyager schema export
+		return false, nil
+	}
 	parseTree, err := queryparser.Parse(stmt)
 	if err != nil {
 		return false, goerrors.Errorf("error parsing the ddl[%s]: %v", stmt, err)
@@ -175,6 +182,9 @@ func executeSqlFile(file string, objType string, skipFn func(string, string) boo
 }
 
 func isSessionVariable(stmt string) (bool, error) {
+	if !strings.HasPrefix(strings.ToUpper(stmt), "SET") {
+		return false, nil
+	}
 	parseTree, err := queryparser.Parse(stmt)
 	if err != nil {
 		return false, fmt.Errorf("error parsing statement: %w", err)
