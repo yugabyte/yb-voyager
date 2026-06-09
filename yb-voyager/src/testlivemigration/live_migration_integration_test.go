d187546f0b750ef575d4856176bd6c6d343399b3
priyanshi-yb <pgupta@yugabyte.com>
2026-06-09 20:02:55 +0530

    fix test as per table partitioning strategy

--- yb-voyager/src/testlivemigration/live_migration_integration_test.go
+++ yb-voyager/src/testlivemigration/live_migration_integration_test.go
@@ -1044,7 +1044,7 @@ FROM generate_series(1, 10);`,
 	//check if the cdc partitioning strategy is auto after the first import
 	importDataStatus, err := testMetaDB.GetImportDataStatusRecord()
 	testutils.FatalIfError(t, err, "Failed to get import data status record")
-	assert.Equal(t, importDataStatus.CdcPartitioningStrategyConfig, "auto")
+	assert.Equal(t, importDataStatus.CdcPartitioningStrategyConfig, "table")
 
 	//so instead dropping the table and creating it back
 	err = lm.WithTargetConn(func(target *sql.DB) error {
