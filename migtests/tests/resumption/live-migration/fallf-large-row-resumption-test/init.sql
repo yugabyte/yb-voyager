99a1ab32c08ee98d18ad190e0e369aa7bf2818ac
yashtyagi-yb <yash.tyagi@yugabyte.com>
2026-06-18 06:13:12 +0000

    addressed another comments

--- migtests/tests/resumption/live-migration/fallf-large-row-resumption-test/init.sql
+++ migtests/tests/resumption/live-migration/fallf-large-row-resumption-test/init.sql
@@ -49,5 +49,6 @@ CREATE TABLE large_col_table (
     uuid_col UUID
 );
 
+-- Set the temp file limit to 5GB to avoid the error "ERROR: temp file limit exceeded" while row_hash_validations in tests
 ALTER DATABASE test_db SET temp_file_limit = 5242880; -- 5GB
 ALTER DATABASE ff_db SET temp_file_limit = 5242880; -- 5GB
--- migtests/tests/resumption/live-migration/fallf-large-row-resumption-test/init_target.sql
+++ migtests/tests/resumption/live-migration/fallf-large-row-resumption-test/init_target.sql
@@ -1,1 +1,2 @@
+-- Set the temp file limit to 5GB to avoid the error "ERROR: temp file limit exceeded" while row_hash_validations in tests
 ALTER DATABASE test_db SET temp_file_limit = 5242880; -- 5GB
