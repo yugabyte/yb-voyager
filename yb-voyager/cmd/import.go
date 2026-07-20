a37dadb8ea9b206ffba9d20044d589d6e5a677dd
priyanshi-yb <pgupta@yugabyte.com>
2026-07-20 14:17:42 +0530

    make disable sequential scan as default true

--- yb-voyager/cmd/import.go
+++ yb-voyager/cmd/import.go
@@ -462,7 +462,7 @@ Note that for the cases where a table doesn't have a primary key, this may lead 
 		"Port for Prometheus metrics server (default: 9101)")
 	cmd.Flags().MarkHidden("prometheus-metrics-port")
 
-	BoolVar(cmd.Flags(), &tconf.DisableSequentialScanOnUpdateDeletes, "disable-sequential-scan-on-update-deletes", false,
+	BoolVar(cmd.Flags(), &tconf.DisableSequentialScanOnUpdateDeletes, "disable-sequential-scan-on-update-deletes", true,
 		"Disable sequential scan on update and delete operations so that concurrent writes in repeatable isoltation to avoid any retriable errors(default false)")
 	cmd.Flags().MarkHidden("disable-sequential-scan-on-update-deletes")
 }
