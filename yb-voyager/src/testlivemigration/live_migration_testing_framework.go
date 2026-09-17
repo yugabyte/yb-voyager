0eb5ba1bbc620e2b7abf2ed6c07ad0600222f878
Priyanshi Gupta <pgupta@yugabyte.com>
2026-09-17 12:49:20 +0530

    Enhance assertions of the live migration unique key conflicts related tests (#3798)
    * fix
    
    * run get data migration report synchronously
    
    * run get data migration report synchronously and add retry on command failure ecause of timing
    
    * minor commenty
    
    * review comments
    
    * empty
    
    * empty
    
    * empty

--- yb-voyager/src/testlivemigration/live_migration_testing_framework.go
+++ yb-voyager/src/testlivemigration/live_migration_testing_framework.go
@@ -1480,6 +1480,8 @@ type DataMigrationReport struct {
 }
 
 // GetDataMigrationReport retrieves the migration report
+// This function retries 5 times with a 2 second delay between retries.
+// If the function fails to get the data migration report for any reason it just logs the error and retries until it succeeds or retries exhausted.
 func (lm *LiveMigrationTest) GetDataMigrationReport() (*DataMigrationReport, error) {
 	if lm.metaDB == nil {
 		err := lm.InitMetaDB()
@@ -1501,13 +1503,20 @@ func (lm *LiveMigrationTest) GetDataMigrationReport() (*DataMigrationReport, err
 		if lm.sourceReplicaContainer != nil {
 			reportArgs = append(reportArgs, "--source-replica-db-password", lm.sourceReplicaContainer.GetConfig().Password)
 		}
-		err := testutils.NewVoyagerCommandRunner(nil, "get data-migration-report", reportArgs, nil, true).WithT(lm.t).Run()
+		err := testutils.NewVoyagerCommandRunner(nil, "get data-migration-report", reportArgs, nil, false).WithT(lm.t).Run()
 		if err != nil {
-			return nil, goerrors.Errorf("get data-migration-report command failed: %w", err)
+			lm.t.Logf("failed to get data migration report: %v", err)
+			maxRetry--
+			if maxRetry <= 0 {
+				return nil, goerrors.Errorf("failed to get data migration report: %w", err)
+			}
+			time.Sleep(2 * time.Second)
+			continue
 		}
 
 		reportFilePath := filepath.Join(lm.exportDir, "reports", "data-migration-report.json")
 		if !utils.FileOrFolderExists(reportFilePath) {
+			lm.t.Logf("report file does not exist: %v", reportFilePath)
 			maxRetry--
 			if maxRetry <= 0 {
 				return nil, goerrors.Errorf("report file does not exist")
